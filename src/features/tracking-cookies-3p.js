9b8dd973ecb14cf57ef684ca27aeae736f08944e
Konrad Dzwinel <kdzwinel@gmail.com>
2022-01-28 17:20:37 +0100

    use globalThis in tracking-cookies-3p

--- src/features/tracking-cookies-3p.js
+++ src/features/tracking-cookies-3p.js
@@ -1,15 +1,15 @@
 import { defineProperty, postDebugMessage } from '../utils'
 
-function blockCookies (debug, window) {
+function blockCookies (debug) {
     // disable setting cookies
-    defineProperty(window.document, 'cookie', {
+    defineProperty(globalThis.document, 'cookie', {
         configurable: false,
         set: function (value) {
             if (debug) {
                 postDebugMessage('jscookie', {
                     action: 'block',
                     reason: 'tracker frame',
-                    documentUrl: window.document.location.href,
+                    documentUrl: globalThis.document.location.href,
                     scriptOrigins: [],
                     value: value
                 })
@@ -20,7 +20,7 @@ function blockCookies (debug, window) {
                 postDebugMessage('jscookie', {
                     action: 'block',
                     reason: 'tracker frame',
-                    documentUrl: window.document.location.href,
+                    documentUrl: globalThis.document.location.href,
                     scriptOrigins: [],
                     value: 'getter'
                 })
@@ -30,10 +30,10 @@ function blockCookies (debug, window) {
     })
 }
 
-export function init (args, window = globalThis.window) {
+export function init (args) {
     args.cookie.debug = args.debug
-    if (window.top !== window && args.cookie.isTrackerFrame && args.cookie.shouldBlock && args.cookie.isThirdParty) {
+    if (globalThis.top !== globalThis && args.cookie.isTrackerFrame && args.cookie.shouldBlock && args.cookie.isThirdParty) {
         // overrides expiry policy with blocking - only in subframes
-        blockCookies(args.debug, window)
+        blockCookies(args.debug)
     }
 }
