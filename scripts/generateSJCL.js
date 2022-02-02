f466e75eb295dc2d24fa75f8916bf769d60efb09
Brad Slayter <slayterdevelopment@gmail.com>
2022-02-02 09:58:53 -0600

    Lint fix

--- scripts/generateSJCL.js
+++ scripts/generateSJCL.js
@@ -10,8 +10,8 @@ async function init () {
         return
     }
 
-    await exec(`cd node_modules/sjcl/ && ./configure --no-export --compress=none --without-all --with-hmac --with-codecHex && make`)
-    const sjclFileContents = await fs.readFile(`node_modules/sjcl/sjcl.js`)
+    await exec('cd node_modules/sjcl/ && ./configure --no-export --compress=none --without-all --with-hmac --with-codecHex && make')
+    const sjclFileContents = await fs.readFile('node_modules/sjcl/sjcl.js')
     // Reexport the file as es6 module format
     const contents = `export const sjcl = (() => {
     ${sjclFileContents}
