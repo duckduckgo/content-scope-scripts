94d93d4fc30678b8157aa06f4d5922a8bd3e637d
Brad Slayter <slayterdevelopment@gmail.com>
2022-02-02 08:13:49 -0600

    Modify post install scripts to use correct paths

--- scripts/generateSJCL.js
+++ scripts/generateSJCL.js
@@ -4,8 +4,14 @@ import { exec as callbackExec } from 'child_process'
 const exec = util.promisify(callbackExec)
 
 async function init () {
-    await exec('cd node_modules/sjcl/ && ./configure --no-export --compress=none --without-all --with-hmac --with-codecHex && make')
-    const sjclFileContents = await fs.readFile('node_modules/sjcl/sjcl.js')
+    let sjclPath = 'node_modules/sjcl/'
+    if (!fs.dirExistsSync(sjclPath)) {
+        // If content scope scripts is installed as a module we need to change the path to the sjcl directory
+        sjclPath = '../../sjcl/'
+    }
+
+    await exec(`cd ${sjclPath} && ./configure --no-export --compress=none --without-all --with-hmac --with-codecHex && make`)
+    const sjclFileContents = await fs.readFile(`${sjclPath}sjcl.js`)
     // Reexport the file as es6 module format
     const contents = `export const sjcl = (() => {
     ${sjclFileContents}
