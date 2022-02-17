943d9ca3d3f32e57eaa918b46fb6b1954b7c3319
Brad Slayter <SlayterDev@users.noreply.github.com>
2022-02-17 12:08:21 -0600

    Extract Apple Config processing functions (#20)
    * Modify apple inject script for modularity
    * Modify post install scripts to use correct paths
    * Refactor build process to rollup scripts. Skip copy sjcl if it doesn't exist

--- build/apple/contentScope.js
+++ build/apple/contentScope.js
@@ -1,4 +1,56 @@
-var contentScopeFeatures = (function (exports) {
+(function () {
+    'use strict';
+
+    function getTopLevelURL () {
+        try {
+            // FROM: https://stackoverflow.com/a/7739035/73479
+            // FIX: Better capturing of top level URL so that trackers in embedded documents are not considered first party
+            if (window.location !== window.parent.location) {
+                return new URL(window.location.href !== 'about:blank' ? document.referrer : window.parent.location.href)
+            } else {
+                return new URL(window.location.href)
+            }
+        } catch (error) {
+            return new URL(location.href)
+        }
+    }
+
+    function isUnprotectedDomain (topLevelUrl, featureList) {
+        let unprotectedDomain = false;
+        const domainParts = topLevelUrl && topLevelUrl.host ? topLevelUrl.host.split('.') : [];
+
+        // walk up the domain to see if it's unprotected
+        while (domainParts.length > 1 && !unprotectedDomain) {
+            const partialDomain = domainParts.join('.');
+
+            unprotectedDomain = featureList.filter(domain => domain.domain === partialDomain).length > 0;
+
+            domainParts.shift();
+        }
+
+        return unprotectedDomain
+    }
+
+    function processConfig (data, userList, preferences) {
+        const topLevelUrl = getTopLevelURL();
+        const allowlisted = userList.filter(domain => domain === topLevelUrl.host).length > 0;
+        const enabledFeatures = Object.keys(data.features).filter((featureName) => {
+            const feature = data.features[featureName];
+            return feature.state === 'enabled' && !isUnprotectedDomain(topLevelUrl, feature.exceptions)
+        });
+        const isBroken = isUnprotectedDomain(topLevelUrl, data.unprotectedTemporary);
+        preferences.site = {
+            domain: topLevelUrl.hostname,
+            isBroken,
+            allowlisted,
+            enabledFeatures
+        };
+        // TODO
+        preferences.cookie = {};
+        return preferences
+    }
+
+    var contentScopeFeatures = (function (exports) {
   'use strict';
 
   const sjcl = (() => {
@@ -2852,68 +2904,21 @@ var contentScopeFeatures = (function (exports) {
 })({});
 
 
-function getTopLevelURL () {
-    try {
-        // FROM: https://stackoverflow.com/a/7739035/73479
-        // FIX: Better capturing of top level URL so that trackers in embedded documents are not considered first party
-        if (window.location !== window.parent.location) {
-            return new URL(window.location.href !== 'about:blank' ? document.referrer : window.parent.location.href)
-        } else {
-            return new URL(window.location.href)
+    function init () {
+        const processedConfig = processConfig($CONTENT_SCOPE$, $USER_UNPROTECTED_DOMAINS$, $USER_PREFERENCES$);
+        if (processedConfig.site.allowlisted) {
+            return
         }
-    } catch (error) {
-        return new URL(location.href)
-    }
-}
 
-function isUnprotectedDomain (topLevelUrl, featureList) {
-    let unprotectedDomain = false
-    const domainParts = topLevelUrl && topLevelUrl.host ? topLevelUrl.host.split('.') : []
+        contentScopeFeatures.load();
 
-    // walk up the domain to see if it's unprotected
-    while (domainParts.length > 1 && !unprotectedDomain) {
-        const partialDomain = domainParts.join('.')
+        contentScopeFeatures.init(processedConfig);
 
-        unprotectedDomain = featureList.filter(domain => domain.domain === partialDomain).length > 0
-
-        domainParts.shift()
+        // Not supported:
+        // contentScopeFeatures.update(message)
     }
 
-    return unprotectedDomain
-}
+    init();
 
-function processConfig (data, userList, preferences) {
-    const topLevelUrl = getTopLevelURL()
-    const allowlisted = userList.filter(domain => domain === topLevelUrl.host).length > 0
-    const enabledFeatures = Object.keys(data.features).filter((featureName) => {
-        const feature = data.features[featureName]
-        return feature.state === 'enabled' && !isUnprotectedDomain(topLevelUrl, feature.exceptions)
-    })
-    const isBroken = isUnprotectedDomain(topLevelUrl, data.unprotectedTemporary)
-    preferences.site = {
-        domain: topLevelUrl.hostname,
-        isBroken,
-        allowlisted,
-        enabledFeatures
-    }
-    // TODO
-    preferences.cookie = {}
-    return preferences
-}
-
-function init () {
-    const processedConfig = processConfig($CONTENT_SCOPE$, $USER_UNPROTECTED_DOMAINS$, $USER_PREFERENCES$)
-    if (processedConfig.site.allowlisted) {
-        return
-    }
-
-    contentScopeFeatures.load()
-
-    contentScopeFeatures.init(processedConfig)
-
-    // Not supported:
-    // contentScopeFeatures.update(message)
-}
-
-init()
+})();
 
--- build/integration/contentScope.js
+++ build/integration/contentScope.js
@@ -1,4 +1,7 @@
-var contentScopeFeatures = (function (exports) {
+(function () {
+    'use strict';
+
+    var contentScopeFeatures = (function (exports) {
   'use strict';
 
   const sjcl = (() => {
@@ -2852,73 +2855,75 @@ var contentScopeFeatures = (function (exports) {
 })({});
 
 
-function getTopLevelURL () {
-    try {
-        // FROM: https://stackoverflow.com/a/7739035/73479
-        // FIX: Better capturing of top level URL so that trackers in embedded documents are not considered first party
-        if (window.location !== window.parent.location) {
-            return new URL(window.location.href !== 'about:blank' ? document.referrer : window.parent.location.href)
-        } else {
-            return new URL(window.location.href)
-        }
-    } catch (error) {
-        return new URL(location.href)
-    }
-}
-
-function generateConfig (data, userList) {
-    const topLevelUrl = getTopLevelURL()
-    return {
-        debug: false,
-        sessionKey: 'randomVal',
-        site: {
-            domain: topLevelUrl.hostname,
-            isBroken: false,
-            allowlisted: false,
-            enabledFeatures: [
-                'fingerprintingCanvas',
-                'fingerprintingScreenSize',
-                'navigatorInterface'
-            ]
+    function getTopLevelURL () {
+        try {
+            // FROM: https://stackoverflow.com/a/7739035/73479
+            // FIX: Better capturing of top level URL so that trackers in embedded documents are not considered first party
+            if (window.location !== window.parent.location) {
+                return new URL(window.location.href !== 'about:blank' ? document.referrer : window.parent.location.href)
+            } else {
+                return new URL(window.location.href)
+            }
+        } catch (error) {
+            return new URL(location.href)
         }
     }
-}
 
-async function init () {
-    const topLevelUrl = getTopLevelURL()
-    const processedConfig = generateConfig()
-    await contentScopeFeatures.load()
-
-    // mark this phase as loaded
-    setStatus('loaded')
-
-    if (!topLevelUrl.searchParams.has('wait-for-init-args')) {
-        await contentScopeFeatures.init(processedConfig)
-        setStatus('initialized')
-        return
+    function generateConfig (data, userList) {
+        const topLevelUrl = getTopLevelURL();
+        return {
+            debug: false,
+            sessionKey: 'randomVal',
+            site: {
+                domain: topLevelUrl.hostname,
+                isBroken: false,
+                allowlisted: false,
+                enabledFeatures: [
+                    'fingerprintingCanvas',
+                    'fingerprintingScreenSize',
+                    'navigatorInterface'
+                ]
+            }
+        }
     }
 
-    // Wait for a message containing additional config
-    document.addEventListener('content-scope-init-args', async (evt) => {
-        const merged = {
-            ...processedConfig,
-            ...evt.detail
+    async function init () {
+        const topLevelUrl = getTopLevelURL();
+        const processedConfig = generateConfig();
+        await contentScopeFeatures.load();
+
+        // mark this phase as loaded
+        setStatus('loaded');
+
+        if (!topLevelUrl.searchParams.has('wait-for-init-args')) {
+            await contentScopeFeatures.init(processedConfig);
+            setStatus('initialized');
+            return
         }
 
-        // init features
-        await contentScopeFeatures.init(merged)
+        // Wait for a message containing additional config
+        document.addEventListener('content-scope-init-args', async (evt) => {
+            const merged = {
+                ...processedConfig,
+                ...evt.detail
+            };
 
-        // set status to initialized so that tests can resume
-        setStatus('initialized')
-    }, { once: true })
-}
+            // init features
+            await contentScopeFeatures.init(merged);
 
-/**
- * @param {"loaded" | "initialized"} status
- */
-function setStatus (status) {
-    window.__content_scope_status = status
-}
+            // set status to initialized so that tests can resume
+            setStatus('initialized');
+        }, { once: true });
+    }
 
-init()
+    /**
+     * @param {"loaded" | "initialized"} status
+     */
+    function setStatus (status) {
+        window.__content_scope_status = status;
+    }
+
+    init();
+
+})();
 
