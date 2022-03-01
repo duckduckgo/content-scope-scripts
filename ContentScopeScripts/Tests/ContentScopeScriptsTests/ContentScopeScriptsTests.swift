4cd893de49677c31b8e296c5d478b6bc72feea24
Jonathan Kingston <jkingston@duckduckgo.com>
2022-03-01 23:48:14 +0000

    Making a test package

---
+++ ContentScopeScripts/Tests/ContentScopeScriptsTests/ContentScopeScriptsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ContentScopeScripts
+
+final class ContentScopeScriptsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        //XCTAssertEqual(ContentScopeScripts().text, "Hello, World!")
+    }
+}
