f5097849f913be50d7750551067dd3d964f726ab
Jonathan Kingston <jkingston@duckduckgo.com>
2022-06-10 15:15:42 +0100

    Revert "Disable JS blocking before policy loads" (#94)
    * Revert "Disable JS blocking before policy loads (#87)"
    
    This reverts commit 0250282b107afa0f1f9d1e1ca8c1db8345d89e66.
    
    * Fix dependency hell

--- build/apple/contentScope.js
+++ build/apple/contentScope.js
@@ -1300,8 +1300,8 @@
       isFrame: isBeingFramed(),
       isTracker: false,
       shouldBlock: !protectionExempted,
-      shouldBlockTrackerCookie: false,
-      shouldBlockNonTrackerCookie: false,
+      shouldBlockTrackerCookie: true,
+      shouldBlockNonTrackerCookie: true,
       isThirdParty: isThirdParty(),
       tabRegisteredDomain: tabOrigin,
       policy: {
