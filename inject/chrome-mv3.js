ce8ed92e9a82b93b1bbc0f5400bef4ed753b9704
Dave Vandyke <kzar@kzar.co.uk>
2022-09-02 11:23:30 +0100

    Add Chrome MV3 build target
    Add a Chrome MV3 build target, that produces a main-world content
    script ready for injection. Since there is not yet a way to pass
    arguments to chrome.scripting.registerContentScript[1] registered
    content scripts, generate a secret in the main-world (before the
    website's own scripts have a chance to run) and emit that as an event
    for our content script. That way, the main-world script still has a
    way to share a secret with the content script for messaging.
    
    Note: The registerContentScript does not yet allow about:blank frames
          to be targeted[2].
    
    1 - https://developer.chrome.com/docs/extensions/reference/scripting/#method-registerContentScripts
    2 - https://crbug.com/1360392

---
+++ inject/chrome-mv3.js
@@ -0,0 +1,24 @@
+/* global contentScopeFeatures */
+
+const secret = window.crypto.randomUUID()
+
+contentScopeFeatures.load()
+
+window.addEventListener(secret, ({ detail: message }) => {
+    if (!message) return
+
+    switch (message.type) {
+    case 'update':
+        contentScopeFeatures.update(message)
+        break
+    case 'register':
+        if (message.argumentsObject) {
+            contentScopeFeatures.init(message.argumentsObject)
+        }
+        break
+    }
+})
+
+window.dispatchEvent(new CustomEvent('ddg-secret', {
+    detail: secret
+}))
