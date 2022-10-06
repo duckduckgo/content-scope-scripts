22641057fdaf9db84de0e708b022d7c4717a8e85
Jonathan Kingston <jkingston@duckduckgo.com>
2022-10-06 13:51:21 +0100

    Fix Catalina not supporting WebGL2

--- Sources/ContentScopeScripts/dist/contentScope.js
+++ Sources/ContentScopeScripts/dist/contentScope.js
@@ -3631,9 +3631,11 @@
               'drawArrays'
           ];
           const glContexts = [
-              WebGL2RenderingContext,
               WebGLRenderingContext
           ];
+          if ('WebGL2RenderingContext' in globalThis) {
+              glContexts.push(WebGL2RenderingContext);
+          }
           for (const context of glContexts) {
               for (const methodName of unsafeGlMethods) {
                   // Some methods are browser specific
