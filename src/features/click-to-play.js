470dba7ec5927c066505e2fb434c2ae9a18344ee
francielefaccin <franfaccin@duck.com>
2022-12-02 15:31:42 +0000

    Comment out Youtube embedded configuration while feature is not released

--- src/features/click-to-play.js
+++ src/features/click-to-play.js
@@ -946,64 +946,64 @@ const config = {
                 }
             }
         }
-    },
-    Youtube: {
-        elementData: {
-            'YouTube embedded video': {
-                selectors: [
-                    "iframe[src*='://youtube.com/embed']",
-                    "iframe[src*='://youtube-nocookie.com/embed']",
-                    "iframe[src*='://www.youtube.com/embed']",
-                    "iframe[src*='://www.youtube-nocookie.com/embed']",
-                    "iframe[data-src*='://youtube.com/embed']",
-                    "iframe[data-src*='://youtube-nocookie.com/embed']",
-                    "iframe[data-src*='://www.youtube.com/embed']",
-                    "iframe[data-src*='://www.youtube-nocookie.com/embed']"
-                ],
-                replaceSettings: {
-                    type: 'youtube-video',
-                    buttonText: 'Unblock video',
-                    infoTitle: 'DuckDuckGo blocked this YouTube video to prevent Google from tracking you',
-                    infoText: 'We blocked Google (which owns YouTube) from tracking you when the page loaded. If you unblock this video, Google will know your activity.',
-                    simpleInfoText: 'We blocked Google (which owns YouTube) from tracking you when the page loaded. If you unblock this video, Google will know your activity.',
-                    previewToggleText: 'Previews disabled for additional privacy',
-                    placeholder: {
-                        previewToggleEnabledText: 'Previews enabled',
-                        previewInfoText: 'Turn previews off for additional privacy from DuckDuckGo.',
-                        videoPlayIcon: {
-                            lightMode: videoPlayLight,
-                            darkMode: videoPlayDark
-                        }
-                    }
-                },
-                clickAction: {
-                    type: 'youtube-video'
-                }
-            },
-            'YouTube embedded subscription button': {
-                selectors: [
-                    "iframe[src*='://youtube.com/subscribe_embed']",
-                    "iframe[src*='://youtube-nocookie.com/subscribe_embed']",
-                    "iframe[src*='://www.youtube.com/subscribe_embed']",
-                    "iframe[src*='://www.youtube-nocookie.com/subscribe_embed']",
-                    "iframe[data-src*='://youtube.com/subscribe_embed']",
-                    "iframe[data-src*='://youtube-nocookie.com/subscribe_embed']",
-                    "iframe[data-src*='://www.youtube.com/subscribe_embed']",
-                    "iframe[data-src*='://www.youtube-nocookie.com/subscribe_embed']"
-                ],
-                replaceSettings: {
-                    type: 'blank'
-                }
-            }
-        },
-        informationalModal: {
-            icon: blockedYTVideo,
-            messageTitle: 'Enable YouTube previews and reduce privacy?',
-            messageBody: 'Showing previews will allow Google (which owns YouTube) to see some of your device’s information, but is still more private than playing the video.',
-            confirmButtonText: 'Enable Previews',
-            rejectButtonText: 'No Thanks'
-        }
     }
+    // Youtube: {
+    //     elementData: {
+    //         'YouTube embedded video': {
+    //             selectors: [
+    //                 "iframe[src*='://youtube.com/embed']",
+    //                 "iframe[src*='://youtube-nocookie.com/embed']",
+    //                 "iframe[src*='://www.youtube.com/embed']",
+    //                 "iframe[src*='://www.youtube-nocookie.com/embed']",
+    //                 "iframe[data-src*='://youtube.com/embed']",
+    //                 "iframe[data-src*='://youtube-nocookie.com/embed']",
+    //                 "iframe[data-src*='://www.youtube.com/embed']",
+    //                 "iframe[data-src*='://www.youtube-nocookie.com/embed']"
+    //             ],
+    //             replaceSettings: {
+    //                 type: 'youtube-video',
+    //                 buttonText: 'Unblock video',
+    //                 infoTitle: 'DuckDuckGo blocked this YouTube video to prevent Google from tracking you',
+    //                 infoText: 'We blocked Google (which owns YouTube) from tracking you when the page loaded. If you unblock this video, Google will know your activity.',
+    //                 simpleInfoText: 'We blocked Google (which owns YouTube) from tracking you when the page loaded. If you unblock this video, Google will know your activity.',
+    //                 previewToggleText: 'Previews disabled for additional privacy',
+    //                 placeholder: {
+    //                     previewToggleEnabledText: 'Previews enabled',
+    //                     previewInfoText: 'Turn previews off for additional privacy from DuckDuckGo.',
+    //                     videoPlayIcon: {
+    //                         lightMode: videoPlayLight,
+    //                         darkMode: videoPlayDark
+    //                     }
+    //                 }
+    //             },
+    //             clickAction: {
+    //                 type: 'youtube-video'
+    //             }
+    //         },
+    //         'YouTube embedded subscription button': {
+    //             selectors: [
+    //                 "iframe[src*='://youtube.com/subscribe_embed']",
+    //                 "iframe[src*='://youtube-nocookie.com/subscribe_embed']",
+    //                 "iframe[src*='://www.youtube.com/subscribe_embed']",
+    //                 "iframe[src*='://www.youtube-nocookie.com/subscribe_embed']",
+    //                 "iframe[data-src*='://youtube.com/subscribe_embed']",
+    //                 "iframe[data-src*='://youtube-nocookie.com/subscribe_embed']",
+    //                 "iframe[data-src*='://www.youtube.com/subscribe_embed']",
+    //                 "iframe[data-src*='://www.youtube-nocookie.com/subscribe_embed']"
+    //             ],
+    //             replaceSettings: {
+    //                 type: 'blank'
+    //             }
+    //         }
+    //     },
+    //     informationalModal: {
+    //         icon: blockedYTVideo,
+    //         messageTitle: 'Enable YouTube previews and reduce privacy?',
+    //         messageBody: 'Showing previews will allow Google (which owns YouTube) to see some of your device’s information, but is still more private than playing the video.',
+    //         confirmButtonText: 'Enable Previews',
+    //         rejectButtonText: 'No Thanks'
+    //     }
+    // }
 }
 
 /*********************************************************
