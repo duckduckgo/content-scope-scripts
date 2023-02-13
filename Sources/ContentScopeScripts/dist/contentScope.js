7357fe17fa9ad458e0f930fec7d239c887315f40
Balázs Szántó <bszanto@duckduckgo.com>
2023-02-13 09:00:23 +0100

    add support for clearing all geolocation watch (#252)
    task: https://app.asana.com/0/1201048563534612/1203514593845595/f
    cc: @q71114 @jonathanKingston 
    
    It's a windows only change, which adds support for clearing all registered geolocation watches.

--- Sources/ContentScopeScripts/dist/contentScope.js
+++ Sources/ContentScopeScripts/dist/contentScope.js
@@ -6951,6 +6951,14 @@
           }
       }
 
+      function stopTracks (streamTracks) {
+          streamTracks?.forEach(track => track.stop());
+      }
+
+      function clearAllGeolocationWatch () {
+          watchedPositions.forEach(id => navigator.geolocation.clearWatch(id));
+      }
+
       function pause (permission) {
           const streamTracks = getTracks(permission);
           streamTracks?.forEach(track => {
@@ -6966,8 +6974,17 @@
       }
 
       function stop (permission) {
-          const streamTracks = getTracks(permission);
-          streamTracks?.forEach(track => track.stop());
+          switch (permission) {
+          case Permission.Camera:
+              stopTracks(videoTracks);
+              break
+          case Permission.Microphone:
+              stopTracks(audioTracks);
+              break
+          case Permission.Geolocation:
+              clearAllGeolocationWatch();
+              break
+          }
       }
 
       function monitorTrack (track) {
