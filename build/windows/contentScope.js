40aa9aad8a9ca7e8438626a41529bb0d06d08cb4
Jonathan Kingston <jkingston@duckduckgo.com>
2023-02-20 07:49:35 +0000

    Runtime check fix tag name (#261)

--- build/windows/contentScope.js
+++ build/windows/contentScope.js
@@ -6920,7 +6920,7 @@
       }
 
       toString () {
-          const interfaceName = this._tagName.charAt(0).toUpperCase() + this._tagName.slice(1);
+          const interfaceName = this.#tagName.charAt(0).toUpperCase() + this.#tagName.slice(1);
           return `[object HTML${interfaceName}Element]`
       }
 
