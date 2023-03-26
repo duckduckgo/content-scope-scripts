c24cff18546ac27a65a45f5497875981054fcdb6
Shane Osbourne <sosbourne@duckduckgo.com>
2023-03-26 12:21:18 +0100

    PR feedback

--- unit-test/verify-artifacts.js
+++ unit-test/verify-artifacts.js
@@ -5,28 +5,30 @@ import { statSync } from 'node:fs'
 const ROOT = new URL('..', import.meta.url).pathname
 const BUILD = join(ROOT, 'build')
 const APPLE_BUILD = join(ROOT, 'Sources/ContentScopeScripts/dist')
+const CSS_OUTPUT_SIZE = 512000
+const CSS_OUTPUT_SIZE_CHROME = CSS_OUTPUT_SIZE * 1.4 // 40% larger for Chrome MV2 due to base64 encoding
 
 const checks = {
     android: [
-        { kind: 'maxFileSize', value: 512000, path: join(BUILD, 'android/contentScope.js') }
+        { kind: 'maxFileSize', value: CSS_OUTPUT_SIZE, path: join(BUILD, 'android/contentScope.js') }
     ],
     chrome: [
-        { kind: 'maxFileSize', value: 716800, path: join(BUILD, 'chrome/inject.js') }
+        { kind: 'maxFileSize', value: CSS_OUTPUT_SIZE_CHROME, path: join(BUILD, 'chrome/inject.js') }
     ],
     'chrome-mv3': [
-        { kind: 'maxFileSize', value: 512000, path: join(BUILD, 'chrome-mv3/inject.js') }
+        { kind: 'maxFileSize', value: CSS_OUTPUT_SIZE, path: join(BUILD, 'chrome-mv3/inject.js') }
     ],
     firefox: [
-        { kind: 'maxFileSize', value: 512000, path: join(BUILD, 'firefox/inject.js') }
+        { kind: 'maxFileSize', value: CSS_OUTPUT_SIZE, path: join(BUILD, 'firefox/inject.js') }
     ],
     integration: [
-        { kind: 'maxFileSize', value: 512000, path: join(BUILD, 'integration/contentScope.js') }
+        { kind: 'maxFileSize', value: CSS_OUTPUT_SIZE, path: join(BUILD, 'integration/contentScope.js') }
     ],
     windows: [
-        { kind: 'maxFileSize', value: 512000, path: join(BUILD, 'windows/contentScope.js') }
+        { kind: 'maxFileSize', value: CSS_OUTPUT_SIZE, path: join(BUILD, 'windows/contentScope.js') }
     ],
     apple: [
-        { kind: 'maxFileSize', value: 512000, path: join(APPLE_BUILD, 'contentScope.js') }
+        { kind: 'maxFileSize', value: CSS_OUTPUT_SIZE, path: join(APPLE_BUILD, 'contentScope.js') }
     ]
 }
 
