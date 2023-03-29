589b5846f85910ed91f5920bd7c26fd897ad0dfd
Jonathan Kingston <jkingston@duckduckgo.com>
2023-03-29 08:22:18 +0100

    Move to using a class for consistency improvements (#345)
    * Move to using a class for consistency improvements
    
    * Remove all args from init where a class method exists for it
    
    * Make more uniform class calling
    
    * PR feedback

--- src/features/fingerprinting-battery.js
+++ src/features/fingerprinting-battery.js
@@ -1,3 +1,4 @@
+import ContentFeature from '../content-feature'
 import { defineProperty } from '../utils'
 
 /**
@@ -5,28 +6,30 @@ import { defineProperty } from '../utils'
  * It will return the values defined in the getBattery function to the client,
  * as well as prevent any script from listening to events.
  */
-export function init (args) {
-    // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
-    if (globalThis.navigator.getBattery) {
-        const BatteryManager = globalThis.BatteryManager
+export default class FingerprintingBattery extends ContentFeature {
+    init () {
+        // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
+        if (globalThis.navigator.getBattery) {
+            const BatteryManager = globalThis.BatteryManager
 
-        const spoofedValues = {
-            charging: true,
-            chargingTime: 0,
-            dischargingTime: Infinity,
-            level: 1
-        }
-        const eventProperties = ['onchargingchange', 'onchargingtimechange', 'ondischargingtimechange', 'onlevelchange']
+            const spoofedValues = {
+                charging: true,
+                chargingTime: 0,
+                dischargingTime: Infinity,
+                level: 1
+            }
+            const eventProperties = ['onchargingchange', 'onchargingtimechange', 'ondischargingtimechange', 'onlevelchange']
 
-        for (const [prop, val] of Object.entries(spoofedValues)) {
-            try {
-                defineProperty(BatteryManager.prototype, prop, { get: () => val })
-            } catch (e) { }
-        }
-        for (const eventProp of eventProperties) {
-            try {
-                defineProperty(BatteryManager.prototype, eventProp, { get: () => null })
-            } catch (e) { }
+            for (const [prop, val] of Object.entries(spoofedValues)) {
+                try {
+                    defineProperty(BatteryManager.prototype, prop, { get: () => val })
+                } catch (e) { }
+            }
+            for (const eventProp of eventProperties) {
+                try {
+                    defineProperty(BatteryManager.prototype, eventProp, { get: () => null })
+                } catch (e) { }
+            }
         }
     }
 }
--- src/features/fingerprinting-hardware.js
+++ src/features/fingerprinting-hardware.js
@@ -1,25 +1,27 @@
-import { overrideProperty, getFeatureAttr } from '../utils'
-const featureName = 'fingerprinting-hardware'
+import { overrideProperty } from '../utils'
+import ContentFeature from '../content-feature'
 
-export function init (args) {
-    const Navigator = globalThis.Navigator
-    const navigator = globalThis.navigator
+export default class FingerprintingHardware extends ContentFeature {
+    init () {
+        const Navigator = globalThis.Navigator
+        const navigator = globalThis.navigator
 
-    overrideProperty('keyboard', {
-        object: Navigator.prototype,
-        // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
-        origValue: navigator.keyboard,
-        targetValue: getFeatureAttr(featureName, args, 'keyboard')
-    })
-    overrideProperty('hardwareConcurrency', {
-        object: Navigator.prototype,
-        origValue: navigator.hardwareConcurrency,
-        targetValue: getFeatureAttr(featureName, args, 'hardwareConcurrency', 2)
-    })
-    overrideProperty('deviceMemory', {
-        object: Navigator.prototype,
-        // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
-        origValue: navigator.deviceMemory,
-        targetValue: getFeatureAttr(featureName, args, 'deviceMemory', 8)
-    })
+        overrideProperty('keyboard', {
+            object: Navigator.prototype,
+            // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
+            origValue: navigator.keyboard,
+            targetValue: this.getFeatureAttr('keyboard')
+        })
+        overrideProperty('hardwareConcurrency', {
+            object: Navigator.prototype,
+            origValue: navigator.hardwareConcurrency,
+            targetValue: this.getFeatureAttr('hardwareConcurrency', 2)
+        })
+        overrideProperty('deviceMemory', {
+            object: Navigator.prototype,
+            // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
+            origValue: navigator.deviceMemory,
+            targetValue: this.getFeatureAttr('deviceMemory', 8)
+        })
+    }
 }
