605528485a50e3c50db4d1af4e7ac1d28446a29e
Jonathan Kingston <jkingston@duckduckgo.com>
2023-03-31 11:30:04 +0100

    Move utils feature code into class

--- src/content-feature.js
+++ src/content-feature.js
@@ -1,4 +1,4 @@
-import { getFeatureSetting, getFeatureSettingEnabled, matchHostname, getFeatureAttr } from './utils'
+import { camelcase, matchHostname, processAttr } from './utils'
 
 export default class ContentFeature {
     constructor (featureName) {
@@ -18,25 +18,35 @@ export default class ContentFeature {
     }
 
     /**
+     * Get the value of a config setting.
+     * If the value is not set, return the default value.
+     * If the value is not an object, return the value.
+     * If the value is an object, check its type property.
      * @param {string} attrName
-     * @param {any} defaultValue
+     * @param {any} defaultValue - The default value to use if the config setting is not set
+     * @returns The value of the config setting or the default value
      */
     getFeatureAttr (attrName, defaultValue) {
-        return getFeatureAttr(this.name, this._args, attrName, defaultValue)
+        const configSetting = this.getFeatureSetting(attrName)
+        return processAttr(configSetting, defaultValue)
     }
 
     /**
      * @param {string} featureKeyName
+     * @returns {any}
      */
     getFeatureSetting (featureKeyName) {
-        return getFeatureSetting(this.name, this._args, featureKeyName)
+        const camelFeatureName = camelcase(this.name)
+        return this._args.featureSettings?.[camelFeatureName]?.[featureKeyName]
     }
 
     /**
      * @param {string} featureKeyName
+     * @returns {boolean}
      */
     getFeatureSettingEnabled (featureKeyName) {
-        return getFeatureSettingEnabled(this.name, this._args, featureKeyName)
+        const result = this.getFeatureSetting(featureKeyName)
+        return result === 'enabled'
     }
 
     /**
