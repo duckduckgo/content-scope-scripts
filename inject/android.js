cf781dc1a2b49e838da926fde7f1b99934c68f6b
Josh Leibstein <joshliebe@gmail.com>
2023-03-31 12:31:15 +0100

    Add Android messaging integration

--- inject/android.js
+++ inject/android.js
@@ -2,9 +2,21 @@
  * @module Android integration
  * @category Content Scope Scripts Integrations
  */
-import { load, init } from '../src/content-scope-features.js'
+import { load, init, update } from '../src/content-scope-features.js'
 import { processConfig, isGloballyDisabled } from './../src/utils'
 
+const allowedMessages = [
+    'getClickToLoadState',
+    'getYouTubeVideoDetails',
+    'setYoutubePreviewsEnabled',
+    'unblockClickToLoadContent',
+    'updateYouTubeCTLAddedFlag'
+]
+const messageSecret = randomString()
+function randomString () {
+    const num = crypto.getRandomValues(new Uint32Array(1))[0] / 2 ** 32
+    return num.toString().replace('0.', '')
+}
 function initCode () {
     // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
     const processedConfig = processConfig($CONTENT_SCOPE$, $USER_UNPROTECTED_DOMAINS$, $USER_PREFERENCES$)
@@ -18,8 +30,29 @@ function initCode () {
 
     init(processedConfig)
 
-    // Not supported:
-    // update(message)
+    window.clickToLoadMessageCallback = (message) => {
+        update(message)
+    }
+
+    window.addEventListener('sendMessageProxy' + messageSecret, event => {
+        event.stopImmediatePropagation()
+
+        if (!(event instanceof CustomEvent) || !event?.detail) {
+            return console.warn('no details in sendMessage proxy', event)
+        }
+
+        const messageType = event.detail?.messageType
+        if (!allowedMessages.includes(messageType)) {
+            return console.warn('Ignoring invalid sendMessage messageType', messageType)
+        }
+
+        const message = {
+            type: messageType,
+            options: event.detail?.options
+        }
+        const stringifiedArgs = JSON.stringify(message)
+        window.ClickToLoadAndroid.process(stringifiedArgs)
+    })
 }
 
 initCode()
