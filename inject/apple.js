6d035a6bb25475c846715bd22b90d707c91dd175
Shane Osbourne <sosbourne@duckduckgo.com>
2023-03-31 13:21:46 +0100

    added a copy/paste example

--- inject/apple.js
+++ inject/apple.js
@@ -2,8 +2,21 @@
  * @module Apple integration
  * @category Content Scope Scripts Integrations
  */
-import { load, init } from '../src/content-scope-features.js'
+import { load, init, update } from '../src/content-scope-features.js'
 import { processConfig, isGloballyDisabled } from './../src/utils'
+
+const allowedMessages = [
+    'getClickToLoadState',
+    'getYouTubeVideoDetails',
+    'setYoutubePreviewsEnabled',
+    'unblockClickToLoadContent',
+    'updateYouTubeCTLAddedFlag'
+]
+const messageSecret = randomString()
+function randomString () {
+    const num = crypto.getRandomValues(new Uint32Array(1))[0] / 2 ** 32
+    return num.toString().replace('0.', '')
+}
 
 function initCode () {
     // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
@@ -20,6 +33,33 @@ function initCode () {
 
     // Not supported:
     // update(message)
+
+    window.clickToLoadMessageCallback = (message) => {
+        update(message)
+    }
+
+    window.addEventListener('sendMessageProxy' + messageSecret, event => {
+        event.stopImmediatePropagation()
+
+        if (!(event instanceof CustomEvent) || !event?.detail) {
+            return console.warn('no details in sendMessage proxy', event)
+        }
+
+        const messageType = event.detail?.messageType
+        if (!allowedMessages.includes(messageType)) {
+            return console.warn('Ignoring invalid sendMessage messageType', messageType)
+        }
+
+        if (!(messageType in window.webkit.messageHandlers)) {
+            console.warn("missing handler", messageType)
+        }
+
+        try {
+            window.webkit.messageHandlers[messageType].postMessage(event.detail?.options)
+        } catch (e) {
+            console.error(e)
+        }
+    })
 }
 
 initCode()
