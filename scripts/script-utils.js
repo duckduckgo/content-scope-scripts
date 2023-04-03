ecc4db995acd8c01fd9db6b04c816a5fb83cf638
Shane Osbourne <shane.osbourne8@gmail.com>
2023-04-03 13:48:27 +0100

    fix: support building on windows (#374)
    * fix: support building on windows
    * support more than 1 output
    * skipping the example build
    * windows compatible script
    * cross-platform file path
    * don't change node requirement
    * trying run on windows
    * ignore a symlink
    * drop symlinks + define build inputs/outputs
    * remove inline json from build script
    * unit tests
    * remove pr-flow for now
    
    ---------
    
    Co-authored-by: Shane Osbourne <sosbourne@duckduckgo.com>
    Co-authored-by: Jonathan Kingston <jkingston@duckduckgo.com>

---
+++ scripts/script-utils.js
@@ -0,0 +1,56 @@
+import { dirname } from 'node:path'
+import { mkdirSync, writeFileSync } from 'node:fs'
+import minimist from 'minimist'
+
+/**
+ * A cross-platform 'mkdirp' + writing to disk
+ * @param {string[] | string} filepaths
+ * @param {string} content
+ */
+export function write (filepaths, content) {
+    for (const filepath of [].concat(filepaths)) {
+        try {
+            const pathWithoutFile = dirname(filepath)
+            mkdirSync(pathWithoutFile, { recursive: true })
+        } catch (e) {
+            // EEXIST is expected, for anything else re-throw
+            if (e.code !== 'EEXIST') {
+                throw e
+            }
+        }
+        writeFileSync(filepath, content)
+    }
+}
+
+/**
+ * Simple required args
+ * @param {string[]} args - the input
+ * @param {string[]} requiredFields - array of required keys
+ * @param {string} [help] - optional help text
+ */
+export function parseArgs (args, requiredFields, help = '') {
+    const parsedArgs = minimist(args)
+
+    for (const field of requiredFields) {
+        if (!(field in parsedArgs)) {
+            console.error(`Missing required argument: --${field}`)
+            if (help) console.log(help)
+            process.exit(1)
+        }
+    }
+
+    return parsedArgs
+}
+
+/**
+ * Cross-platform absolute directory path for modules.
+ *
+ * On windows, 'pathname' has a leading `/` which needs removing
+ */
+export function cwd (current) {
+    const pathname = new URL('.', current).pathname
+    if (process.platform === 'win32') {
+        return pathname.slice(1)
+    }
+    return pathname
+}
