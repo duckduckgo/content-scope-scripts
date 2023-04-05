f319dc60927f4ad7b09ef1b00a98f7964d98246e
Jonathan Kingston <jkingston@duckduckgo.com>
2023-04-05 01:04:44 +0100

    Clean indenting of generated code

--- src/features/runtime-checks/script-overload.js
+++ src/features/runtime-checks/script-overload.js
@@ -110,7 +110,7 @@ function stringifyScope (scope, scopePath) {
             }
         } else {
             output += `
-            let ${varOutName} = ${valToString(value)};
+            let ${varOutName} = ${JSON.stringify(value)};
             `
         }
     }
