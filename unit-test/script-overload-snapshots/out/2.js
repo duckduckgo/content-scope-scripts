ef4affacbd7b16913cedb1e5735ee528446e4ee6
Jonathan Kingston <jkingston@duckduckgo.com>
2023-04-05 11:16:37 +0100

    Add runtime code comment to indicate where to raise issues

--- unit-test/script-overload-snapshots/out/2.js
+++ unit-test/script-overload-snapshots/out/2.js
@@ -1,4 +1,9 @@
 (function (parentScope) {
+  /**
+  * DuckDuckGo Runtime Checks injected code.
+  * If you're reading this, you're probably trying to debug a site that is breaking due to our runtime checks.
+  * Please raise an issues on our GitHub repo: https://github.com/duckduckgo/content-scope-scripts/
+  */
   function constructProxy (scope, outputs) {
     if (Object.is(scope)) {
       // Should not happen, but just in case fail safely
