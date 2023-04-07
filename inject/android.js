d90095846663e6613f94706511201bbcf0129fc6
Marcos <marcosholgado@gmail.com>
2023-04-07 12:08:45 +0100

    Revert "Add Android messaging integration (#380)" (#394)
    This reverts commit 2aa976604471bc56192522c3de72754ad092bba4.

--- inject/android.js
+++ inject/android.js
@@ -2,17 +2,8 @@
  * @module Android integration
  * @category Content Scope Scripts Integrations
  */
-import { load, init, update } from '../src/content-scope-features.js'
+import { load, init } from '../src/content-scope-features.js'
 import { processConfig, isGloballyDisabled } from './../src/utils'
-
-const allowedMessages = [
-    'getClickToLoadState',
-    'getYouTubeVideoDetails',
-    'openShareFeedbackPage',
-    'setYoutubePreviewsEnabled',
-    'unblockClickToLoadContent',
-    'updateYouTubeCTLAddedFlag'
-]
 
 function initCode () {
     // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
@@ -25,47 +16,10 @@ function initCode () {
         platform: processedConfig.platform
     })
 
-    const messageSecret = processedConfig.messageSecret
-    // Receives messages from the platform
-    const messageCallback = processedConfig.messageCallback
-    // Sends messages to the platform
-    const messageInterface = processedConfig.messageInterface
-
-    const wrappedUpdate = ((providedSecret, ...args) => {
-        if (providedSecret === messageSecret) {
-            update(...args)
-        }
-    }).bind()
-
-    Object.defineProperty(window, messageCallback, {
-        value: wrappedUpdate
-    })
-
-    // @ts-ignore
-    const sendMessageToAndroid = window[messageInterface].process.bind(window[messageInterface])
-    delete window[messageInterface]
-
     init(processedConfig)
 
-    window.addEventListener('sendMessageProxy' + messageSecret, event => {
-        event.stopImmediatePropagation()
-
-        if (!(event instanceof CustomEvent) || !event?.detail) {
-            return console.warn('no details in sendMessage proxy', event)
-        }
-
-        const messageType = event.detail?.messageType
-        if (!allowedMessages.includes(messageType)) {
-            return console.warn('Ignoring invalid sendMessage messageType', messageType)
-        }
-
-        const message = {
-            type: messageType,
-            options: event.detail?.options
-        }
-        const stringifiedArgs = JSON.stringify(message)
-        sendMessageToAndroid(stringifiedArgs, messageSecret)
-    })
+    // Not supported:
+    // update(message)
 }
 
 initCode()
