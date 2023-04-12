b72ebbf08661712a8088ac956d83ec8aab68d8ef
Jonathan Kingston <jkingston@duckduckgo.com>
2023-04-12 14:22:19 +0100

    Add in exception handle debugging (#373)
    * Add in exception handle debugging
    
    * Rename message for exception

---
+++ src/features/exception-handler.js
@@ -0,0 +1,19 @@
+import { postDebugMessage } from '../utils.js'
+import ContentFeature from '../content-feature.js'
+
+export default class ExceptionHandler extends ContentFeature {
+    init () {
+        // Report to the debugger panel if an uncaught exception occurs
+        function handleUncaughtException (e) {
+            postDebugMessage('jsException', {
+                documentUrl: document.location.href,
+                message: e.message,
+                filename: e.filename,
+                lineno: e.lineno,
+                colno: e.colno,
+                stack: e.error.stack
+            })
+        }
+        globalThis.addEventListener('error', handleUncaughtException)
+    }
+}
