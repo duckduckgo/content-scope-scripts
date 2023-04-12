d648f34f46b74417e86c87e22552ff43e4b7ead1
Jonathan Kingston <jkingston@duckduckgo.com>
2023-04-12 13:42:32 +0100

    Rename message for exception

--- src/features/exception-handler.js
+++ src/features/exception-handler.js
@@ -5,7 +5,7 @@ export default class ExceptionHandler extends ContentFeature {
     init () {
         // Report to the debugger panel if an uncaught exception occurs
         function handleUncaughtException (e) {
-            postDebugMessage('error', {
+            postDebugMessage('jsException', {
                 documentUrl: document.location.href,
                 message: e.message,
                 filename: e.filename,
