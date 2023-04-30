c7fd5906a214bea5936067c41cf299440678fbfc
Jonathan Kingston <jkingston@duckduckgo.com>
2023-04-30 22:24:32 +0100

    Initial canvas test import

--- integration-test/test-pages.js
+++ integration-test/test-pages.js
@@ -26,7 +26,8 @@ describe('Test integration pages', () => {
             'runtime-checks/pages/basic-run.html': 'runtime-checks/config/basic-run.json',
             'runtime-checks/pages/filter-props.html': 'runtime-checks/config/filter-props.json',
             'runtime-checks/pages/shadow-dom.html': 'runtime-checks/config/shadow-dom.json',
-            'runtime-checks/pages/script-overload.html': 'runtime-checks/config/script-overload.json'
+            'runtime-checks/pages/script-overload.html': 'runtime-checks/config/script-overload.json',
+            'canvas/pages/canvas.html': 'canvas/config/canvas.json'
         }
         for (const pageName in pages) {
             const configName = pages[pageName]
