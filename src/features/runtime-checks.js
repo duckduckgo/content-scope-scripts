953f186e161f77fbeb666e1e1a803d60738ccc91
Jonathan Kingston <jkingston@duckduckgo.com>
2023-05-31 12:36:05 +0100

    Enable unused var lint check as an error (#545)

--- src/features/runtime-checks.js
+++ src/features/runtime-checks.js
@@ -471,6 +471,7 @@ function overrideCreateElement () {
     initialCreateElement = proxy._native
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function getTaintFromScope (scope, args) {
     try {
         scope = args.callee.caller
