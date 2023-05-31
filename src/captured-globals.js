7f4a69cebb8d36d4157e51375346fc947e0d90d8
Jonathan Kingston <jkingston@duckduckgo.com>
2023-05-31 17:37:14 +0100

    Change to throw away iframe for capturing globals

--- src/captured-globals.js
+++ src/captured-globals.js
@@ -1,16 +1,23 @@
+import { getInjectionElement } from './utils.js'
+
+let dummyWindow
+if ('document' in globalThis) {
+    const injectionElement = getInjectionElement()
+    // injectionElement is null in some playwright context tests
+    if (injectionElement) {
+        dummyWindow = document.createElement('iframe')
+        injectionElement.appendChild(dummyWindow)
+    }
+}
 // Capture prototype to prevent overloading
-function captureGlobals (object) {
-    const newGlobal = object
-    for (const key in object) {
-        const value = object[key]
-        newGlobal[key] = value
-    }
-    for (const key in object.prototype) {
-        const value = object.prototype[key]
-        newGlobal.prototype[key] = value
-    }
-    return newGlobal
+function captureGlobal (globalName) {
+    // if we were unable to create a dummy window, return the global
+    // this still has the advantage of preventing aliasing of the global through shawdowing
+    return dummyWindow?.contentWindow?.[globalName] || globalThis[globalName]
 }
 
-export const Set = captureGlobals(globalThis.Set)
-export const Reflect = captureGlobals(globalThis.Reflect)
+export const Set = captureGlobal('Set')
+export const Reflect = captureGlobal('Reflect')
+
+// Clean up the dummy window
+dummyWindow?.remove()
