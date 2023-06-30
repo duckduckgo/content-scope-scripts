1ba80d2eac1b32316fa657ce47cef6381ae6ca9e
Maxim Tsoy <maks.tsoy@gmail.com>
2023-06-30 12:02:38 +0200

    re-enable windows permission tests

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
             name: 'windows',
             testMatch: [
                 'integration-test/playwright/duckplayer.spec.js',
-                'integration-test/playwright/harmful-apis.spec.js'
+                'integration-test/playwright/harmful-apis.spec.js',
+                'integration-test/playwright/windows-permissions.spec.js'
             ],
             use: { injectName: 'windows', platform: 'windows' }
         },
