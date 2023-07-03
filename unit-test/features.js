1109a4639d9e0495d24fec85411c1df42b4ead7b
Shane Osbourne <sosbourne@duckduckgo.com>
2023-07-03 18:15:09 +0100

    remove `fit`

--- unit-test/features.js
+++ unit-test/features.js
@@ -1,7 +1,7 @@
 import { platformSupport } from '../src/features.js'
 
 describe('Features definition', () => {
-    fit('calls `webCompat` before `fingerPrintingScreenSize` https://app.asana.com/0/1177771139624306/1204944717262422/f', () => {
+    it('calls `webCompat` before `fingerPrintingScreenSize` https://app.asana.com/0/1177771139624306/1204944717262422/f', () => {
         // ensuring this order doesn't change, as it recently caused breakage
         expect(platformSupport.apple).toEqual([
             'webCompat',
