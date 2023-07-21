acc38de473f162e406d51c70ce0dcac75f58bbb6
Sam Macbeth <sammacbeth@users.noreply.github.com>
2023-07-21 15:36:41 +0200

    Handle when document.referrer is empty (#647)

--- src/features/referrer.js
+++ src/features/referrer.js
@@ -4,7 +4,7 @@ import { wrapProperty } from '../wrapper-utils'
 export default class Referrer extends ContentFeature {
     init () {
         // If the referer is a different host to the current one, trim it.
-        if (new URL(document.URL).hostname !== new URL(document.referrer).hostname) {
+        if (document.referrer && new URL(document.URL).hostname !== new URL(document.referrer).hostname) {
             // trim referrer to origin.
             const trimmedReferer = new URL(document.referrer).origin + '/'
             wrapProperty(Document.prototype, 'referrer', {
