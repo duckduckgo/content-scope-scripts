1e6dac4e1e4eb21f476752460b1842a3e08d17d2
Maxim Tsoy <maks.tsoy@gmail.com>
2023-07-26 14:43:46 +0200

    Remove unused standalone wrapper utils

--- src/wrapper-utils.js
+++ src/wrapper-utils.js
@@ -2,9 +2,7 @@
 // Tests don't define this variable so fallback to behave like chrome
 export const hasMozProxies = typeof mozProxies !== 'undefined' ? mozProxies : false
 const globalObj = typeof window === 'undefined' ? globalThis : window
-const getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor
 const functionToString = Function.prototype.toString
-const objectKeys = Object.keys
 
 /**
  * @deprecated use the ContentFeature method instead
@@ -76,84 +74,4 @@ export function wrapFunction (functionValue, realTarget) {
             return Reflect.apply(functionValue, thisArg, argumentsList)
         }
     })
-}
-
-/**
- * Wrap a get/set or value property descriptor. Only for data properties. For methods, use wrapMethod(). For constructors, use wrapConstructor().
- * @param {any} object - object whose property we are wrapping (most commonly a prototype)
- * @param {string} propertyName
- * @param {Partial<PropertyDescriptor>} descriptor
- * @returns {PropertyDescriptor|undefined} original property descriptor, or undefined if it's not found
- * @deprecated use the ContentFeature method instead
- */
-export function wrapProperty (object, propertyName, descriptor) {
-    if (!object) {
-        return
-    }
-    if (hasMozProxies) {
-        object = object.wrappedJSObject || object
-    }
-
-    const origDescriptor = getOwnPropertyDescriptor(object, propertyName)
-    if (!origDescriptor) {
-        // this happens if the property is not implemented in the browser
-        return
-    }
-
-    if (('value' in origDescriptor && 'value' in descriptor) ||
-        ('get' in origDescriptor && 'get' in descriptor) ||
-        ('set' in origDescriptor && 'set' in descriptor)
-    ) {
-        wrapToString(descriptor.value, origDescriptor.value)
-        wrapToString(descriptor.get, origDescriptor.get)
-        wrapToString(descriptor.set, origDescriptor.set)
-
-        defineProperty(object, propertyName, {
-            ...origDescriptor,
-            ...descriptor
-        })
-        return origDescriptor
-    } else {
-        // if the property is defined with get/set it must be wrapped with a get/set. If it's defined with a `value`, it must be wrapped with a `value`
-        throw new Error(`Property descriptor for ${propertyName} may only include the following keys: ${objectKeys(origDescriptor)}`)
-    }
-}
-
-/**
- * Wrap a method descriptor. Only for function properties. For data properties, use wrapProperty(). For constructors, use wrapConstructor().
- * @param {any} object - object whose property we are wrapping (most commonly a prototype)
- * @param {string} propertyName
- * @param {(originalFn, ...args) => any } wrapperFn - wrapper function receives the original function as the first argument
- * @returns {PropertyDescriptor|undefined} original property descriptor, or undefined if it's not found
- * @deprecated use the ContentFeature method instead
- */
-export function wrapMethod (object, propertyName, wrapperFn) {
-    if (!object) {
-        return
-    }
-    if (hasMozProxies) {
-        object = object.wrappedJSObject || object
-    }
-    const origDescriptor = getOwnPropertyDescriptor(object, propertyName)
-    if (!origDescriptor) {
-        // this happens if the property is not implemented in the browser
-        return
-    }
-
-    const origFn = origDescriptor.value
-    if (!origFn || typeof origFn !== 'function') {
-        // method properties are expected to be defined with a `value`
-        throw new Error(`Property ${propertyName} does not look like a method`)
-    }
-
-    const newFn = function () {
-        return wrapperFn.call(this, origFn, ...arguments)
-    }
-    wrapToString(newFn, origFn)
-
-    defineProperty(object, propertyName, {
-        ...origDescriptor,
-        value: newFn
-    })
-    return origDescriptor
 }
