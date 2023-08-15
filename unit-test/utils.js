5f51005d8dc25d4b9b6c3df527bf8141b2c7d20a
Jonathan Kingston <jkingston@duckduckgo.com>
2023-08-15 12:54:01 +0000

    Enable max debug count (#667)
    * Enable max debug count
    
    * Always check debug before sending debug notices
    
    * Add test for debugging only
    
    * Use feature as the key instead for rate limiting and up the limit instead
    
    * Fix test

--- unit-test/utils.js
+++ unit-test/utils.js
@@ -1,4 +1,4 @@
-import { matchHostname, processConfig, satisfiesMinVersion } from '../src/utils.js'
+import { matchHostname, postDebugMessage, initStringExemptionLists, processConfig, satisfiesMinVersion } from '../src/utils.js'
 import polyfillProcessGlobals from './helpers/pollyfil-for-process-globals.js'
 
 polyfillProcessGlobals()
@@ -211,5 +211,37 @@ describe('Helpers checks', () => {
                 expect(satisfiesMinVersion(versionString, extensionVersionString)).toEqual(expectedOutcome)
             })
         }
+    })
+
+    describe('utils.postDebugMessage', () => {
+        const counters = new Map()
+        globalThis.postMessage = message => {
+            counters.set(message.action, (counters.get(message.action) || 0) + 1)
+        }
+        initStringExemptionLists({ debug: false })
+        for (let i = 0; i < 10; i++) {
+            postDebugMessage('testa', { ding: 1 })
+            postDebugMessage('testa', { ding: 2 })
+            postDebugMessage('testb', { boop: true })
+            postDebugMessage('testc', { boop: true }, true)
+        }
+        it('does not trigger post messages without debug flag', () => {
+            expect(counters.get('testa')).toEqual(undefined)
+            expect(counters.get('testb')).toEqual(undefined)
+            expect(counters.get('testc')).toEqual(10)
+        })
+
+        initStringExemptionLists({ debug: true })
+        for (let i = 0; i < 6000; i++) {
+            postDebugMessage('testd', { ding: 1 })
+            postDebugMessage('testd', { ding: 2 })
+            postDebugMessage('teste', { boop: true })
+            postDebugMessage('testf', { boop: true }, true)
+        }
+        it('posts messages', () => {
+            expect(counters.get('testd')).toEqual(5000)
+            expect(counters.get('teste')).toEqual(5000)
+            expect(counters.get('testf')).toEqual(5000)
+        })
     })
 })
