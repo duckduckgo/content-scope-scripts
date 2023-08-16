ace21fa4fedccb0e83daa632299a20592b9c82f7
Jonathan Kingston <jkingston@duckduckgo.com>
2023-08-16 12:21:10 +0100

    Add debug check back in to prevent stack trace calls

--- src/utils.js
+++ src/utils.js
@@ -459,14 +459,17 @@ export class DDGProxy {
                 const isTainted = hasTaintedMethod(scope)
                 isExempt = !isTainted
             }
-            postDebugMessage(this.camelFeatureName, {
-                isProxy: true,
-                action: isExempt ? 'ignore' : 'restrict',
-                kind: this.property,
-                documentUrl: document.location.href,
-                stack: getStack(),
-                args: debugSerialize(args[2])
-            })
+            // Keep this here as getStack() is expensive
+            if (debug) {
+                postDebugMessage(this.camelFeatureName, {
+                    isProxy: true,
+                    action: isExempt ? 'ignore' : 'restrict',
+                    kind: this.property,
+                    documentUrl: document.location.href,
+                    stack: getStack(),
+                    args: debugSerialize(args[2])
+                })
+            }
             // The normal return value
             if (isExempt) {
                 return DDGReflect.apply(...args)
