b8311523600b821f23c7e734fdfb57f680408c42
Ivy Grey <ivymisc@posteo.net>
2023-11-01 17:35:46 -0700

    Run prettier

--- packages/special-pages/pages/onboarding/app/FirstPage.js
+++ packages/special-pages/pages/onboarding/app/FirstPage.js
@@ -1,23 +1,32 @@
-import { h, Fragment } from 'preact'
-import { useEffect, useState } from 'preact/hooks'
-import classNames from 'classnames'
-import styles from '../src/js/styles.module.css'
-import { Header } from './Header'
+import { h, Fragment } from "preact";
+import { useEffect, useState } from "preact/hooks";
+import classNames from "classnames";
+import styles from "../src/js/styles.module.css";
+import { Header } from "./Header";
 
 export function FirstPage({ onNextPage }) {
-    const [pageIndex, setPageIndex] = useState(0)
+  const [pageIndex, setPageIndex] = useState(0);
 
-    useEffect(() => {
-        setTimeout(() => setPageIndex(1), 2500)
-    }, [])
+  useEffect(() => {
+    setTimeout(() => setPageIndex(1), 2500);
+  }, []);
 
-    return <>
-        {pageIndex === 0 && <Header title="Welcome to DuckDuckGo!" />}
-        {pageIndex === 1 && <>
-            <Header title={"Tired of being tracked online?\nWe can help 💪"} />
-            <div className={styles.wrapper}>
-                <button className={classNames(styles.primary, styles.large)} onClick={() => onNextPage()}>Get Started</button>
-            </div>
-        </>}
+  return (
+    <>
+      {pageIndex === 0 && <Header title="Welcome to DuckDuckGo!" />}
+      {pageIndex === 1 && (
+        <>
+          <Header title={"Tired of being tracked online?\nWe can help 💪"} />
+          <div className={styles.wrapper}>
+            <button
+              className={classNames(styles.primary, styles.large)}
+              onClick={() => onNextPage()}
+            >
+              Get Started
+            </button>
+          </div>
+        </>
+      )}
     </>
+  );
 }
--- packages/special-pages/pages/onboarding/app/LastPage.js
+++ packages/special-pages/pages/onboarding/app/LastPage.js
@@ -1,17 +1,41 @@
-import { h, Fragment } from 'preact'
-import { useState } from 'preact/hooks'
-import classNames from 'classnames'
-import styles from '../src/js/styles.module.css'
-import { Header } from './Header'
+import { h, Fragment } from "preact";
+import { useState } from "preact/hooks";
+import classNames from "classnames";
+import styles from "../src/js/styles.module.css";
+import { Header } from "./Header";
 
-export function LastPage({ onNextPage, stepResults }) {
-    return <>
-        <Header title="done" />
+export function LastPage({ onNextPage, stepsPages, stepResults }) {
+  const enabledSteps = stepsPages
+    .reduce((arr, page) => {
+      arr = [...arr, ...page.steps];
+      return arr;
+    }, [])
+    .filter((step) => stepResults[step.id] === true);
 
-        <div className={styles.wrapper}>
+  return (
+    <>
+      <Header title="You're all set!" />
 
-            <button className={classNames(styles.primary, styles.large)} onClick={() => onNextPage()}>Next</button>
+      <div className={styles.wrapper}>
+        <h2>DuckDuckGo is customized for you and ready to go.</h2>
 
+        <ul>
+          {enabledSteps.map((step) => (
+            <li>{step.title}</li>
+          ))}
+        </ul>
+
+        <button
+          className={classNames(styles.primary, styles.large)}
+          onClick={() => onNextPage()}
+        >
+          Start Browsing
+        </button>
+
+        <div>
+          You can change your choices any time in <a href="#">Settings</a>.
         </div>
+      </div>
     </>
+  );
 }
