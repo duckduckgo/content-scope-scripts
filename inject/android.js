744d41eb9b726f6dabb722e05903be0526e5db3d
Jonathan Kingston <jkingston@duckduckgo.com>
2023-11-10 18:40:14 +0000

    Make messaging a relative path again (#828)

--- inject/android.js
+++ inject/android.js
@@ -5,7 +5,7 @@
 import { load, init } from '../src/content-scope-features.js'
 import { processConfig, isGloballyDisabled } from './../src/utils'
 import { isTrackerOrigin } from '../src/trackers'
-import { AndroidMessagingConfig } from '@duckduckgo/messaging'
+import { AndroidMessagingConfig } from '../packages/messaging/index.js'
 
 function initCode () {
     // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
