208d0f27a86d82062d2550e9813943efb8eb464c
Shane Osbourne <sosbourne@duckduckgo.com>
2023-11-16 16:03:56 +0000

    remove messaging duplication

--- src/features/broker-protection.js
+++ src/features/broker-protection.js
@@ -1,15 +1,7 @@
 import ContentFeature from '../content-feature.js'
 import { execute } from './broker-protection/actions.js'
-import { createMessaging } from '../create-messaging.js'
 
 export default class BrokerProtection extends ContentFeature {
-    get messaging () {
-        if (this._messaging) return this._messaging
-        if (!import.meta.injectName) throw new Error('unreachable')
-        this._messaging = createMessaging(this, import.meta.injectName)
-        return this._messaging
-    }
-
     init () {
         this.messaging.subscribe('onActionReceived', async (/** @type {any} */params) => {
             const action = params.state.action
