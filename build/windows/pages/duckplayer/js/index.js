9120b460ef43fd0677458f2f4aa625541e7cc449
Shane Osbourne <sosbourne@duckduckgo.com>
2023-12-18 16:33:09 +0000

    assets

--- build/windows/pages/duckplayer/js/index.js
+++ build/windows/pages/duckplayer/js/index.js
@@ -1265,7 +1265,7 @@
      * clicks within the iframe - for example when showing a video
      * that cannot be played in the embed
      *
-     * @param {string} urlBase
+     * @param {string} urlBase - macos/windows current use a different base URL
      */
     setClickListener: (urlBase) => {
       VideoPlayer.onIframeLoaded(() => {
