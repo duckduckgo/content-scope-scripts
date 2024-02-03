ae5f48e4d1797708a5d7f817df13497da1d5bad0
Shane Osbourne <sosbourne@duckduckgo.com>
2024-02-03 08:15:03 +0000

    don't publish to macos yet

--- packages/special-pages/index.mjs
+++ packages/special-pages/index.mjs
@@ -36,7 +36,6 @@ export const support = {
     onboarding: {
         'integration': ['copy', 'build-js'],
         'windows': ['copy', 'build-js'],
-        'apple': ['copy', 'build-js', 'inline-html'],
     },
 }
 
