8115fb6b54eb2044ab1f678afdb9a5b5c4a9bea6
Shane Osbourne <shane.osbourne8@gmail.com>
2024-02-26 18:53:45 +0000

    adding debug output (#921)
    * adding debug output
    
    * add metadata to extract action
    
    * stricter checks
    
    * better naming + fixed tests
    
    * fixing tests
    
    * don't fall through
    
    ---------
    
    Co-authored-by: Shane Osbourne <sosbourne@duckduckgo.com>

--- src/features/broker-protection/types.js
+++ src/features/broker-protection/types.js
@@ -25,8 +25,31 @@ export class SuccessResponse {
    * @param {string} params.actionID
    * @param {string} params.actionType
    * @param {any} params.response
+   * @param {Record<string, any>} [params.meta] - optional meta data
    */
     constructor (params) {
         this.success = params
+    }
+}
+
+/**
+ * A type that includes the result + metadata of comparing a DOM element + children
+ * to a set of data. Use this for analysis/debugging
+ */
+export class ProfileResult {
+    /**
+     * @param {object} params
+     * @param {boolean} params.result - whether we consider this a 'match'
+     * @param {string[]} params.matchedFields - a list of the fields in the data that were matched.
+     * @param {number} params.score - value to determine
+     * @param {HTMLElement} [params.element] - the parent element that was matched. Not present in JSON
+     * @param {Record<string, any>} params.scrapedData
+     */
+    constructor (params) {
+        this.scrapedData = params.scrapedData
+        this.result = params.result
+        this.score = params.score
+        this.element = params.element
+        this.matchedFields = params.matchedFields
     }
 }
