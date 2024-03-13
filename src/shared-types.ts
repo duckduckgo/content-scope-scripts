99df88408811b999c936013b28358318108e23a8
Shane Osbourne <sosbourne@duckduckgo.com>
2024-03-13 16:50:07 +0000

    optimize for re-use

---
+++ src/shared-types.ts
@@ -0,0 +1,20 @@
+export interface MessageTypes {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  requests?: Record<string, any>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  notifications?: Record<string, any>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  subscriptions?: Record<string, any>
+}
+
+export interface MessagingBase<T extends MessageTypes = MessageTypes> {
+  notify<
+    Method extends T['notifications'] extends { method: string } ? T['notifications']['method'] : never,
+    Msg = Extract<T['notifications'], { method: Method }>,
+  >(...args: Msg extends { params: infer Params } ? [Method, Params]: [Method]): void
+  request<
+    Method extends T['requests'] extends {method: string} ? T['requests']['method'] : never,
+    Msg = Extract<T['requests'], { method: Method }>,
+    Return = Msg extends { result: infer Result } ? Result : void
+  >(...args: Msg extends { params: infer Params } ? [Method, Params]: [Method]): Promise<Return>
+}
