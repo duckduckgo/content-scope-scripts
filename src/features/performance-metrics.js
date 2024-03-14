8de0b9e41c18b1b8ba4b526c068ba6742ab17e33
Brad Slayter <slayterdevelopment@gmail.com>
2024-03-14 14:00:56 -0500

    Reanme file. Cleanup array creation

--- src/features/performance-metrics.js
+++ src/features/performance-metrics.js
@@ -1,11 +1,12 @@
 import ContentFeature from '../content-feature'
 
-export default class WebVitals extends ContentFeature {
+export default class PerformanceMetrics extends ContentFeature {
     init () {
         this.messaging.subscribe('getVitals', () => {
             const paintResources = performance.getEntriesByType('paint')
             const firstPaint = paintResources.find((entry) => entry.name === 'first-contentful-paint')
-            this.messaging.notify('vitalsResult', { vitals: [firstPaint?.startTime] })
+            const vitals = firstPaint ? [firstPaint.startTime] : []
+            this.messaging.notify('vitalsResult', { vitals })
         })
     }
 }
