a1966c034311c524b8f80b01d63995d769ed2d49
Sabrina Tardio <stardio@duckduckgo.com>
2024-03-18 12:55:44 +0100

    fix linting errors

--- packages/special-pages/pages/sslerrorpage/src/js/index.js
+++ packages/special-pages/pages/sslerrorpage/src/js/index.js
@@ -7,45 +7,44 @@
  * [[include:packages/special-pages/pages/sslerrorpage/readme.md]]
  */
 
-import { Messaging, MessagingContext, WebkitMessagingConfig } from '@duckduckgo/messaging';
-import { SSLErrorPageMessages } from './messages.js';
+import { Messaging, MessagingContext, WebkitMessagingConfig } from '@duckduckgo/messaging'
+import { SSLErrorPageMessages } from './messages.js'
 
 const config = new WebkitMessagingConfig({
     hasModernWebkitAPI: true,
     secret: 'SECRET',
     webkitMessageHandlerNames: ['contentScopeScripts']
-});
+})
 
 const messagingContext = new MessagingContext({
     context: 'specialPages',
     featureName: 'sslErrorPage',
     env: 'development'
-});
+})
 
-const messages = new Messaging(messagingContext, config);
-const sslErrorPageMessages = new SSLErrorPageMessages(messages);
-console.log(messages);
+const messages = new Messaging(messagingContext, config)
+const sslErrorPageMessages = new SSLErrorPageMessages(messages)
+console.log(messages)
 
 document.addEventListener('DOMContentLoaded', () => {
-    const acceptRiskLink = document.getElementById('acceptRiskLink');
+    const acceptRiskLink = document.getElementById('acceptRiskLink')
     if (acceptRiskLink) {
         acceptRiskLink.addEventListener('click', (event) => {
-            event.preventDefault();
+            event.preventDefault()
             // Ensure this matches the expected format for the notify method
-            sslErrorPageMessages.notify({ action: "visitSite" });
-        });
+            sslErrorPageMessages.notify({ action: 'visitSite'})
+        })
     } else {
-        console.error('Accept risk link not found.');
+        console.error('Accept risk link not found.')
     }
 
-    const leaveSiteButton = document.getElementById('leaveThisSiteBtn');
+    const leaveSiteButton = document.getElementById('leaveThisSiteBtn')
     if (leaveSiteButton) {
         leaveSiteButton.addEventListener('click', (event) => {
-            event.preventDefault();
-            // Ensure this matches the expected format for the notify method
-            sslErrorPageMessages.notify({ action: "leaveSite" });
+            event.preventDefault()
+            sslErrorPageMessages.notify({ action: "leaveSite" })
         });
     } else {
-        console.error('Leave Site button not found.');
+        console.error('Leave Site button not found.')
     }
-});
+})
