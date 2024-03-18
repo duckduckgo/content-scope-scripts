c30de7647b11eb323ee9fddd0d0a0b858d522af8
Sabrina Tardio <stardio@duckduckgo.com>
2024-03-18 12:22:13 +0100

    add messages.js

---
+++ packages/special-pages/pages/sslerrorpage/src/js/messages.js
@@ -0,0 +1,17 @@
+export class SSLErrorPageMessages {
+  /**
+     * @param {import("@duckduckgo/messaging").Messaging} messaging
+     * @internal
+     */
+  constructor (messaging) {
+    /**
+     * @internal
+     */
+    this.messaging = messaging
+}
+
+    notify (message) {
+        return this.messaging.notify(message)
+    }
+}
+
