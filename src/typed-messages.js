3b7c88f26ed08e8be16a7d467a13a546dc333ee9
Shane Osbourne <sosbourne@duckduckgo.com>
2024-03-22 10:24:58 +0000

    docs

--- src/typed-messages.js
+++ src/typed-messages.js
@@ -1,9 +1,17 @@
 /**
- * @template {import("./shared-types").MessagingBase} BaseClass
+ * This utility can be used to convert an instance of the `Messaging` interface
+ * into a strongly typed one.
+ *
+ * The first input is any class where you've added your typed messages,
+ * and then the second input is any instance of Messaging, which gets converted
+ * into only calls supported by your schema
+ *
+ * @template {Partial<import("./shared-types").MessagingBase>} BaseClass
  * @param {BaseClass} base - the class onto which you've added the properties from `MessagingBase`
  * @param {import("@duckduckgo/messaging").Messaging} messaging
- * @returns {{notify: BaseClass['notify']; request: BaseClass['request']; subscribe: BaseClass['subscribe']}}
+ * @returns {BaseClass}
  */
 export function createTypedMessages (base, messaging) {
-    return /** @type {any} */(messaging)
+    const asAny = /** @type {any} */(messaging)
+    return /** @type {BaseClass} */(asAny)
 }
