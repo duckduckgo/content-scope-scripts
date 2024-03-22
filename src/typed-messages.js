3dc4b12c8e769af238a7dec98ece12c7e8480c98
Shane Osbourne <sosbourne@duckduckgo.com>
2024-03-22 09:47:43 +0000

    simpler pass-through

--- src/typed-messages.js
+++ src/typed-messages.js
@@ -2,29 +2,11 @@
  * @template {import("./shared-types").MessagingBase} BaseClass
  * @param {BaseClass} base - the class onto which you've added the properties from `MessagingBase`
  * @param {import("@duckduckgo/messaging").Messaging} messaging
+ * @returns {{notify: BaseClass['notify']; request: BaseClass['request']}}
  */
 export function createTypedMessages (base, messaging) {
-    return {
-        /**
-         * @type {BaseClass['notify']}
-         */
-        notify: (...args) => {
-            const [name, params] = args
-            if (name && params) {
-                messaging.notify(name, params)
-            } else {
-                messaging.notify(name)
-            }
-        },
-        /**
-         * @type {BaseClass['request']}
-         */
-        request: (...args) => {
-            const [name, params] = args
-            if (name && params) {
-                return messaging.request(args[0], params)
-            }
-            return messaging.request(args[0])
-        }
-    }
+    return /** @type {any} */({
+        notify: messaging.notify,
+        request: messaging.request
+    })
 }
