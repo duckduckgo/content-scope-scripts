9120bb7359091720e9b343cd3b7c2405b131a69a
Thomas Espach <tespach@duckduckgo.com>
2024-04-22 18:13:44 +0100

    Add phishing error page template.

---
+++ packages/special-pages/pages/phishingerrorpage/src/js/defaults.js
@@ -0,0 +1,12 @@
+export const defaultLoadData = {
+    strings: {
+        header: 'Warning: This site may be malicious',
+        body: 'This website may try to trick you into doing something dangerous, like installing software or disclosing personal or financial information, like passwords, phone numbers or credit cards.',
+        advancedInfoHeader: 'DuckDuckGo warns you when a website has been flagged as malicious.',
+        advancedButton: 'Advanced...',
+        leaveSiteButton: 'Leave This Site',
+        specificMessage: 'Warnings are shown for websites that have been reported as deceptive. Deceptive websites try to trick you into believing they are legitimate websites you trust.',
+        advancedInfoBody: 'If you understand the risks involved, you can continue anyway.',
+        visitSiteBody: 'Accept Risk and Visit Site'
+    }
+}
