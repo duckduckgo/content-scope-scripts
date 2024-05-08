109cacef2ff3794d9954ee5f23ca568445aec59a
Shane Osbourne <sosbourne@duckduckgo.com>
2024-05-08 16:34:30 +0100

    initial layout for the newtab page

---
+++ packages/special-pages/pages/new-tab/app/components/Layout.js
@@ -0,0 +1,13 @@
+import { h } from "preact";
+import styles from "./Layout.module.css";
+import {Stack} from "../../../../shared/components/Stack";
+
+export function Layout({ children }) {
+    return (
+        <div className={styles.layout}>
+            <Stack gap={'var(--sp-7)'}>
+                {children}
+            </Stack>
+        </div>
+    )
+}
