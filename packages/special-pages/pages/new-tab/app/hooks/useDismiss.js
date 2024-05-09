a2edd56f013d684b8047e9e7870f71e9d9e938a2
Shane Osbourne <sosbourne@duckduckgo.com>
2024-05-09 15:50:51 +0100

    added section toggles for the NTP

---
+++ packages/special-pages/pages/new-tab/app/hooks/useDismiss.js
@@ -0,0 +1,24 @@
+import {useEffect} from "preact/hooks";
+
+export function useDismiss(contentRef, close) {
+    useEffect(() => {
+        const handler = (e) => {
+            if (e.code === 'Escape') close();
+        };
+        const clickHandler = (e) => {
+            if (!e.target.isConnected) return;
+            if (contentRef.current?.contains(e.target)) {
+                return;
+            } else {
+                close();
+            }
+        };
+        window.addEventListener('keyup', handler);
+        window.addEventListener('click', clickHandler);
+
+        return () => {
+            window.removeEventListener('keyup', handler);
+            window.removeEventListener('click', clickHandler);
+        };
+    }, [close, contentRef]);
+}
