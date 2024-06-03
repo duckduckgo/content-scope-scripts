d35090ab53f650b964c50ab9faa0d6fa5a412429
Marcos Gurgel <mgurgel@gmail.com>
2024-06-03 15:19:12 +0100

    Undid Prettier changes

--- packages/special-pages/pages/specialerrorpage/src/js/template.js
+++ packages/special-pages/pages/specialerrorpage/src/js/template.js
@@ -9,9 +9,7 @@ export function execTemplate (strings) {
                     <img src="${shieldIcon}" alt="Warning" class="watermark">
                     ${strings.header}
                 </h1>
-                <p class="warning-text">
-                    ${trustedUnsafeEscaped(strings.body)}
-                </p>
+                <p class="warning-text">${trustedUnsafeEscaped(strings.body)}</p>
                 <div class="buttons">
                     <button class="button advanced" id="advancedBtn">${strings.advancedButton}</button>
                     <button class="button leave-this-site" id="leaveThisSiteBtn">${strings.leaveSiteButton}</button>
