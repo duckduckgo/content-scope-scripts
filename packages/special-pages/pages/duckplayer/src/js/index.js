a1f1498abcd951929498f704107fb512c53cc0c8
Maxim Tsoy <maks.tsoy@gmail.com>
2024-06-05 21:23:14 +0200

    Lint fix

--- packages/special-pages/pages/duckplayer/src/js/index.js
+++ packages/special-pages/pages/duckplayer/src/js/index.js
@@ -244,10 +244,10 @@ const VideoPlayer = {
                 const iframeWindow = iframe?.contentWindow
                 if (iframeDocument && iframeWindow) {
                     // @ts-expect-error - typescript doesn't know about CSSStyleSheet here for some reason
-                    const styleSheet = new iframeWindow.CSSStyleSheet();
-                    styleSheet.replaceSync("button.ytp-pip-button { display: inline-block !important; }");
+                    const styleSheet = new iframeWindow.CSSStyleSheet()
+                    styleSheet.replaceSync('button.ytp-pip-button { display: inline-block !important; }')
                     // See https://developer.mozilla.org/en-US/docs/Web/API/Document/adoptedStyleSheets#append_a_new_stylesheet
-                    iframeDocument.adoptedStyleSheets = [...iframeDocument.adoptedStyleSheets, styleSheet];
+                    iframeDocument.adoptedStyleSheets = [...iframeDocument.adoptedStyleSheets, styleSheet]
                 }
             } catch (e) {
                 // ignore errors
