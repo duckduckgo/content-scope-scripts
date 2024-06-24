7aa9f4af946cf1d397ecaacfddbe67d16ff65bc5
Marcos Gurgel <mgurgel@gmail.com>
2024-06-24 18:33:40 +0100

    New tag callout; ContentPlaceholder component

---
+++ packages/special-pages/pages/release-notes/app/components/ContentPlaceholder.js
@@ -0,0 +1,19 @@
+/**
+ * Renders a purely visual placeholder for when content is still loading.
+ * Sematic HTML tags are used to mimic the final rendered elements.
+ */
+import { h } from 'preact'
+import styles from './ContentPlaceholder.module.css'
+
+export function ContentPlaceholder() {
+    return (
+        <div className={styles.contentPlaceholder} aria-hidden="true">
+            <h2></h2>
+            <p></p>
+            <ul>
+                <li><p></p><p></p></li>
+                <li><p></p><p></p></li>
+            </ul>
+        </div>
+    )
+}
