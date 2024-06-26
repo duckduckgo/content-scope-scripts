da4534e3ba358d3d8ec05eb209cc6253e15d2d55
Shane Osbourne <sosbourne@duckduckgo.com>
2024-06-26 14:57:21 +0100

    logging

--- Sources/BuildNode/main.swift
+++ Sources/BuildNode/main.swift
@@ -1,7 +1,21 @@
 import Foundation
 
+// Define the process
 let process = Process()
 process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
-process.arguments = ["bash", "-c", "npm install && npm run build"]
-try process.run()
-process.waitUntilExit()
+process.arguments = ["bash", "-c", "npm install && npm run build-apple"]
+
+do {
+    // Run the process
+    try process.run()
+    process.waitUntilExit()
+
+    // Check the exit status
+    if process.terminationStatus == 0 {
+        print("Node.js build succeeded.")
+    } else {
+        print("Node.js build failed.")
+    }
+} catch {
+    print("An error occurred: \(error)")
+}
