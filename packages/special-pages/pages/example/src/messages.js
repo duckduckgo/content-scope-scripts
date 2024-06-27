25041091baa4d807277b57520bf740448b5811eb
Marcos Gurgel <mgurgel@gmail.com>
2024-06-27 10:57:52 +0100

    Checked out example from main

---
+++ packages/special-pages/pages/example/src/messages.js
@@ -0,0 +1,27 @@
+export class ExamplePageMessages {
+    /**
+     * @param {import("@duckduckgo/messaging").Messaging} messaging
+     * @internal
+     */
+    constructor (messaging) {
+        this.messaging = messaging
+    }
+
+    requests = {
+        /**
+         * @returns {Promise<boolean>}
+         */
+        getOptions: () => {
+            return this.messaging.request('hello', { world: 'here' })
+        }
+    }
+
+    notifications = {
+        /**
+         * @param {import("./pixels.js").Pixel} knownPixel
+         */
+        sendPixel: (knownPixel) => {
+            this.messaging.notify(knownPixel.name(), knownPixel.params())
+        }
+    }
+}
