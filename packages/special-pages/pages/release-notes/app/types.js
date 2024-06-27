238cf3ba3f6967dd8d1bfe0891a79f27a12dde67
Marcos Gurgel <mgurgel@gmail.com>
2024-06-27 17:36:22 +0100

    Import fix

--- packages/special-pages/pages/release-notes/app/types.js
+++ packages/special-pages/pages/release-notes/app/types.js
@@ -1,11 +1,9 @@
 import { useContext } from 'preact/hooks'
 import { TranslationContext } from '../../../shared/components/TranslationsProvider.js'
-import { i18n } from './text.js'
-
 
 /**
  * This is a wrapper to only allow keys from the default translation file
- * @type {() => { t: (key: keyof i18n['en']['translations'], replacements?: Record<string, string>) => string }}
+ * @type {() => { t: (key: keyof import('./text.js').i18n['en']['translations'], replacements?: Record<string, string>) => string }}
  */
 export function useTypedTranslation () {
     return {
