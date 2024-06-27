425369bc80d82e4e623d388a8c6bd041354e1d8b
Marcos Gurgel <mgurgel@gmail.com>
2024-06-27 17:00:03 +0100

    Components module and CSS tweaks

--- packages/special-pages/shared/components/Button/Button.js
+++ packages/special-pages/shared/components/Button/Button.js
@@ -6,7 +6,7 @@ import styles from './Button.module.css'
  * @param {object} props
  * @param {'apple'} [props.platform] // More platforms to be supported in the future
  * @param {import("preact").ComponentChild} props.children
- * @param {import("preact").JSX.MouseEventHandler<EventTarget>} props.onClick
+ * @param {import("preact").JSX.MouseEventHandler<EventTarget>} [props.onClick]
  * @param {import('preact').ComponentProps<'button'>} [props.otherProps]
  */
 export function Button ({ platform = 'apple', children, onClick }) {
