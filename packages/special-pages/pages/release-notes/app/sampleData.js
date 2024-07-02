c6d1740842ab9d8089e46ad1fccad9eedb6d5051
Marcos Gurgel <mgurgel@gmail.com>
2024-07-02 11:12:46 +0100

    Lint fixes

--- packages/special-pages/pages/release-notes/app/sampleData.js
+++ packages/special-pages/pages/release-notes/app/sampleData.js
@@ -2,7 +2,7 @@
  * @typedef {import('../../../types/release-notes').UpdateMessage} UpdateMessage
  */
 
-const timestampInSeconds = Date.now() / 1000;
+const timestampInSeconds = Date.now() / 1000
 
 /**
  * @type {Record<UpdateMessage['status'], UpdateMessage>}
