081971ca56ea12fe7cb8e7c9032b4f3bd4fbafbc
Shane Osbourne <shane.osbourne8@gmail.com>
2024-07-08 17:38:46 +0100

    update special pages example to new baseline (#994)
    * update special pages example to new baseline
    
    * fix build
    
    * PR feedback
    
    ---------
    
    Co-authored-by: Shane Osbourne <sosbourne@duckduckgo.com>

---
+++ packages/special-pages/pages/example/app/components/App.jsx
@@ -0,0 +1,12 @@
+import { h } from "preact";
+import styles from "./App.module.css";
+import { useTypedTranslation } from "../types.js";
+
+export function App() {
+    const { t } = useTypedTranslation()
+    return (
+        <main class={styles.main}>
+            {t('helloWorld')}
+        </main>
+    )
+}
