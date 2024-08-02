3d4b0e2067165a88706d6c6ab28d7f1580166fa0
Anastasia Shuba <ashuba22@gmail.com>
2024-08-01 19:34:56 -0700

    use array + share code

--- src/features/breakage-reporting.js
+++ src/features/breakage-reporting.js
@@ -1,15 +1,14 @@
 import ContentFeature from '../content-feature'
+import { getJsPerformanceMetrics } from './breakage-reporting/utils.js'
 
 export default class BreakageReporting extends ContentFeature {
     init () {
         this.messaging.subscribe('getBreakageReportValues', () => {
-            const paintResources = performance.getEntriesByType('paint')
-            const firstPaint = paintResources.find((entry) => entry.name === 'first-contentful-paint')
-            const firstPaintStart = firstPaint ? firstPaint.startTime : -1.0
-
+            const jsPerformance = getJsPerformanceMetrics()
             const referrer = document.referrer
+
             this.messaging.notify('breakageReportResult', {
-                firstPaintStart,
+                jsPerformance,
                 referrer
             })
         })
