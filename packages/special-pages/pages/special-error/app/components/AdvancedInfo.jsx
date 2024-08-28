a23c0218a6326cabd39c08988337d87217b9ae3a
Marcos Gurgel <mgurgel@gmail.com>
2024-08-28 15:22:30 +0100

    Mitigating jumpiness when toggling Advanced Info on small screens

--- packages/special-pages/pages/special-error/app/components/AdvancedInfo.jsx
+++ packages/special-pages/pages/special-error/app/components/AdvancedInfo.jsx
@@ -1,4 +1,5 @@
 import { h } from 'preact'
+import { useLayoutEffect, useRef } from 'preact/hooks'
 import { useTypedTranslation } from '../types'
 import { Text } from '../../../../shared/components/Text/Text'
 import { useMessaging } from '../providers/MessagingProvider'
@@ -9,10 +10,22 @@ import styles from './AdvancedInfo.module.css'
 export function VisitSiteLink() {
     const { messaging } = useMessaging()
     const { t } = useTypedTranslation()
+    /**
+     * @type {import("preact/hooks").MutableRef<HTMLDivElement|null>}
+     */
+    const ref = useRef(null)
+
+    useLayoutEffect(() => {
+        setTimeout(() => {
+            if (ref.current) {
+                ref.current.scrollIntoView({ behavior: 'smooth' })
+            }
+        }, 300) // Must be the same duration as CSS 'appear' animation in AdvancedInfo.module.css
+    }, [])
 
     return (
         <Text as="a" variant="body" className={styles.visitSite} onClick={() => messaging?.visitSite()}>
-            {t('visitSiteButton')}
+            <span ref={ref}>{t('visitSiteButton')}</span>
         </Text>
     )
 }
