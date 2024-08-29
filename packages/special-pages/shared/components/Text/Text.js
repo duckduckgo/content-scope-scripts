a94cd78073a0185eaab18ce36c8bea72df746e1b
Marcos Gurgel <mgurgel@users.noreply.github.com>
2024-08-29 16:24:04 +0100

    Special Error Page on Preact (#1009)
    Includes SSL and Phishing pages for macOS and iOS
    
    ---------
    
    Co-authored-by: Shane Osbourne <sosbourne@duckduckgo.com>

--- packages/special-pages/shared/components/Text/Text.js
+++ packages/special-pages/shared/components/Text/Text.js
@@ -7,11 +7,12 @@ import styles from './Text.module.css'
  * @param {import('preact').JSX.ElementType} [props.as]
  * @param {Omit<keyof styles, "text">} [props.variant]
  * @param {string} [props.className]
+ * @param {boolean} [props.strictSpacing] - Apply Design System letter spacing. Default: true
  * @param {import("preact").ComponentChild} [props.children]
  */
-export function Text ({ as: Comp = 'p', variant, className, children }) {
+export function Text ({ as: Comp = 'p', variant, strictSpacing = true, className, children }) {
     return (
-        <Comp className={classNames({ [styles[`${variant}`]]: variant }, className)}>
+        <Comp className={classNames({ [styles[`${variant}`]]: variant, [styles.strictSpacing]: strictSpacing }, className)}>
             {children}
         </Comp>
     )
