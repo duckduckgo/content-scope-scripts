3ff7b4ee91502c2d5d084466d14511c35e5951e3
Marcos Gurgel <mgurgel@gmail.com>
2024-09-25 18:33:53 +0100

    Speech bubble resize

--- packages/special-pages/pages/onboarding/app/components/v3/Heading.js
+++ packages/special-pages/pages/onboarding/app/components/v3/Heading.js
@@ -1,8 +1,7 @@
 import { h } from 'preact'
 import cn from 'classnames'
-import { useState, useEffect } from 'preact/hooks'
+import { useState, useEffect, useRef, useLayoutEffect } from 'preact/hooks'
 import { Typed } from '../Typed'
-import { Stack } from '../Stack'
 
 import styles from './Heading.module.css'
 
@@ -17,7 +16,8 @@ import styles from './Heading.module.css'
  * @param {import('preact').ComponentChildren} [props.children]
  */
 export function Heading ({ title, subtitle, speechBubble = false, onTitleComplete, children }) {
-    const [animationDone, setAnimationDone] = useState(false)
+    const [typingDone, setTypingDone] = useState(false)
+    const [typingPaused, setTypingPaused] = useState(!!speechBubble)
 
     if (!title) {
         console.warn('Missing title')
@@ -25,18 +25,23 @@ export function Heading ({ title, subtitle, speechBubble = false, onTitleComplet
     }
 
     useEffect(() => {
-        setAnimationDone(false)
-    }, [title])
+        setTypingDone(false)
+        setTypingPaused(!!speechBubble)
+    }, [speechBubble, title, subtitle])
 
-    const onComplete = () => {
-        setAnimationDone(true)
+    const onTypingComplete = () => {
+        setTypingDone(true)
         onTitleComplete && onTitleComplete()
+    }
+
+    const onAnimationDone = () => {
+        setTypingPaused(false)
     }
 
     const HeadingComponent = speechBubble ? SpeechBubble : PlainHeading
     const subtitleClass = cn({
         [styles.subTitle]: true,
-        [styles.hidden]: !animationDone
+        [styles.hidden]: !typingDone
     })
 
     return (
@@ -44,14 +49,12 @@ export function Heading ({ title, subtitle, speechBubble = false, onTitleComplet
             <div className={styles.logo}>
                 <img className={styles.svg} src="assets/img/dax.svg" alt="DuckDuckGo Logo" />
             </div>
-            <HeadingComponent>
-                <Stack animate>
-                    <h1 className={styles.title}>
-                        <Typed onComplete={onComplete} text={title} />
-                    </h1>
-                    {subtitle && <h2 className={subtitleClass}>{subtitle}</h2>}
-                    {!!animationDone && children}
-                </Stack>
+            <HeadingComponent onAnimationDone={onAnimationDone}>
+                <h1 className={styles.title}>
+                    <Typed onComplete={onTypingComplete} text={title} paused={typingPaused} />
+                </h1>
+                {subtitle && <h2 className={subtitleClass}>{subtitle}</h2>}
+                {typingDone && children}
             </HeadingComponent>
         </header>
     )
@@ -71,14 +74,43 @@ function PlainHeading ({ children }) {
 
 /**
  * @param {object} props
+ * @param {() => void} [props.onAnimationDone]
  * @param {import('preact').ComponentChildren} props.children
  */
-function SpeechBubble ({ children }) {
+function SpeechBubble ({ onAnimationDone, children }) {
+    const bubbleContents = useRef(null)
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
+    const [animationDone, setAnimationDone] = useState(false)
+
+    useLayoutEffect(() => {
+        if (bubbleContents.current) {
+            const div = /** @type {HTMLDivElement} */(bubbleContents.current)
+            const { width, height } = div.getBoundingClientRect()
+            if (dimensions.width !== width || dimensions.height !== height) {
+                setAnimationDone(false)
+                setDimensions({ width, height })
+            }
+        }
+    }, [bubbleContents, children])
+
+    const onTransitionEnd = () => {
+        setAnimationDone(true)
+        onAnimationDone && onAnimationDone()
+    }
+
+    const contentsClass = cn({
+        [styles.speechBubbleContents]: true,
+        [styles.hidden]: !animationDone
+    })
+
     return (
         <div className={styles.speechBubble}>
             <div className={styles.speechBubbleCallout} />
-            <div className={styles.speechBubbleContents}>
-                {children}
+            <div className={styles.speechBubbleContainer}>
+                <div className={styles.speechBubbleBackground} style={{ width: `${dimensions.width}px`, height: `${dimensions.height}px` }} onTransitionEnd={onTransitionEnd}></div>
+                <div className={contentsClass} ref={bubbleContents}>
+                    {children}
+                </div>
             </div>
         </div>
     )
