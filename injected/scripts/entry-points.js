f16b140c7731f9a66619e426b1f24414abaeb954
Shane Osbourne <shane.osbourne8@gmail.com>
2024-10-15 10:56:50 +0100

    Move all packages to root level (#1103)
    * moved the top-level feature into injected
    * moved packages/messaging + packages/special-pages to the root level
    * created another top-level folder to contain the stuff about type-generation, since it's re-used in a couple of places
    * updated the README files
    * everything still builds into the top-level./build - means no changes for native folk
    * docs, eslint, typescript etc all work as before 🥳
    * updated code owners
    
    ---------
    
    Co-authored-by: Shane Osbourne <sosbourne@duckduckgo.com>

---
+++ injected/scripts/entry-points.js
@@ -0,0 +1,115 @@
+import { postProcess, rollupScript } from './utils/build.js'
+import { parseArgs, write } from '../../scripts/script-utils.js'
+import { camelcase } from '../src/utils.js'
+
+const contentScopePath = 'src/content-scope-features.js'
+const contentScopeName = 'contentScopeFeatures'
+
+/**
+ * @typedef Build
+ * @property {string} input
+ * @property {string[]} output
+ * @property {boolean} [postProcess] - optional value to post-process an output file
+ *
+ * @typedef {Record<NonNullable<ImportMeta['injectName']>, Build>} BuildManifest
+ */
+
+/** @satisfies {BuildManifest} */
+const builds = {
+    firefox: {
+        input: 'entry-points/mozilla.js',
+        output: ['../build/firefox/entry-points.js']
+    },
+    apple: {
+        input: 'entry-points/apple.js',
+        postProcess: true,
+        output: ['../Sources/ContentScopeScripts/dist/contentScope.js']
+    },
+    'apple-isolated': {
+        input: 'entry-points/apple.js',
+        output: ['../Sources/ContentScopeScripts/dist/contentScopeIsolated.js']
+    },
+    android: {
+        input: 'entry-points/android.js',
+        output: ['../build/android/contentScope.js']
+    },
+    windows: {
+        input: 'entry-points/windows.js',
+        output: ['../build/windows/contentScope.js']
+    },
+    integration: {
+        input: 'entry-points/integration.js',
+        output: [
+            '../build/integration/contentScope.js',
+            'integration-test/extension/contentScope.js',
+            'integration-test/test-pages/build/contentScope.js'
+        ]
+    },
+    'chrome-mv3': {
+        input: 'entry-points/chrome-mv3.js',
+        output: ['../build/chrome-mv3/entry-points.js']
+    },
+    chrome: {
+        input: 'entry-points/chrome.js',
+        output: ['../build/chrome/entry-points.js']
+    }
+}
+
+async function initOther (injectScriptPath, platformName) {
+    const supportsMozProxies = platformName === 'firefox'
+    const identName = `inject${camelcase(platformName)}`
+    const injectScript = await rollupScript({
+        scriptPath: injectScriptPath,
+        name: identName,
+        supportsMozProxies,
+        platform: platformName
+    })
+    const outputScript = injectScript
+    return outputScript
+}
+
+/**
+ * @param {string} entry
+ * @param {string} platformName
+ */
+async function initChrome (entry, platformName) {
+    const replaceString = '/* global contentScopeFeatures */'
+    const injectScript = await rollupScript({ scriptPath: entry, platform: platformName })
+    const contentScope = await rollupScript({
+        scriptPath: contentScopePath,
+        name: contentScopeName,
+        platform: platformName
+    })
+    // Encode in URI format to prevent breakage (we could choose to just escape ` instead)
+    // NB: .replace(/\r\n/g, "\n") is needed because in Windows rollup generates CRLF line endings
+    const encodedString = encodeURI(contentScope.toString().replace(/\r\n/g, '\n'))
+    const outputScript = injectScript.toString().replace(replaceString, '${decodeURI("' + encodedString + '")}')
+    return outputScript
+}
+
+async function init () {
+    // verify the input
+    const requiredFields = ['platform']
+    const args = parseArgs(process.argv.slice(2), requiredFields)
+    const build = builds[args.platform]
+
+    if (!build) {
+        throw new Error('unsupported platform: ' + args.platform)
+    }
+
+    let output
+    if (args.platform === 'chrome') {
+        output = await initChrome(build.input, args.platform)
+    } else {
+        output = await initOther(build.input, args.platform)
+        if (build.postProcess) {
+            const processResult = await postProcess(output)
+            output = processResult.code
+        }
+    }
+
+    // bundle and write the output
+    write([build.output], output)
+}
+
+init()
