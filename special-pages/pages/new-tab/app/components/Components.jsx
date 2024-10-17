2369232f1a33e5a487ffd327d7974f2b70032e30
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-10-16 22:57:08 -0600

    chore: Update the Components

--- special-pages/pages/new-tab/app/components/Components.jsx
+++ special-pages/pages/new-tab/app/components/Components.jsx
@@ -12,7 +12,7 @@ const examples = {
         factory: () => <PrivacyStatsMockProvider ticker={true}><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
     },
     'stats.few.collapsed': {
-        factory: () => <PrivacyStatsMockProvider config={{expansion: "collapsed"}}><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
+        factory: () => <PrivacyStatsMockProvider config={{ expansion: "collapsed" }}><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
     },
     'stats.single': {
         factory: () => <PrivacyStatsMockProvider data={stats.single}><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
@@ -32,8 +32,59 @@ const examples = {
     'stats.heading.none': {
         factory: () => <Heading trackerCompanies={stats.none.trackerCompanies} totalCount={stats.none.totalCount} />
     },
-    'rmf': {
-        factory: () => <RemoteMessagingFramework messageType="medium" />
+    'rmf-small': {
+        factory: () => (
+            <RemoteMessagingFramework
+                messageType="small"
+                titleText="Small title"
+                descriptionText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget elit vel ex dapibus."
+            />)
+    },
+    'rmf-medium': {
+        factory: () => (
+            <RemoteMessagingFramework
+                messageType="medium"
+                icon="Announce"
+                titleText="Medium title"
+                descriptionText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget elit vel ex dapibus."
+            />)
+    },
+    'rmf-big-single-action': {
+        factory: () => (
+            <RemoteMessagingFramework
+                messageType="big_single_action"
+                icon="AppUpdate"
+                titleText="Big one button title"
+                descriptionText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget elit vel ex dapibus."
+                primaryActionText="Take Survey"
+                primaryAction={() => { }}
+            />)
+    },
+    'rmf-big-two-action': {
+        factory: () => (
+            <RemoteMessagingFramework
+                messageType="big_two_action"
+                icon="CriticalUpdate"
+                titleText="Big 2 buttons title"
+                descriptionText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget elit vel ex dapibus."
+                primaryActionText="Take Survey"
+                primaryAction={() => { }}
+                secondaryActionText="Remind Me Later"
+                secondaryAction={() => { }}
+            />)
+    },
+    'rmf-big-two-action-overflow': {
+        factory: () => (
+            <RemoteMessagingFramework
+                messageType="big_two_action"
+                icon="DDGAnnounce"
+                titleText="Big 2 buttons with long titles"
+                descriptionText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget elit vel ex dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget elit vel ex dapibus."
+                primaryActionText="How to update Windows with every step fully explained"
+                primaryAction={() => { }}
+                secondaryActionText="Remind me later, but only if I’m actually going to update soon"
+                secondaryAction={() => { }}
+            />)
     }
 }
 
@@ -42,7 +93,7 @@ const url = new URL(window.location.href);
 export function Components() {
     const id = url.searchParams.get("id");
     const isolated = url.searchParams.has("isolate");
-    const valid = (id||'') in examples;
+    const valid = (id || '') in examples;
     const entries = Object.entries(examples);
     const filtered = id && valid
         ? entries.filter(([_id]) => _id === id)
@@ -54,7 +105,7 @@ export function Components() {
 
     return (
         <div>
-            <DebugBar id={id} entries={entries}/>
+            <DebugBar id={id} entries={entries} />
             <Stage entries={filtered} />
         </div>
     )
@@ -142,7 +193,7 @@ function ExampleSelector({ entries, id }) {
         if (!(event.target instanceof HTMLSelectElement)) return;
         const selectedId = event.target.value;
         if (selectedId) {
-            if (selectedId==="none") return onReset();
+            if (selectedId === "none") return onReset();
             const url = new URL(window.location.href);
             url.searchParams.set("id", selectedId);
             window.location.href = url.toString();
