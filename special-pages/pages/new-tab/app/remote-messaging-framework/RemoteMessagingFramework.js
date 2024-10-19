809b92560ee0ad58698f02d36e3c76d391a24968
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-10-18 21:27:15 -0600

    chore: Update RMF components

--- special-pages/pages/new-tab/app/remote-messaging-framework/RemoteMessagingFramework.js
+++ special-pages/pages/new-tab/app/remote-messaging-framework/RemoteMessagingFramework.js
@@ -1,16 +1,56 @@
-import {h} from "preact";
-import cn from "classnames";
-import styles from "./RemoteMessagingFramework.module.css"
+import { h, Fragment } from 'preact'
+import cn from 'classnames'
+import styles from './RemoteMessagingFramework.module.css'
+import MessageIcons from './MessageIcons'
+
 /**
- * 
- * @param {object} props 
+ *
+ * @param {object} props;
  */
 
-export function RemoteMessagingFramework(props) {
-    const {id, messageType, titleText, descriptionText, icon, primaryActionText = "", primaryAction, secondaryActionText = "", secondaryAction} = props;
+// icon : "Announce"|"DDGAnnounce"|"CriticalUpdate"|"AppUpdate"
+// messageType : "small"|"medium"|"big_single_action"|"big_two_action"
 
+export function RemoteMessagingFramework (props) {
+    const { id, messageType, titleText, descriptionText, icon, primaryActionText = '', primaryAction, secondaryActionText = '', secondaryAction } = props
+
+    const handlePrimaryClick = () => {
+        primaryAction()
+    }
+
+    const handleSecondaryClick = () => {
+        secondaryAction()
+    }
 
     return (
-        <div id={id} class={styles.root}>{messageType}</div>
+        <>
+            <p>
+                {messageType}
+            </p>
+            <div id={id} class={cn(styles.root, icon && styles.icon)}>
+                {icon && (
+                    <span class={styles.iconBlock}>
+                        <MessageIcons name={icon} />
+                    </span>
+                )}
+                <div class={styles.content}>
+                    <p class={styles.title}>{titleText}</p>
+                    <p>{descriptionText}</p>
+                    {messageType === 'big_two_action' && (
+                        <div className="buttonRow">
+                            {primaryActionText.length && (
+                                <button class={cn(styles.btn, styles.primary)} onClick={handlePrimaryClick}>{primaryActionText}</button>
+                            )}
+                            {secondaryActionText.length > 0 && (
+                                <button class={cn(styles.btn, styles.secondary)} onClick={handleSecondaryClick}>{secondaryActionText}</button>
+                            )}
+                        </div>
+                    )}
+                </div>
+                {messageType === 'big_single_action' && primaryActionText && primaryAction && (
+                    <button class={cn(styles.btn)} onClick={handlePrimaryClick}>{primaryActionText}</button>
+                )}
+            </div>
+        </>
     )
 }
