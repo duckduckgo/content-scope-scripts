cecbde3e934afdc3696c06d94c0d5528fedd56fe
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-10-18 21:46:29 -0600

    chore: Add to Examples.jsx

--- special-pages/pages/new-tab/app/components/Examples.jsx
+++ special-pages/pages/new-tab/app/components/Examples.jsx
@@ -1,35 +1,67 @@
 import { h } from "preact";
 import { PrivacyStatsMockProvider } from "../privacy-stats/mocks/PrivacyStatsMockProvider.js";
 import { Body, Heading, PrivacyStatsConsumer } from "../privacy-stats/PrivacyStats.js";
+import { RemoteMessagingFramework } from "../remote-messaging-framework/RemoteMessagingFramework.js";
 import { stats } from "../privacy-stats/mocks/stats.js";
 
 /** @type {Record<string, {factory: () => import("preact").ComponentChild}>} */
 export const mainExamples = {
     'stats.few': {
-        factory: () => <PrivacyStatsMockProvider ticker={true}><PrivacyStatsConsumer/></PrivacyStatsMockProvider>
+        factory: () => <PrivacyStatsMockProvider ticker={true}><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
     },
     'stats.few.collapsed': {
-        factory: () => <PrivacyStatsMockProvider config={{expansion: "collapsed"}}><PrivacyStatsConsumer/></PrivacyStatsMockProvider>
+        factory: () => <PrivacyStatsMockProvider config={{ expansion: "collapsed" }}><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
     },
     'stats.single': {
-        factory: () => <PrivacyStatsMockProvider data={stats.single}><PrivacyStatsConsumer/></PrivacyStatsMockProvider>
+        factory: () => <PrivacyStatsMockProvider data={stats.single}><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
     },
     'stats.none': {
-        factory: () => <PrivacyStatsMockProvider data={stats.none}><PrivacyStatsConsumer/></PrivacyStatsMockProvider>
+        factory: () => <PrivacyStatsMockProvider data={stats.none}><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
     },
     'stats.norecent': {
         factory: () => <PrivacyStatsMockProvider
-            data={stats.norecent}><PrivacyStatsConsumer/></PrivacyStatsMockProvider>
+            data={stats.norecent}><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
     },
     'stats.list': {
-        factory: () => <Body trackerCompanies={stats.few.trackerCompanies} id='example-stats.list'/>
+        factory: () => <Body trackerCompanies={stats.few.trackerCompanies} id='example-stats.list' />
     },
     'stats.heading': {
-        factory: () => <Heading trackerCompanies={stats.few.trackerCompanies} totalCount={stats.few.totalCount}/>
+        factory: () => <Heading trackerCompanies={stats.few.trackerCompanies} totalCount={stats.few.totalCount} />
     },
     'stats.heading.none': {
-        factory: () => <Heading trackerCompanies={stats.none.trackerCompanies} totalCount={stats.none.totalCount}/>
+        factory: () => <Heading trackerCompanies={stats.none.trackerCompanies} totalCount={stats.none.totalCount} />
     },
+    'rmf-small': {
+        factory: () => (
+            <RemoteMessagingFramework
+                messageType="small"
+                title="Update Available"
+                descriptionText="A new version of DuckDuckGo Browser is available. Update now to enjoy improved privacy features and enhanced performance."
+            />
+        )
+    },
+    'rmf-medium': {
+        factory: () => (
+            <RemoteMessagingFramework
+                messageType="medium"
+                icon="Announce"
+                title="Tell Us Your Thoughts on Privacy Pro"
+                descriptionText="A new version of DuckDuckGo Browser is available. Update now to enjoy improved privacy features and enhanced performance."
+            />
+        )
+    },
+    'rmf-big-single-action': {
+        factory: () => (
+            <RemoteMessagingFramework
+                messageType="big-single-action"
+                icon="DDGAnnounce"
+                title="New Search Feature!"
+                descriptionText="DuckDuckGo now offers Instant Answers for quicker access to the information you need."
+                primaryAction={() => { }}
+                primaryActionText="Learn More"
+            />
+        )
+    }
 }
 
 export const otherExamples = {
@@ -40,7 +72,7 @@ export const otherExamples = {
                 expansion: "expanded",
                 animation: { kind: "none" }
             }}
-        ><PrivacyStatsConsumer/></PrivacyStatsMockProvider>
+        ><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
     },
     'stats.with-view-transitions': {
         factory: () => <PrivacyStatsMockProvider
@@ -49,6 +81,6 @@ export const otherExamples = {
                 expansion: "expanded",
                 animation: { kind: "view-transitions" }
             }}
-        ><PrivacyStatsConsumer/></PrivacyStatsMockProvider>
+        ><PrivacyStatsConsumer /></PrivacyStatsMockProvider>
     },
 }
