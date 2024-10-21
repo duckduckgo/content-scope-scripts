bb9c497faa5ea303d263e9822f18868a727b788c
Shane Osbourne <shane.osbourne8@gmail.com>
2024-10-21 16:31:49 +0100

    ntp: keyboard support for stats (#1149)
    * ntp: keyboard support
    
    * linting
    
    ---------
    
    Co-authored-by: Shane Osbourne <sosbourne@duckduckgo.com>

--- special-pages/pages/new-tab/app/privacy-stats/integration-tests/privacy-stats.spec.js
+++ special-pages/pages/new-tab/app/privacy-stats/integration-tests/privacy-stats.spec.js
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test'
-import { NewtabPage } from './page-objects/newtab'
+import { NewtabPage } from '../../../integration-tests/new-tab.page.js'
 
 test.describe('newtab privacy stats', () => {
     test('fetches config + stats', async ({ page }, workerInfo) => {
         const ntp = NewtabPage.create(page, workerInfo)
         await ntp.reducedMotion()
         await ntp.openPage()
+
         const calls1 = await ntp.mocks.waitForCallCount({ method: 'initialSetup', count: 1 })
         const calls2 = await ntp.mocks.waitForCallCount({ method: 'stats_getData', count: 1 })
         const calls3 = await ntp.mocks.waitForCallCount({ method: 'stats_getConfig', count: 1 })
