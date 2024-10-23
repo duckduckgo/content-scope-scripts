6ccec737e5fa923d9d925af365b794f79cf9467f
Marcos Gurgel <mgurgel@gmail.com>
2024-10-23 20:08:09 +0100

    Simplified step

--- special-pages/pages/onboarding/app/components/v3/DuckPlayerStep.js
+++ special-pages/pages/onboarding/app/components/v3/DuckPlayerStep.js
@@ -1,8 +1,8 @@
 import { h } from 'preact'
-import { useEffect, useRef, useState } from 'preact/hooks'
-import { useEnv } from '../../../../../shared/components/EnvironmentProvider'
+// import { useEffect, useRef, useState } from 'preact/hooks'
+// import { useEnv } from '../../../../../shared/components/EnvironmentProvider'
 import { RiveAnimation } from '../../components/RiveAnimation'
-import { useBeforeAfter } from './BeforeAfterProvider'
+// import { useBeforeAfter } from './BeforeAfterProvider'
 import { SlideIn } from './Animation'
 
 import onboardingAnimation from '../../animations/Onboarding.riv'
@@ -10,26 +10,27 @@ import onboardingAnimation from '../../animations/Onboarding.riv'
 import styles from './DuckPlayerStep.module.css'
 
 export function DuckPlayerStep () {
-    const { isDarkMode, isReducedMotion } = useEnv()
-    const [canPlay, setCanPlay] = useState(false)
-    const { getStep, setStep } = useBeforeAfter()
-    /** @type {import('preact/hooks').MutableRef<ReturnType<setTimeout>|null>} */
-    const timer = useRef(null)
+    // const { isDarkMode, isReducedMotion } = useEnv()
+    // const [canPlay, setCanPlay] = useState(false)
+    // const { getStep, setStep } = useBeforeAfter()
+    // /** @type {import('preact/hooks').MutableRef<ReturnType<setTimeout>|null>} */
+    // const timer = useRef(null)
 
-    useEffect(() => {
-        if (canPlay && !timer.current) {
-            timer.current = setTimeout(() => {
-                setStep('duckPlayerSingle', 'after')
-            }, isReducedMotion ? 100 : 0)
-        }
+    // useEffect(() => {
+    //     if (canPlay && !timer.current) {
+    //         timer.current = setTimeout(() => {
+    //             setStep('duckPlayerSingle', 'after')
+    //         }, isReducedMotion ? 100 : 0)
+    //     }
 
-        return () => {
-            if (timer.current) clearTimeout(timer.current)
-        }
-    }, [canPlay, isReducedMotion])
+    //     return () => {
+    //         if (timer.current) clearTimeout(timer.current)
+    //     }
+    // }, [canPlay, isReducedMotion])
 
     const animationDidEnd = () => {
-        if (!timer.current) setCanPlay(true)
+        console.log('ANIMATION ENDED')
+        // if (!timer.current) setCanPlay(true)
     }
 
     return (
@@ -37,8 +38,8 @@ export function DuckPlayerStep () {
             <div className={styles.animationContainer}>
                 <RiveAnimation
                     animation={onboardingAnimation}
-                    state={getStep('duckPlayerSingle') || 'before'}
-                    isDarkMode={isDarkMode}
+                    state={'before'}
+                    isDarkMode={false}
                     artboard='Duck Player'
                     inputName='Duck Player?'
                     stateMachine='State Machine 2'
