1076381906f4a7c6a1c8575ef82c04c0e6aba5ed
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-10-28 09:42:42 -0600

    RMF Docs (#1170)
    * docs: Add RMF Service spec docs, examples
    
    * fix: Examples
    
    * docs: Update new-tab app
    
    * fix: RMF service

--- special-pages/pages/new-tab/app/docs.js
+++ special-pages/pages/new-tab/app/docs.js
@@ -2,8 +2,10 @@
  *
  * - {@link WidgetConfigService}
  * - {@link PrivacyStatsService}
+ * - {@link RMFService}
  *
  * @module NewTab Services
  */
 export * from './privacy-stats/privacy-stats.service.js'
 export * from './widget-list/widget-config.service.js'
+export * from './remote-messaging-framework/rmf.service.js'
