7e61895f8bd0cff1cfd42b3ccc00a51aa93f474b
Shane Osbourne <sosbourne@duckduckgo.com>
2024-10-29 14:00:12 +0000

    disable CSS nesting in special pages output

--- special-pages/index.mjs
+++ special-pages/index.mjs
@@ -143,6 +143,9 @@ for (const buildJob of buildJobs) {
             format: 'iife',
             // external: ['../assets/img/*'],
             sourcemap: NODE_ENV === 'development',
+            supported: {
+                nesting: false,
+            },
             loader: {
                 '.js': 'jsx',
                 '.module.css': 'local-css',
