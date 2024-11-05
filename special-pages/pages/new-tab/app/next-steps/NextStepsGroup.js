80f1435e5079cbcf8ed92cff95d7e4015b22ed50
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-11-04 20:09:41 -0700

    fix: lint

--- special-pages/pages/new-tab/app/next-steps/NextStepsGroup.js
+++ special-pages/pages/new-tab/app/next-steps/NextStepsGroup.js
@@ -2,4 +2,6 @@ import { h } from 'preact';
 import cn from 'classnames';
 import styles from './NextSteps.module.css';
 
-export function NextStepsCardGroup({ cards, expansion }) {}
+export function NextStepsCardGroup({ cards, expansion }) {
+    return <div class={cn(styles.cardGroup)}>Card group</div>;
+}
