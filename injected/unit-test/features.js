f1c63237cc61c4d8bf4ba811d8f80231fd2478cb
Jonathan Kingston <jkingston@duckduckgo.com>
2024-11-06 18:37:53 +0000

    Fix unit test

--- injected/unit-test/features.js
+++ injected/unit-test/features.js
@@ -17,6 +17,7 @@ describe('Features definition', () => {
             'navigatorInterface',
             'elementHiding',
             'exceptionHandler',
+            'apiManipulation',
         ]);
     });
 });
