327a32becbf52f90f0a8c3cdbfa9a02d5084a95c
Jonathan Kingston <jkingston@duckduckgo.com>
2024-11-08 16:30:41 +0000

    Switch to using ts direct from privacy config

--- injected/scripts/types.mjs
+++ injected/scripts/types.mjs
@@ -1,13 +1,8 @@
 import { cwd, isLaunchFile } from '../../scripts/script-utils.js';
-import { dirname, join } from 'node:path';
-import { createRequire } from 'node:module';
+import { join } from 'node:path';
 import { buildTypes } from '../../types-generator/build-types.mjs';
 
 const injectRoot = join(cwd(import.meta.url), '..');
-
-// eslint-disable-next-line no-redeclare
-const require = createRequire(import.meta.url);
-const configBuilderRoot = dirname(require.resolve('config-builder'));
 
 /** @type {Record<string, import('../../types-generator/build-types.mjs').Mapping>} */
 const injectSchemaMapping = {
@@ -16,20 +11,6 @@ const injectSchemaMapping = {
      *  - `schema` should be an absolute path to a valid JSON Schema document
      *  - `types` should be an absolute path to the output file
      */
-    'Webcompat Settings': {
-        schema: join(configBuilderRoot, 'tests/schemas/webcompat-settings.json'),
-        types: join(injectRoot, 'src/types/webcompat-settings.d.ts'),
-        // todo: fix this on windows.
-        exclude: process.platform === 'win32',
-        kind: 'settings',
-    },
-    'Duckplayer Settings': {
-        schema: join(configBuilderRoot, 'tests/schemas/duckplayer-settings.json'),
-        types: join(injectRoot, 'src/types/duckplayer-settings.d.ts'),
-        // todo: fix this on windows.
-        exclude: process.platform === 'win32',
-        kind: 'settings',
-    },
     'Schema Messages': {
         schemaDir: join(injectRoot, 'src/messages'),
         typesDir: join(injectRoot, 'src/types'),
