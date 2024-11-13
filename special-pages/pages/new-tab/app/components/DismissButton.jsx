72409600ad9faa8ee56349937bb2bd9a95369d77
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-11-13 09:29:10 -0700

    chore: Address PR comments

--- special-pages/pages/new-tab/app/components/DismissButton.jsx
+++ special-pages/pages/new-tab/app/components/DismissButton.jsx
@@ -4,8 +4,6 @@ import { Cross } from './Icons';
 import { useTypedTranslation } from '../types';
 import styles from './DismissButton.module.css';
 
-const dismissText = (_t) => _t('ntp_dismiss');
-
 /*
  * @param {object} props
  * @param {string} [props.className]
@@ -14,7 +12,7 @@ const dismissText = (_t) => _t('ntp_dismiss');
 export function DismissButton({ className, onClick }) {
     const { t } = useTypedTranslation();
     return (
-        <button class={cn(styles.btn, className)} onClick={onClick} aria-label={dismissText(t)} data-testid="dismissBtn">
+        <button class={cn(styles.btn, className)} onClick={onClick} aria-label={() => t('ntp_dismiss')} data-testid="dismissBtn">
             <Cross />
         </button>
     );
