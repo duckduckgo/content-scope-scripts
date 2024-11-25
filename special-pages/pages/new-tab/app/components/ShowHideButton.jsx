1ac7bf619733d9923a90e4904dad5489d54accfc
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-11-25 15:36:29 -0700

    chore: Modify the showHide button

--- special-pages/pages/new-tab/app/components/ShowHideButton.jsx
+++ special-pages/pages/new-tab/app/components/ShowHideButton.jsx
@@ -1,21 +1,26 @@
 import styles from './ShowHide.module.css';
 import cn from 'classnames';
-import { ChevronButton } from './Icons.js';
-import { h } from 'preact';
+import { ChevronButton, Chevron } from './Icons.js';
+import { Fragment, h } from 'preact';
 
 /**
  * Function to handle showing or hiding content based on certain conditions.
  *
  * @param {Object} props - Input parameters for controlling the behavior of the ShowHide functionality.
  * @param {string} props.text
+ * @param {boolean} [props.showText]
  * @param {() => void} props.onClick
  * @param {'none'|'round'} [props.shape]
  * @param {import("preact").ComponentProps<'button'>} [props.buttonAttrs]
  */
-export function ShowHideButton({ text, onClick, buttonAttrs = {}, shape = 'none' }) {
+export function ShowHideButton({ text, onClick, buttonAttrs = {}, shape = 'none', showText = false }) {
     return (
         <button {...buttonAttrs} class={cn(styles.button, shape === 'round' && styles.round)} aria-label={text} onClick={onClick}>
+            {showText ? (
+                <Fragment>{text}<Chevron /></Fragment>
+            ) : (
             <ChevronButton />
+            )}
         </button>
     );
 }
