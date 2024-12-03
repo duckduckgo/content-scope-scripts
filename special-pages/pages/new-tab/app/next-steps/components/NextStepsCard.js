fc29ac40e179e0e1df188f6cda3bf1b66df97141
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-12-03 09:53:04 -0700

    feat: Add confirmation to Mac Next Steps card

--- special-pages/pages/new-tab/app/next-steps/components/NextStepsCard.js
+++ special-pages/pages/new-tab/app/next-steps/components/NextStepsCard.js
@@ -3,6 +3,8 @@ import styles from './NextSteps.module.css';
 import { DismissButton } from '../../components/DismissButton';
 import { variants } from '../nextsteps.data';
 import { useTypedTranslationWith } from '../../types';
+import { CheckColor } from '../../components/Icons';
+import { useState } from 'preact/hooks';
 
 /**
  * @param {object} props
@@ -14,14 +16,30 @@ import { useTypedTranslationWith } from '../../types';
 export function NextStepsCard({ type, dismiss, action }) {
     const { t } = useTypedTranslationWith(/** @type {import("../strings.json")} */ ({}));
     const message = variants[type]?.(t);
+    const [showMacConfirmation, setShowMacConfirmation] = useState(false);
+
+    const handleClick = () => {
+        if (message.id !== 'addAppToDockMac') {
+            return action(message.id);
+        }
+        action(message.id);
+        setShowMacConfirmation(true);
+    };
     return (
         <div class={styles.card}>
             <img src={`./icons/${message.icon}-128.svg`} alt="" class={styles.icon} />
             <h3 class={styles.title}>{message.title}</h3>
             <p class={styles.description}>{message.summary}</p>
-            <button class={styles.btn} onClick={() => action(message.id)}>
-                {message.actionText}
-            </button>
+            {message.id === 'addAppToDockMac' && !!showMacConfirmation ? (
+                <div class={styles.confirmation}>
+                    <CheckColor />
+                    <p>Added to Dock!</p>
+                </div>
+            ) : (
+                <button class={styles.btn} onClick={handleClick}>
+                    {message.actionText}
+                </button>
+            )}
 
             <DismissButton className={styles.dismissBtn} onClick={() => dismiss(message.id)} />
         </div>
