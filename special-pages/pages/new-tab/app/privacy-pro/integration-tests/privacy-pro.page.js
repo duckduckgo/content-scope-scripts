0a6d63d060f7c12c264688aea598005542ad84fe
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-12-05 13:34:39 -0700

    fix: lint/testing stuff for deploy

--- special-pages/pages/new-tab/app/privacy-pro/integration-tests/privacy-pro.page.js
+++ special-pages/pages/new-tab/app/privacy-pro/integration-tests/privacy-pro.page.js
@@ -1,54 +1,54 @@
-import { data } from '../mocks/stats.js';
-import { expect } from '@playwright/test';
+// import { data } from '../mocks/stats.js';
+// import { expect } from '@playwright/test';
 
-export class PrivacyProPage {
-    /**
-     * @param {import("@playwright/test").Page} page
-     * @param {import("../../../integration-tests/new-tab.page.js").NewtabPage} ntp
-     */
-    constructor(page, ntp) {
-        this.page = page;
-        this.ntp = ntp;
-    }
+// export class PrivacyProPage {
+//     /**
+//      * @param {import("@playwright/test").Page} page
+//      * @param {import("../../../integration-tests/new-tab.page.js").NewtabPage} ntp
+//      */
+//     constructor(page, ntp) {
+//         this.page = page;
+//         this.ntp = ntp;
+//     }
 
-    /**
-     * @param {object} params
-     * @param {number} params.count
-     */
-    async receive({ count }) {
-        /** @type {import("../../../types/new-tab.js").PrivacyProData} */
-        const next = data.basic;
-        await this.ntp.mocks.simulateSubscriptionMessage('privacyPro_onDataUpdate', next);
-    }
+//     /**
+//      * @param {object} params
+//      * @param {number} params.count
+//      */
+//     async receive({ count }) {
+//         /** @type {import("../../../types/new-tab.js").PrivacyProData} */
+//         const next = data.basic;
+//         await this.ntp.mocks.simulateSubscriptionMessage('privacyPro_onDataUpdate', next);
+//     }
 
-    /**
-     * @param {import("../../../types/new-tab.js").PrivacyProData} data
-     */
-    async receiveData(data) {
-        await this.ntp.mocks.simulateSubscriptionMessage('privacyPro_onDataUpdate', data);
-    }
+//     /**
+//      * @param {import("../../../types/new-tab.js").PrivacyProData} data
+//      */
+//     async receiveData(data) {
+//         await this.ntp.mocks.simulateSubscriptionMessage('privacyPro_onDataUpdate', data);
+//     }
 
-    context() {
-        return this.page.locator('[data-entry-point="privacyPro"]');
-    }
+//     context() {
+//         return this.page.locator('[data-entry-point="privacyPro"]');
+//     }
 
-    rows() {
-        return this.context().getByTestId('CompanyList').locator('li');
-    }
+//     rows() {
+//         return this.context().getByTestId('CompanyList').locator('li');
+//     }
 
-    /**
-     * @param {number} count
-     */
-    async hasRows(count) {
-        const rows = this.rows();
-        expect(await rows.count()).toBe(count);
-    }
+//     /**
+//      * @param {number} count
+//      */
+//     async hasRows(count) {
+//         const rows = this.rows();
+//         expect(await rows.count()).toBe(count);
+//     }
 
-    async showMoreSecondary() {
-        await this.context().getByLabel('Show More', { exact: true }).click();
-    }
+//     async showMoreSecondary() {
+//         await this.context().getByLabel('Show More', { exact: true }).click();
+//     }
 
-    async showLessSecondary() {
-        await this.context().getByLabel('Show Less', { exact: true }).click();
-    }
-}
+//     async showLessSecondary() {
+//         await this.context().getByLabel('Show Less', { exact: true }).click();
+//     }
+// }
