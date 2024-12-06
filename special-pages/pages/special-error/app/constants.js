688204414e0d818398608a8151b98c41506e479f
Marcos Gurgel <mgurgel@gmail.com>
2024-12-06 13:24:45 +0000

    Final URLs

--- special-pages/pages/special-error/app/constants.js
+++ special-pages/pages/special-error/app/constants.js
@@ -1,3 +1,3 @@
 export const phishingMalwareHelpPageURL =
-    'https://dub.duckduckgo.com/pages/duckduckgo/mgurgel-help-pages/privacy/phishing-and-malware-protection/';
-export const reportSiteAsSafeFormURL = 'https://use-devtesting12.duckduckgo.com/malicious-site-protection/report-error';
+    'https://duckduckgo.com/duckduckgo-help-pages/privacy/phishing-and-malware-protection/';
+export const reportSiteAsSafeFormURL = 'https://duckduckgo.com/malicious-site-protection/report-error';
