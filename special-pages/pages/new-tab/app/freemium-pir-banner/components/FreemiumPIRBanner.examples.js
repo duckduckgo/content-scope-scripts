35c3672a1003a44956bc724645a708220bcdfab5
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-12-13 11:24:47 -0700

    fix: compnents view

--- special-pages/pages/new-tab/app/freemium-pir-banner/components/FreemiumPIRBanner.examples.js
+++ special-pages/pages/new-tab/app/freemium-pir-banner/components/FreemiumPIRBanner.examples.js
@@ -9,7 +9,7 @@ export const FreemiumPIRBannerExamples = {
     'freemiumPIR.onboarding': {
         factory: () => (
             <FreemiumPIRBanner
-                message={freemiumPIRDataExamples.unused.content}
+                message={freemiumPIRDataExamples.onboarding.content}
                 dismiss={noop('freemiumPIRBanner_dismiss')}
                 action={noop('freemiumPIRBanner_action')}
             />
@@ -18,21 +18,9 @@ export const FreemiumPIRBannerExamples = {
     'freemiumPIR.scan_results': {
         factory: () => (
             <FreemiumPIRBanner
-                message={freemiumPIRDataExamples.used.content}
+                message={freemiumPIRDataExamples.scan_results.content}
                 dismiss={noop('freemiumPIRBanner_dismiss')}
                 action={noop('freemiumPIRBanner_action')}
-            />
-        ),
-    },
-};
-
-export const otherFreemiumPIRBannerExamples = {
-    'freemiumPIR.big-two-action-overflow': {
-        factory: () => (
-            <FreemiumPIRBanner
-                message={freemiumPIRDataExamples.big_two_action_overflow.content}
-                action={noop('freemiumPIRBanner_action')}
-                dismiss={noop('freemiumPIRBanner_dismiss')}
             />
         ),
     },
