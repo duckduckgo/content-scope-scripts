30e53a8fa057c1d4512ff3cbbb9381b61c5826f0
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-12-13 20:59:56 -0700

    chore: wat

--- special-pages/pages/new-tab/app/components/ShowHideButton.jsx
+++ special-pages/pages/new-tab/app/components/ShowHideButton.jsx
@@ -19,7 +19,7 @@ export function ShowHideButton({ text, onClick, buttonAttrs = {}, shape = 'none'
      */
     const handleClick = (e) => {
         onClick();
-        if (e.target) {
+        if (e?.target) {
             // @ts-ignore
             e.target.blur();
         }
