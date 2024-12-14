5b0e51e095def79b52b716b609a37ce333fd0a0a
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2024-12-13 17:56:06 -0700

    chore: Move convertMarkdownToHTMLForStrongTags to util file

--- special-pages/pages/new-tab/app/freemium-pir-banner/components/FreemiumPIRBanner.js
+++ special-pages/pages/new-tab/app/freemium-pir-banner/components/FreemiumPIRBanner.js
@@ -5,6 +5,7 @@ import { DismissButton } from '../../components/DismissButton';
 import styles from './FreemiumPIRBanner.module.css';
 import { FreemiumPIRBannerContext } from '../FreemiumPIRBannerProvider';
 import { useContext } from 'preact/hooks';
+import { convertMarkdownToHTMLForStrongTags } from '../freemiumPIRBanner.utils';
 
 /**
  * @typedef { import("../../../types/new-tab").FreemiumPIRBannerMessage} FreemiumPIRBannerMessage
@@ -15,32 +16,24 @@ import { useContext } from 'preact/hooks';
  */
 
 export function FreemiumPIRBanner({ message, action, dismiss }) {
-    const convertMarkdownToHTML = (markdown) => {
-        // Use a regular expression to find all the words wrapped in **
-        const regex = /\*\*(.*?)\*\*/g;
-
-        // Replace the matched text with the HTML <strong> tags
-        const result = markdown.replace(regex, '<strong>$1</strong>');
-        return result;
-    };
-
+    const processedMessageDescription = convertMarkdownToHTMLForStrongTags(message.descriptionText);
     return (
-        <div id={message?.id} class={cn(styles.root, styles.icon)}>
+        <div id={message.id} class={cn(styles.root, styles.icon)}>
             <span class={styles.iconBlock}>
                 <img src={`./icons/Information-Remover-96.svg`} alt="" />
             </span>
             <div class={styles.content}>
-                {message?.titleText && <h2 class={styles.title}>{message.titleText}</h2>}
-                <p class={styles.description} dangerouslySetInnerHTML={{ __html: convertMarkdownToHTML(message?.descriptionText) }} />
+                {message.titleText && <h2 class={styles.title}>{message.titleText}</h2>}
+                <p class={styles.description} dangerouslySetInnerHTML={{ __html: processedMessageDescription }} />
             </div>
-            {message?.messageType === 'big_single_action' && message?.actionText && action && (
+            {message.messageType === 'big_single_action' && message?.actionText && action && (
                 <div class={styles.btnBlock}>
                     <Button variant="standard" onClick={() => action(message.id)}>
                         {message.actionText}
                     </Button>
                 </div>
             )}
-            {message?.id && dismiss && <DismissButton className={styles.dismissBtn} onClick={() => dismiss(message.id)} />}
+            {message.id && dismiss && <DismissButton className={styles.dismissBtn} onClick={() => dismiss(message.id)} />}
         </div>
     );
 }
@@ -48,7 +41,6 @@ export function FreemiumPIRBanner({ message, action, dismiss }) {
 export function FreemiumPIRBannerConsumer() {
     const { state, action, dismiss } = useContext(FreemiumPIRBannerContext);
 
-    // `state.data.content` can be empty - meaning there's no message to display!
     if (state.status === 'ready' && state.data.content) {
         return <FreemiumPIRBanner message={state.data.content} action={action} dismiss={dismiss} />;
     }
