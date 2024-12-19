eaac9c4c155d0484fc5e7093a4c7ad7661363e11
Shane Osbourne <shane.osbourne8@gmail.com>
2024-12-19 11:23:51 +0000

    ntp: prep a re-usable switch (#1339)

--- special-pages/shared/components/Switch/Switch.js
+++ special-pages/shared/components/Switch/Switch.js
@@ -1,22 +1,20 @@
 import { h } from 'preact';
 import styles from './Switch.module.css';
-import { useEnv } from '../../../../shared/components/EnvironmentProvider';
 
 /**
  * Switch component used to toggle between two states.
  *
  * @param {Object} props - The props object.
  * @param {string} props.ariaLabel - Label applied to the <input />
- * @param {ImportMeta['injectName']} [props.variant]
+ * @param {"macos" | "windows"} props.platformName - only supported platforms
+ * @param {"light" | "dark"} props.theme
  * @param {boolean} props.pending - Indicates if the switch is in a pending state.
  * @param {boolean} [props.checked=false] - Indicates if the switch is checked.
  * @param {Function} props.onChecked - Callback function to be called when the switch is checked.
  * @param {Function} props.onUnchecked - Callback function to be called when the switch is unchecked.
  */
-export function Switch({ checked = false, variant, ...props }) {
+export function Switch({ checked = false, platformName, theme, ...props }) {
     const { onChecked, onUnchecked, ariaLabel, pending } = props;
-    const env = useEnv();
-    const platform = variant || env.injectName;
     function change(e) {
         if (e.target.checked === true) {
             onChecked();
@@ -25,17 +23,17 @@ export function Switch({ checked = false, variant, ...props }) {
         }
     }
     return (
-        <label className={styles.toggleSwitch} data-variant={platform}>
+        <label class={styles.label} data-platform-name={platformName} data-theme={theme}>
             <input
                 disabled={pending}
                 type="checkbox"
                 role="switch"
                 aria-label={ariaLabel}
-                className={styles.input}
+                class={styles.input}
                 checked={checked}
                 onChange={change}
             />
-            <span className={styles.switch} style="transition-duration: 130ms;transition-delay: 0ms;"></span>
+            <span class={styles.switch} style="transition-duration: 130ms;transition-delay: 0ms;"></span>
         </label>
     );
 }
