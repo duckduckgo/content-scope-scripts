f7a573e90ca6d21ff0f5032f1b1666094812390a
Shane Osbourne <shane.osbourne8@gmail.com>
2025-01-17 17:24:48 +0000

    ntp: shipreview changes to favorites (#1405)
    * ntp: shipreview changes to favorites
    
    * compare id

---
+++ special-pages/pages/new-tab/app/favorites/unit-tests/color.spec.mjs
@@ -0,0 +1,63 @@
+import assert from 'node:assert';
+import { test } from 'node:test';
+import { urlToColor } from '../getColorForString.js';
+import fc from 'fast-check';
+
+/**
+ * These values were extracted from the running macOS application
+ * These tests ensure our JS version matches what users have seen already in macOS
+ */
+const macosValues = [
+    { input: 'google.pl', expected: '#ECCC7B' },
+    { input: 'wikipedia.org', expected: '#DD6B4C' },
+    { input: 'twitter.com', expected: '#99DB7A' },
+    { input: 'icloud.com', expected: '#645468' },
+    { input: 'apple.com', expected: '#855DB6' },
+    { input: 'apple.com', expected: '#855DB6' },
+    { input: 'polskieradio.pl', expected: '#99DB7A' },
+    { input: 'example.com', expected: '#645468' },
+    { input: 'google.com', expected: '#D65D62' },
+    { input: 'google.pl', expected: '#ECCC7B' },
+    { input: 'machars.net', expected: '#6BB4EF' },
+    { input: 'badssl.com', expected: '#99DB7A' },
+    { input: 'bing.com', expected: '#645468' },
+    { input: 'facebook.com', expected: '#855DB6' },
+    { input: 'example.com', expected: '#645468' },
+    { input: 'github.com', expected: '#5E5ADB' },
+    { input: 'example.com', expected: '#645468' },
+    { input: 'gumroad.com', expected: '#D65D62' },
+    { input: 'trainingpeaks.com', expected: '#DD6B4C' },
+    { input: 'youtube.com', expected: '#727998' },
+    { input: 'polskieradio.pl', expected: '#99DB7A' },
+    { input: 'mbed.com', expected: '#55D388' },
+];
+
+test(`Test urlToColor on macos`, () => {
+    for (const testcase of macosValues) {
+        const result = urlToColor(testcase.input);
+        assert.strictEqual(result, testcase.expected, `Expected "${testcase.expected}", but got "${result}"`);
+    }
+});
+
+test('urlToColor never throws', () => {
+    fc.assert(
+        fc.property(fc.anything(), (input) => {
+            assert.doesNotThrow(() => urlToColor(/** @type {any} */ (input)));
+        }),
+        { verbose: true },
+    );
+});
+
+test('urlToColor returns string or null', () => {
+    fc.assert(
+        fc.property(fc.anything(), (input) => {
+            const result = urlToColor(/** @type {any} */ (input));
+            assert.strictEqual(
+                typeof result === 'string' || result === null,
+                true,
+                `Expected result to be string or null, but got "${result}"`,
+            );
+        }),
+        { verbose: true },
+    );
+});
--- special-pages/pages/new-tab/types/new-tab.ts
+++ special-pages/pages/new-tab/types/new-tab.ts
@@ -502,7 +502,14 @@ export interface FavoritesData {
   favorites: Favorite[];
 }
 export interface Favorite {
+  /**
+   * The full url that will be navigated to (including path, query params and hash)
+   */
   url: string;
+  /**
+   * The eTLD+1 of the URL, representing the effective top-level domain and one second-level domain
+   */
+  etldPlusOne: null | string;
   id: string;
   title: string;
   favicon: null | FavoriteFavicon;
