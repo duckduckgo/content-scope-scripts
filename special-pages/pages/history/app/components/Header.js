8a043e3dd8e4e2b40197f60d203e59706bda3a1f
Shane Osbourne <shane.osbourne8@gmail.com>
2025-02-13 16:54:33 +0000

    history: adding item selection (#1486)
    * history: adding item selection
    
    * refactor handlers
    
    * single row selection
    
    * global handlers

--- special-pages/pages/history/app/components/Header.js
+++ special-pages/pages/history/app/components/Header.js
@@ -1,20 +1,22 @@
 import styles from './Header.module.css';
 import { h } from 'preact';
 import { useComputed } from '@preact/signals';
-import { SearchForm, useSearchContext } from './SearchForm.js';
+import { SearchForm } from './SearchForm.js';
 import { Trash } from '../icons/Trash.js';
 import { useTypedTranslation } from '../types.js';
+import { useQueryContext } from '../global-state/QueryProvider.js';
+import { BTN_ACTION_DELETE_ALL } from '../constants.js';
 
 /**
  */
 export function Header() {
     const { t } = useTypedTranslation();
-    const search = useSearchContext();
+    const search = useQueryContext();
     const term = useComputed(() => search.value.term);
     return (
         <div class={styles.root}>
             <div class={styles.controls}>
-                <button class={styles.largeButton} data-delete-all>
+                <button class={styles.largeButton} data-action={BTN_ACTION_DELETE_ALL}>
                     <span>{t('delete_all')}</span>
                     <Trash />
                 </button>
