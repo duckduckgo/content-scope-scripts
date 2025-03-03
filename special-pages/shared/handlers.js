12bbe33d52ce505f72dc5a83fc7dcb3aac179dfc
Shane Osbourne <shane.osbourne8@gmail.com>
2025-03-03 20:18:04 +0000

    history: use double-click for opening links (#1534)

--- special-pages/shared/handlers.js
+++ special-pages/shared/handlers.js
@@ -1,5 +1,5 @@
 /**
- * @param {MouseEvent} event
+ * @param {MouseEvent|KeyboardEvent} event
  * @param {ImportMeta['platform']} platformName
  * @return {'new-tab' | 'new-window' | 'same-tab'}
  */
@@ -7,7 +7,7 @@ export function eventToTarget(event, platformName) {
     const isControlClick = platformName === 'macos' ? event.metaKey : event.ctrlKey;
     if (isControlClick) {
         return 'new-tab';
-    } else if (event.shiftKey || event.button === 1 /* middle click */) {
+    } else if (event.shiftKey || ('button' in event && event.button === 1) /* middle click */) {
         return 'new-window';
     }
     return 'same-tab';
