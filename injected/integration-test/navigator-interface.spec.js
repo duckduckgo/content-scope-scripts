27b7e0fc13c5ce5c3d16e853bbfcc2604a700825
Shane Osbourne <shane.osbourne8@gmail.com>
2025-03-20 11:22:22 +0000

    allow non-protection features to survive protections toggle (#1564)
    * injected: only call 'load' on enabled features
    
    * prettier stuff
    
    * pr feedback
    
    * remove refactoring mistake

--- injected/integration-test/navigator-interface.spec.js
+++ injected/integration-test/navigator-interface.spec.js
@@ -8,7 +8,7 @@ const test = testContextForExtension(base);
 
 test.describe('Ensure navigator interface is injected', () => {
     test('should expose navigator.navigator.isDuckDuckGo(): Promise<boolean> and platform === "extension"', async ({ page }) => {
-        await gotoAndWait(page, '/blank.html', { platform: { name: 'extension' } });
+        await gotoAndWait(page, '/blank.html');
         const isDuckDuckGoResult = await page.evaluate(() => {
             // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
             const fn = navigator.duckduckgo?.isDuckDuckGo;
