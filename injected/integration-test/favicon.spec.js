73cf52f97f4ea03e94f92b345e4e165448e6c659
Shane Osbourne <shane.osbourne8@gmail.com>
2025-03-20 11:20:03 +0000

    full href value

--- injected/integration-test/favicon.spec.js
+++ injected/integration-test/favicon.spec.js
@@ -4,21 +4,22 @@ import { ResultsCollector } from './page-objects/results-collector.js';
 const HTML = '/favicon/index.html';
 const CONFIG = './integration-test/test-pages/favicon/config/favicon-enabled.json';
 
-test('favicon feature absent', async ({ page }, testInfo) => {
+test('favicon feature absent', async ({ page, baseURL }, testInfo) => {
     const CONFIG = './integration-test/test-pages/favicon/config/favicon-absent.json';
     const favicon = ResultsCollector.create(page, testInfo.project.use);
     await favicon.load(HTML, CONFIG);
 
     // ensure first favicon item was sent
     const messages = await favicon.waitForMessage('faviconFound', 1);
+    const url = new URL('/favicon/favicon.png', baseURL);
 
     expect(messages[0].payload.params).toStrictEqual({
-        favicons: [{ href: './favicon.png', rel: 'shortcut icon' }],
+        favicons: [{ href: url.href, rel: 'shortcut icon' }],
         documentUrl: 'http://localhost:3220/favicon/index.html',
     });
 });
 
-test('favicon + monitor', async ({ page }, testInfo) => {
+test('favicon + monitor', async ({ page, baseURL }, testInfo) => {
     const favicon = ResultsCollector.create(page, testInfo.project.use);
     await favicon.load(HTML, CONFIG);
 
@@ -31,18 +32,21 @@ test('favicon + monitor', async ({ page }, testInfo) => {
     // wait for the second message
     const messages = await favicon.waitForMessage('faviconFound', 2);
 
+    const url1 = new URL('/favicon/favicon.png', baseURL);
+    const url2 = new URL('/favicon/new_favicon.png', baseURL);
+
     expect(messages[0].payload.params).toStrictEqual({
-        favicons: [{ href: './favicon.png', rel: 'shortcut icon' }],
+        favicons: [{ href: url1.href, rel: 'shortcut icon' }],
         documentUrl: 'http://localhost:3220/favicon/index.html',
     });
 
     expect(messages[1].payload.params).toStrictEqual({
-        favicons: [{ href: './new_favicon.png', rel: 'shortcut icon' }],
+        favicons: [{ href: url2.href, rel: 'shortcut icon' }],
         documentUrl: 'http://localhost:3220/favicon/index.html',
     });
 });
 
-test('favicon + monitor (many updates)', async ({ page }, testInfo) => {
+test('favicon + monitor (many updates)', async ({ page, baseURL }, testInfo) => {
     const favicon = ResultsCollector.create(page, testInfo.project.use);
     await page.clock.install();
     await favicon.load(HTML, CONFIG);
@@ -66,18 +70,22 @@ test('favicon + monitor (many updates)', async ({ page }, testInfo) => {
     }
 
     {
+        const url1 = new URL('/favicon/favicon.png', baseURL);
+        const url2 = new URL('/favicon/new_favicon.png?count=0', baseURL);
+        const url3 = new URL('/favicon/new_favicon.png?count=1', baseURL);
+
         const messages = await favicon.outgoingMessages();
         expect(messages.map((x) => /** @type {{params: any}} */ (x.payload).params)).toStrictEqual([
             {
-                favicons: [{ href: './favicon.png', rel: 'shortcut icon' }],
+                favicons: [{ href: url1.href, rel: 'shortcut icon' }],
                 documentUrl: 'http://localhost:3220/favicon/index.html',
             },
             {
-                favicons: [{ href: './new_favicon.png?count=0', rel: 'shortcut icon' }],
+                favicons: [{ href: url2.href, rel: 'shortcut icon' }],
                 documentUrl: 'http://localhost:3220/favicon/index.html',
             },
             {
-                favicons: [{ href: './new_favicon.png?count=1', rel: 'shortcut icon' }],
+                favicons: [{ href: url3.href, rel: 'shortcut icon' }],
                 documentUrl: 'http://localhost:3220/favicon/index.html',
             },
         ]);
@@ -98,7 +106,6 @@ test('favicon + monitor disabled', async ({ page }, testInfo) => {
     // now update it
     await page.getByRole('button', { name: 'Set override' }).click();
 
-    //
     await expect(page.locator('link')).toHaveAttribute('href', './new_favicon.png');
 
     // account for the debounce
