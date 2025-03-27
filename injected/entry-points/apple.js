82f329378d4726a1c8939e9c62d44358ca6a59a7
Jonathan Kingston <jkingston@duckduckgo.com>
2025-03-27 15:30:19 +0000

    PoC prototype gating of messaging

--- injected/entry-points/apple.js
+++ injected/entry-points/apple.js
@@ -3,7 +3,8 @@
  */
 import { load, init } from '../src/content-scope-features.js';
 import { processConfig, platformSpecificFeatures } from './../src/utils';
-import { WebkitMessagingConfig } from '../../messaging/index.js';
+import { WebkitMessagingConfig, TestTransportConfig } from '../../messaging/index.js';
+import ConfigFeature from '../src/config-feature.js';
 
 function initCode() {
     // @ts-expect-error https://app.asana.com/0/1201614831475344/1203979574128023/f
@@ -14,18 +15,45 @@ function initCode() {
     const userPreferences = $USER_PREFERENCES$;
 
     const processedConfig = processConfig(config, userUnprotectedDomains, userPreferences, platformSpecificFeatures);
-
-    const handlerNames = [];
-    if (import.meta.injectName === 'apple-isolated') {
-        handlerNames.push('contentScopeScriptsIsolated');
-    } else {
-        handlerNames.push('contentScopeScripts');
+    class AppleMessagingConfig extends ConfigFeature {
+        get messagingConfig() {
+            const handlerNames = [];
+            if (import.meta.injectName === 'apple-isolated') {
+                handlerNames.push('contentScopeScriptsIsolated');
+            } else {
+                handlerNames.push('contentScopeScripts');
+            }
+            // Bail out if feature isn't enabled
+            if (!this.getFeatureSettingEnabled('bloop')) {
+                return new TestTransportConfig({
+                    notify() {
+                        // noop
+                    },
+                    request: async () => {
+                        // noop
+                    },
+                    subscribe() {
+                        return () => {
+                            // noop
+                        };
+                    },
+                });
+            }
+            return new WebkitMessagingConfig({
+                webkitMessageHandlerNames: handlerNames,
+                secret: '',
+                hasModernWebkitAPI: true,
+            });
+        }
     }
-    processedConfig.messagingConfig = new WebkitMessagingConfig({
-        webkitMessageHandlerNames: handlerNames,
-        secret: '',
-        hasModernWebkitAPI: true,
+    const messagingConfig = new AppleMessagingConfig('messaging', {
+        platform: processedConfig.platform,
+        site: processedConfig.site,
+        bundledConfig: processedConfig.bundledConfig,
+        messagingConfig: processedConfig.messagingConfig,
+        messageSecret: processedConfig.messageSecret,
     });
+    processedConfig.messagingConfig = messagingConfig.messagingConfig;
 
     load({
         platform: processedConfig.platform,
