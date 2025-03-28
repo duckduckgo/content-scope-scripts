c40a4dcbb8ab35fd6ac6f0c395edb2e82b454007
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-03-28 15:32:29 +0000

    New Duck Player entry point (#1559)

--- injected/src/features/duckplayer/components/index.js
+++ injected/src/features/duckplayer/components/index.js
@@ -1,6 +1,8 @@
 import { DDGVideoOverlay } from './ddg-video-overlay.js';
 import { customElementsDefine, customElementsGet } from '../../../captured-globals.js';
 import { DDGVideoOverlayMobile } from './ddg-video-overlay-mobile.js';
+import { DDGVideoThumbnailOverlay } from './ddg-video-thumbnail-overlay-mobile.js';
+import { DDGVideoDrawerMobile } from './ddg-video-drawer-mobile.js';
 
 /**
  * Register custom elements in this wrapper function to be called only when we need to
@@ -13,5 +15,11 @@ export function registerCustomElements() {
     }
     if (!customElementsGet(DDGVideoOverlayMobile.CUSTOM_TAG_NAME)) {
         customElementsDefine(DDGVideoOverlayMobile.CUSTOM_TAG_NAME, DDGVideoOverlayMobile);
+    }
+    if (!customElementsGet(DDGVideoDrawerMobile.CUSTOM_TAG_NAME)) {
+        customElementsDefine(DDGVideoDrawerMobile.CUSTOM_TAG_NAME, DDGVideoDrawerMobile);
+    }
+    if (!customElementsGet(DDGVideoThumbnailOverlay.CUSTOM_TAG_NAME)) {
+        customElementsDefine(DDGVideoThumbnailOverlay.CUSTOM_TAG_NAME, DDGVideoThumbnailOverlay);
     }
 }
