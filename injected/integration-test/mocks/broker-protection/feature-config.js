6d6fa5c3c7df1548c78cd76dece9a4200acd806b
Alex <abuliga@duckduckgo.com>
2025-04-02 17:58:34 +0200

    feat(pir): deprecate useEnhancedCaptchaSystem setting

--- injected/integration-test/mocks/broker-protection/feature-config.js
+++ injected/integration-test/mocks/broker-protection/feature-config.js
@@ -3,7 +3,6 @@
  * @param {'enabled' | 'disabled'} [brokerProtection.state] - optional state of the broker protection feature
  * @param {string[]} [brokerProtection.exceptions] - optional list of exceptions
  * @param {object} [brokerProtection.settings] - optional settings
- * @param {'enabled' | 'disabled'} [brokerProtection.settings.useEnhancedCaptchaSystem] - optional flag to use new action handlers
  */
 export function createFeatureConfig(brokerProtection = {}) {
     return {
