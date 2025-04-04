2b4eaabcd705d67f65ff38c4c19bf3faf86e333e
Brian Hall <brian@hack.design>
2025-04-04 12:54:27 +0200

    Add tests

--- injected/integration-test/broker-protection-tests/broker-protection-captcha.spec.js
+++ injected/integration-test/broker-protection-tests/broker-protection-captcha.spec.js
@@ -1,6 +1,6 @@
 import { test as base } from '@playwright/test';
 import { createConfiguredDbpTest } from './fixtures';
-import { createGetRecaptchaInfoAction, createSolveRecaptchaAction } from '../mocks/broker-protection/captcha.js';
+import { createGetRecaptchaInfoAction, createSolveRecaptchaAction, createGetImageCaptchaInfoAction, createSolveImageCaptchaAction } from '../mocks/broker-protection/captcha.js';
 import { BROKER_PROTECTION_CONFIGS } from './tests-config.js';
 
 const test = createConfiguredDbpTest(base);
@@ -97,6 +97,40 @@ test.describe('Broker Protection Captcha', () => {
             const sucessResponse = await dbp.getSuccessResponse();
 
             dbp.isQueryParamRemoved(sucessResponse);
+        });
+    });
+
+    test.describe('image captcha', () => {
+        const imageCaptchaTargetPage = 'image-captcha.html';
+        const imageCaptchaResponseSelector = '#svgCaptchaInputId';
+
+        test.describe('getCaptchaInfo', () => {
+            test('returns the expected response for the correct action data', async ({ createConfiguredDbp }) => {
+                const dbp = await createConfiguredDbp(BROKER_PROTECTION_CONFIGS.default);
+                await dbp.navigatesTo(imageCaptchaTargetPage);
+                await dbp.receivesInlineAction(createGetImageCaptchaInfoAction({ selector: "#svg-captcha-rendering svg" }));
+                const sucessResponse = await dbp.getSuccessResponse();
+                dbp.isCaptchaMatch(sucessResponse, { captchaType: 'image', targetPage: imageCaptchaTargetPage });
+            });
+
+            test('returns an error response when the selector is not an svg or image tag', async ({ createConfiguredDbp }) => {
+                const dbp = await createConfiguredDbp(BROKER_PROTECTION_CONFIGS.default);
+                await dbp.navigatesTo(imageCaptchaTargetPage);
+                await dbp.receivesInlineAction(createGetRecaptchaInfoAction({ selector: "#svg-captcha-rendering" }));
+
+                await dbp.isCaptchaError();
+            });
+        });
+
+        test.describe('solveCaptchaInfo', () => {
+            test('solves the captcha for the correct action data', async ({ createConfiguredDbp }) => {
+                const dbp = await createConfiguredDbp(BROKER_PROTECTION_CONFIGS.default);
+                await dbp.navigatesTo(imageCaptchaTargetPage);
+                await dbp.receivesInlineAction(createSolveImageCaptchaAction({ selector: imageCaptchaResponseSelector }));
+                dbp.getSuccessResponse();
+
+                await dbp.isCaptchaTokenFilled(imageCaptchaResponseSelector);
+            });
         });
     });
 });
