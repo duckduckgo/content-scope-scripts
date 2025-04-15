33273939ea5a5124b5fd54ee7927599de15b756c
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-04-15 15:45:40 +0100

    Thumbnail is showing

--- injected/src/features/duckplayer-native/overlays/thumbnail-overlay.js
+++ injected/src/features/duckplayer-native/overlays/thumbnail-overlay.js
@@ -1,6 +1,7 @@
 import mobilecss from './thumbnail-overlay.css';
 import { createPolicy, html } from '../../../dom-utils.js';
 import { customElementsDefine, customElementsGet } from '../../../captured-globals.js';
+import { VideoParams, appendImageAsBackground } from '../../duckplayer/util';
 
 export function registerCustomElements() {
     if (!customElementsGet(DDGVideoThumbnailOverlay.CUSTOM_TAG_NAME)) {
@@ -18,6 +19,8 @@ export class DDGVideoThumbnailOverlay extends HTMLElement {
     policy = createPolicy();
     /** @type {boolean} */
     testMode = false;
+    /** @type {HTMLElement} */
+    container;
 
     connectedCallback() {
         this.createMarkupAndStyles();
@@ -32,6 +35,43 @@ export class DDGVideoThumbnailOverlay extends HTMLElement {
         container.innerHTML = this.policy.createHTML(content);
         shadow.append(style, container);
         this.container = container;
+        this.appendThumbnail();
+    }
+
+    appendThumbnail() {
+        const params = VideoParams.forWatchPage(this.getPlayerPageHref());
+        const videoId = params?.id;
+
+        const imageUrl = this.getLargeThumbnailSrc(videoId);
+        console.log('Image url', imageUrl);
+        appendImageAsBackground(this.container, '.ddg-vpo-bg', imageUrl);
+    }
+
+    getLargeThumbnailSrc(videoId) {
+        const url = new URL(`/vi/${videoId}/maxresdefault.jpg`, 'https://i.ytimg.com');
+        return url.href;
+    }
+
+    /**
+     * This is the URL of the page that the user is currently on
+     * It's abstracted so that we can mock it in tests
+     * @return {string}
+     */
+    getPlayerPageHref() {
+        if (this.testMode) {
+            const url = new URL(window.location.href);
+            if (url.hostname === 'www.youtube.com') return window.location.href;
+
+            // reflect certain query params, this is useful for testing
+            if (url.searchParams.has('v')) {
+                const base = new URL('/watch', 'https://youtube.com');
+                base.searchParams.set('v', url.searchParams.get('v') || '');
+                return base.toString();
+            }
+
+            return 'https://youtube.com/watch?v=123';
+        }
+        return window.location.href;
     }
 
     /**
@@ -46,6 +86,10 @@ export class DDGVideoThumbnailOverlay extends HTMLElement {
         `.toString();
     }
 }
+
+
+
+
 
 /**
  *
