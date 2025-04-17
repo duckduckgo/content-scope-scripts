01eb84e7d79a94addc50634ac9dc24f3ea0a758c
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-04-17 12:37:37 +0100

    Duck Player Native tests

---
+++ injected/integration-test/duckplayer-native.spec.js
@@ -0,0 +1,30 @@
+import { test } from '@playwright/test';
+import { DuckPlayerNative } from './page-objects/duckplayer-native.js';
+
+test.describe('Duck Player Native messaging', () => {
+    test('Calls initial setup', async ({ page }, workerInfo) => {
+        const duckPlayer = DuckPlayerNative.create(page, workerInfo);
+
+        // Given the duckPlayerNative feature is enabled
+        await duckPlayer.withRemoteConfig();
+
+        // When I go to a YouTube page
+        await duckPlayer.gotoYouTubePage();
+
+        // Then Initial Setup should be called
+        await duckPlayer.didSendInitialHandshake();
+    });
+
+    test('Polls timestamp on YouTube', async ({ page }, workerInfo) => {
+        const duckPlayer = DuckPlayerNative.create(page, workerInfo);
+
+        // Given the duckPlayerNative feature is enabled
+        await duckPlayer.withRemoteConfig();
+
+        // When I go to a YouTube page
+        await duckPlayer.gotoYouTubePage();
+
+        // Then the current timestamp should be polled back to the browser
+        await duckPlayer.didSendCurrentTimestamp();
+    });
+});
