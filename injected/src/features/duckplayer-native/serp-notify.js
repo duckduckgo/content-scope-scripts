405e8db428aace87be61e30eb3c56444d2adc4ea
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-04-23 11:32:38 +0100

    Catchup with Daniel

--- injected/src/features/duckplayer-native/serp-notify.js
+++ injected/src/features/duckplayer-native/serp-notify.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck - Typing will be fixed in the future
 
+// TODO: Add page load event listener
 export function serpNotify() {
     window.dispatchEvent(
         new CustomEvent('ddg-serp-yt-response', {
