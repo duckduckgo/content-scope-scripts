cbf156d52ac4a053ebdc570c0ca3001820d42184
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-04-23 14:13:52 +0100

    Using SideEffects class

--- injected/src/features/duckplayer-native/serp-notify.js
+++ injected/src/features/duckplayer-native/serp-notify.js
@@ -1,7 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck - Typing will be fixed in the future
-
-// TODO: Add page load event listener
 export function serpNotify() {
     window.dispatchEvent(
         new CustomEvent('ddg-serp-yt-response', {
