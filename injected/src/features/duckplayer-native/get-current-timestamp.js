712ea79edd7e0e865b8a42551818e21afb995d09
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-04-25 10:46:24 +0100

    Changed loading sequence

--- injected/src/features/duckplayer-native/get-current-timestamp.js
+++ injected/src/features/duckplayer-native/get-current-timestamp.js
@@ -5,3 +5,29 @@ export function getCurrentTimestamp() {
     const video = document.querySelector('video');
     return video?.currentTime || 0;
 }
+
+/**
+ * Sends the timestamp to the browser every 300ms
+ * TODO: Can we not brute force this?
+ */
+/**
+ * Sends the timestamp to the browser every 300ms
+ * TODO: Can we not brute force this?
+ * @param {number} interval - Polling interval
+ * @param {(number) => void} callback - Callback handler for polling event
+ * @returns
+ */
+export function pollTimestamp(interval = 300, callback) {
+    if (!callback) {
+        console.error('Timestamp polling failed. No callback defined');
+    }
+
+    const timestampPolling = setInterval(() => {
+        const timestamp = getCurrentTimestamp();
+        callback(timestamp);
+    }, interval);
+
+    return () => {
+        clearInterval(timestampPolling);
+    };
+}
