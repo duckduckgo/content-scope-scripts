d82d002920967055da27b0250c71c05ac3071ab4
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-05-01 12:39:36 +0100

    Lint

--- special-pages/pages/duckplayer/integration-tests/duck-player.js
+++ special-pages/pages/duckplayer/integration-tests/duck-player.js
@@ -5,7 +5,8 @@ import { perPlatform } from 'injected/integration-test/type-helpers.mjs';
 
 const MOCK_VIDEO_ID = 'VIDEO_ID';
 const MOCK_VIDEO_TITLE = 'Embedded Video - YouTube';
-const youtubeEmbed = (id) => 'https://www.youtube-nocookie.com/embed/' + id + '?iv_load_policy=1&autoplay=1&rel=0&modestbranding=1&color=white';
+const youtubeEmbed = (id) =>
+    'https://www.youtube-nocookie.com/embed/' + id + '?iv_load_policy=1&autoplay=1&rel=0&modestbranding=1&color=white';
 const youtubeEmbedIOS = (id) =>
     'https://www.youtube-nocookie.com/embed/' + id + '?iv_load_policy=1&autoplay=1&muted=1&rel=0&modestbranding=1&color=white';
 const html = {
