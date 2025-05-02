b62081bae9681701baeeec04ef039fb25e3c4a48
Shane Osbourne <shane.osbourne8@gmail.com>
2025-05-02 11:15:02 +0100

    pass the message secret

--- injected/entry-points/windows.js
+++ injected/entry-points/windows.js
@@ -31,6 +31,7 @@ function initCode() {
         site: processedConfig.site,
         bundledConfig: processedConfig.bundledConfig,
         messagingConfig: processedConfig.messagingConfig,
+        messageSecret: processedConfig.messageSecret,
     });
 
     init(processedConfig);
