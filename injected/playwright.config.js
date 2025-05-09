fb7e2303b53388f6ca3347198e1cb49ad06fed7f
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-05-09 08:24:26 +0100

    Revert "Duck Player Native non-isolated"
    This reverts commit 7baec4ad03ae98c67fea9bfa9f90de26d81c7ee1.

--- injected/playwright.config.js
+++ injected/playwright.config.js
@@ -31,14 +31,17 @@ export default defineConfig({
             testMatch: [
                 'integration-test/navigator-interface-insecure.js',
                 'integration-test/webcompat.spec.js',
-                'integration-test/message-bridge-apple.spec.js',
-                'integration-test/duckplayer-native.spec.js',
+                'integration-test/message-bridge-apple.spec.js'
             ],
             use: { injectName: 'apple', platform: 'macos' },
         },
         {
             name: 'ios',
-            testMatch: ['integration-test/duckplayer-mobile.spec.js', 'integration-test/duckplayer-mobile-drawer.spec.js'],
+            testMatch: [
+                'integration-test/duckplayer-mobile.spec.js',
+                'integration-test/duckplayer-mobile-drawer.spec.js',
+                'integration-test/duckplayer-native.spec.js',
+            ],
             use: { injectName: 'apple-isolated', platform: 'ios', ...devices['iPhone 13'] },
         },
         {
