3419e29265c9652b14692fe43c85c94704e8c9b3
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-05-09 08:38:06 +0100

    Reverting non-isolated mode

--- injected/src/features.js
+++ injected/src/features.js
@@ -34,7 +34,7 @@ const otherFeatures = /** @type {const} */ ([
 /** @type {Record<string, FeatureName[]>} */
 export const platformSupport = {
     apple: ['webCompat', ...baseFeatures],
-    'apple-isolated': ['duckPlayerNative', 'brokerProtection', 'performanceMetrics', 'clickToLoad', 'messageBridge', 'favicon'],
+    'apple-isolated': ['duckPlayer', 'duckPlayerNative', 'brokerProtection', 'performanceMetrics', 'clickToLoad', 'messageBridge', 'favicon'],
     android: [...baseFeatures, 'webCompat', 'breakageReporting', 'duckPlayer', 'messageBridge'],
     'android-broker-protection': ['brokerProtection'],
     'android-autofill-password-import': ['autofillPasswordImport'],
