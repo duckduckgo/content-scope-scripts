2835d8d37601300954f00747f80039bbd22317a2
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-06-09 18:28:06 +0100

    Lint

--- injected/integration-test/duckplayer-native.spec.js
+++ injected/integration-test/duckplayer-native.spec.js
@@ -144,17 +144,3 @@ test.describe('Duck Player Native custom error view', () => {
         await duckPlayer.didShowSignInError();
     });
 });
-
-/**
- * @param {import("@playwright/test").TestInfo} testInfo
- */
-function isMobile(testInfo) {
-    const u = /** @type {any} */ (testInfo.project.use);
-    return u?.platform === 'android' || u?.platform === 'ios';
-}
-/**
- * @param {import("@playwright/test").TestInfo} testInfo
- */
-function isDesktop(testInfo) {
-    return !isMobile(testInfo);
-}
