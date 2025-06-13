d95b88897ed77307e1447f7b9bed04ef6ec7c7b2
David Harbage <harbagio@gmail.com>
2025-06-13 16:44:52 -0400

    experiment with using npm module instead of submodule

--- injected/src/features/scriptlets.js
+++ injected/src/features/scriptlets.js
@@ -1,28 +1,32 @@
 import ContentFeature from '../content-feature.js';
 import { isBeingFramed } from '../utils.js';
-import { setCookie } from './Scriptlets/src/scriptlets/set-cookie.js';
-import { trustedSetCookie } from './Scriptlets/src/scriptlets/trusted-set-cookie.js';
-import { setCookieReload } from './Scriptlets/src/scriptlets/set-cookie-reload.js';
-import { removeCookie } from './Scriptlets/src/scriptlets/remove-cookie.js';
-import { setConstant } from './Scriptlets/src/scriptlets/set-constant.js';
-import { setLocalStorageItem } from './Scriptlets/src/scriptlets/set-local-storage-item.js';
-import { abortCurrentInlineScript } from './Scriptlets/src/scriptlets/abort-current-inline-script.js';
-import { abortOnPropertyRead } from './Scriptlets/src/scriptlets/abort-on-property-read.js';
-import { abortOnPropertyWrite } from './Scriptlets/src/scriptlets/abort-on-property-write.js';
-import { preventAddEventListener } from './Scriptlets/src/scriptlets/prevent-addEventListener.js';
-import { preventWindowOpen } from './Scriptlets/src/scriptlets/prevent-window-open.js';
-import { preventSetTimeout } from './Scriptlets/src/scriptlets/prevent-setTimeout.js';
-import { removeNodeText } from './Scriptlets/src/scriptlets/remove-node-text.js';
-import { preventFetch } from './Scriptlets/src/scriptlets/prevent-fetch.js';
+
+// Import from our minimal scriptlets module (contains only functions we actually use)
+import {
+    setCookie,
+    trustedSetCookie,
+    setCookieReload,
+    removeCookie,
+    setConstant,
+    setLocalStorageItem,
+    abortCurrentInlineScript,
+    abortOnPropertyRead,
+    abortOnPropertyWrite,
+    preventAddEventListener,
+    preventWindowOpen,
+    preventSetTimeout,
+    removeNodeText,
+    preventFetch
+} from './scriptlets-minimal.js';
 
 export class Scriptlets extends ContentFeature {
     init() {
         if (isBeingFramed()) {
             return;
         }
-        /* @type {import('./Scriptlets/src/scriptlets/scriptlets.ts').Source} */
+        /* @type {import('@adguard/scriptlets').Source} */
         const source = {
-            verbose: false,
+            verbose: true,
         };
 
         const scriptlets = this.getFeatureSetting('scriptlets');
@@ -39,47 +43,48 @@ export class Scriptlets extends ContentFeature {
         // add debug flag to site breakage reports
         this.addDebugFlag();
 
+        // The extracted scriptlets expect (source, args) where args is an array
         if (scriptlet.name === 'setCookie') {
-            setCookie(source, attrs.name, attrs.value, attrs.path, attrs.domain);
+            setCookie(source, [attrs.name, attrs.value, attrs.path, attrs.domain]);
         }
         if (scriptlet.name === 'trustedSetCookie') {
-            trustedSetCookie(source, attrs.name, attrs.value, attrs.path, attrs.domain);
+            trustedSetCookie(source, [attrs.name, attrs.value, attrs.path, attrs.domain]);
         }
         if (scriptlet.name === 'setCookieReload') {
-            setCookieReload(source, attrs.name, attrs.value, attrs.path, attrs.domain);
+            setCookieReload(source, [attrs.name, attrs.value, attrs.path, attrs.domain]);
         }
         if (scriptlet.name === 'removeCookie') {
-            removeCookie(source, attrs.match);
+            removeCookie(source, [attrs.match]);
         }
         if (scriptlet.name === 'setConstant') {
-            setConstant(source, attrs.property, attrs.value, attrs.stack, attrs.valueWrapper, attrs.setProxyTrap);
+            setConstant(source, [attrs.property, attrs.value, attrs.stack, attrs.valueWrapper, attrs.setProxyTrap]);
         }
         if (scriptlet.name === 'setLocalStorageItem') {
-            setLocalStorageItem(source, attrs.key, attrs.value);
+            setLocalStorageItem(source, [attrs.key, attrs.value]);
         }
         if (scriptlet.name === 'abortCurrentInlineScript') {
-            abortCurrentInlineScript(source, attrs.property, attrs.search);
+            abortCurrentInlineScript(source, [attrs.property, attrs.search]);
         }
         if (scriptlet.name === 'abortOnPropertyRead') {
-            abortOnPropertyRead(source, attrs.property);
+            abortOnPropertyRead(source, [attrs.property]);
         }
         if (scriptlet.name === 'abortOnPropertyWrite') {
-            abortOnPropertyWrite(source, attrs.property);
+            abortOnPropertyWrite(source, [attrs.property]);
         }
         if (scriptlet.name === 'preventAddEventListener') {
-            preventAddEventListener(source, attrs.typeSearch, attrs.listenerSearch, attrs.additionalArgName, attrs.additionalArgValue);
+            preventAddEventListener(source, [attrs.typeSearch, attrs.listenerSearch, attrs.additionalArgName, attrs.additionalArgValue]);
         }
         if (scriptlet.name === 'preventWindowOpen') {
-            preventWindowOpen(source, attrs.match, attrs.delay, attrs.replacement);
+            preventWindowOpen(source, [attrs.match, attrs.delay, attrs.replacement]);
         }
         if (scriptlet.name === 'preventSetTimeout') {
-            preventSetTimeout(source, attrs.matchCallback, attrs.matchDelay);
+            preventSetTimeout(source, [attrs.matchCallback, attrs.matchDelay]);
         }
         if (scriptlet.name === 'removeNodeText') {
-            removeNodeText(source, attrs.nodeName, attrs.textMatch, attrs.parentSelector);
+            removeNodeText(source, [attrs.nodeName, attrs.textMatch, attrs.parentSelector]);
         }
         if (scriptlet.name === 'preventFetch') {
-            preventFetch(source, attrs.propsToMatch, attrs.responseBody, attrs.responseType);
+            preventFetch(source, [attrs.propsToMatch, attrs.responseBody, attrs.responseType]);
         }
     }
 }
