5c8609b638835c035c51b2cf2bb992d4c697a10b
Robert Anderson <randerson@duckduckgo.com>
2025-07-10 11:35:39 +1000

    Tab switcher styling

--- special-pages/pages/new-tab/app/omnibar/components/TabSwitcher.js
+++ special-pages/pages/new-tab/app/omnibar/components/TabSwitcher.js
@@ -1,7 +1,6 @@
 import { h } from 'preact';
-import { AiChatIcon, SearchIcon } from '../../components/Icons.js';
+import { AiChatColorIcon, AiChatIcon, SearchColorIcon, SearchIcon } from '../../components/Icons.js';
 import { useTypedTranslationWith } from '../../types';
-import { viewTransition } from '../../utils';
 import styles from './TabSwitcher.module.css';
 
 /**
@@ -16,37 +15,64 @@ import styles from './TabSwitcher.module.css';
  */
 export function TabSwitcher({ mode, setMode }) {
     const { t } = useTypedTranslationWith(/** @type {Strings} */ ({}));
-
     return (
-        <div class={styles.tabListWrap}>
-            <div class={styles.tabList} role="tablist" aria-label={t('omnibar_tabSwitcherLabel')}>
-                <button
-                    class={styles.tab}
-                    role="tab"
-                    aria-selected={mode === 'search'}
-                    onClick={() => {
-                        viewTransition(() => {
-                            setMode('search');
-                        });
-                    }}
-                >
-                    <SearchIcon className={styles.searchIcon} />
-                    {t('omnibar_searchTabLabel')}
-                </button>
-                <button
-                    class={styles.tab}
-                    role="tab"
-                    aria-selected={mode === 'ai'}
-                    onClick={() => {
-                        viewTransition(() => {
-                            setMode('ai');
-                        });
-                    }}
-                >
-                    <AiChatIcon className={styles.aiChatIcon} />
-                    {t('omnibar_aiTabLabel')}
-                </button>
-            </div>
+        <div class={styles.tabSwitcher} role="tablist" aria-label={t('omnibar_tabSwitcherLabel')}>
+            <Blob class={styles.blob} style={{ translate: mode === 'search' ? 0 : 92 }} />
+            <button class={styles.tab} role="tab" aria-selected={mode === 'search'} onClick={() => setMode('search')}>
+                {mode === 'search' ? <SearchColorIcon /> : <SearchIcon />}
+                <span class={styles.tabLabel}>{t('omnibar_searchTabLabel')}</span>
+            </button>
+            <button class={styles.tab} role="tab" aria-selected={mode === 'ai'} onClick={() => setMode('ai')}>
+                {mode === 'ai' ? <AiChatColorIcon /> : <AiChatIcon />}
+                <span class={styles.tabLabel}>{t('omnibar_aiTabLabel')}</span>
+            </button>
         </div>
+    );
+}
+
+/**
+ * @param {import('preact').JSX.SVGAttributes<SVGSVGElement>} props
+ */
+function Blob(props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="108" height="48" viewBox="0 0 108 48" fill="none" {...props}>
+            <g filter="url(#filter0_ddi_26080_85978)">
+                <path
+                    d="M8 20C8 11.1634 15.1634 4 24 4H84C92.8366 4 100 11.1634 100 20C100 28.8366 92.8366 36 84 36H24C15.1634 36 8 28.8366 8 20Z"
+                    fill="white"
+                />
+            </g>
+            <defs>
+                <filter
+                    id="filter0_ddi_26080_85978"
+                    x="0"
+                    y="0"
+                    width="108"
+                    height="48"
+                    filterUnits="userSpaceOnUse"
+                    color-interpolation-filters="sRGB"
+                >
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                    <feOffset dy="4" />
+                    <feGaussianBlur stdDeviation="4" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0" />
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_26080_85978" />
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                    <feOffset dy="1" />
+                    <feGaussianBlur stdDeviation="2" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0" />
+                    <feBlend mode="normal" in2="effect1_dropShadow_26080_85978" result="effect2_dropShadow_26080_85978" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_26080_85978" result="shape" />
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                    <feOffset dy="1" />
+                    <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                    <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.48 0" />
+                    <feBlend mode="normal" in2="shape" result="effect3_innerShadow_26080_85978" />
+                </filter>
+            </defs>
+        </svg>
     );
 }
