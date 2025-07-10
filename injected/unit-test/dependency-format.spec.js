b5061b737f279edb4274f98da635e3eb9a01f7a3
Jonathan Kingston <jkingston@duckduckgo.com>
2025-07-10 16:43:36 +0100

    Lint fix

--- injected/unit-test/dependency-format.spec.js
+++ injected/unit-test/dependency-format.spec.js
@@ -6,14 +6,9 @@ describe('Dependency format check', () => {
     it('should use a tag for @duckduckgo/privacy-configuration', () => {
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = path.dirname(__filename);
-        const pkg = JSON.parse(
-            fs.readFileSync(
-                path.join(__dirname, '../package.json'),
-                'utf-8'
-            )
-        );
+        const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'));
         const dep = pkg.dependencies['@duckduckgo/privacy-configuration'];
         // Accepts only a tag (e.g., #1752154773643), not a commit hash (40 hex chars)
         expect(dep).toMatch(/^github:duckduckgo\/privacy-configuration#\d+$/);
     });
-}); 
+});
