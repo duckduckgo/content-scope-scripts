f1a3a75bc0460fe7d8e572d5bc81c30bb312029b
Robert Anderson <randerson@duckduckgo.com>
2025-07-11 19:01:35 +1000

    NTP: Style Omnibar to match specs (#1811)
    * Search form and suggestions list styling
    
    * Root container styling
    
    * Search form and root container spacing tweaks
    
    * AI chat form styling
    
    * Remove unecessary .icon class
    
    * Tab switcher styling
    
    * Dark mode
    
    * Make AI form same size as search form
    
    * Animation
    
    * Remove unused import
    
    * Remove test for removed feature
    
    * Use useLayoutEffect
    
    * Support Shift+Enter for newlines in AI chat form
    
    * Remove redundant padding-bottom from AiChatForm styles
    
    * Refactor icon components to accept SVG props
    
    * Remove input padding in search form
    
    * Fix broken JSDoc comment
    
    * Refactor AI chat form submission and keyboard tests

--- special-pages/pages/new-tab/app/omnibar/components/SearchForm.js
+++ special-pages/pages/new-tab/app/omnibar/components/SearchForm.js
@@ -1,13 +1,10 @@
-import cn from 'classnames';
-import { Fragment, h } from 'preact';
+import { h } from 'preact';
 import { useContext, useId } from 'preact/hooks';
-import { eventToTarget } from '../../../../../shared/handlers';
-import { AiChatIcon, SearchIcon } from '../../components/Icons.js';
-import { usePlatformName } from '../../settings.provider';
+import { SearchIcon } from '../../components/Icons.js';
 import { useTypedTranslationWith } from '../../types';
-import styles from './Omnibar.module.css';
 import { OmnibarContext } from './OmnibarProvider';
-import { SuggestionsList } from './SuggestionList.js';
+import styles from './SearchForm.module.css';
+import { SuggestionsList } from './SuggestionsList.js';
 import { useSuggestionInput } from './useSuggestionInput.js';
 import { useSuggestions } from './useSuggestions';
 
@@ -17,15 +14,13 @@ import { useSuggestions } from './useSuggestions';
 
 /**
  * @param {object} props
- * @param {boolean} props.enableAi
  * @param {string} props.term
  * @param {(term: string) => void} props.setTerm
  */
-export function SearchForm({ enableAi, term, setTerm }) {
-    const { submitSearch, submitChat } = useContext(OmnibarContext);
+export function SearchForm({ term, setTerm }) {
+    const { submitSearch } = useContext(OmnibarContext);
 
     const { t } = useTypedTranslationWith(/** @type {Strings} */ ({}));
-    const platformName = usePlatformName();
     const suggestionsListId = useId();
 
     const {
@@ -56,54 +51,30 @@ export function SearchForm({ enableAi, term, setTerm }) {
     };
 
     return (
-        <div class={styles.formWrap}>
-            <form class={styles.form} onBlur={onFormBlur} onSubmit={onFormSubmit}>
-                <div class={styles.inputRoot} style={{ viewTransitionName: 'omnibar-input-transition' }}>
-                    <div class={styles.inputContainer} style={{ viewTransitionName: 'omnibar-input-transition2' }}>
-                        <input
-                            ref={inputRef}
-                            type="text"
-                            role="combobox"
-                            class={styles.input}
-                            placeholder={t('searchForm_placeholder')}
-                            aria-label={t('searchForm_placeholder')}
-                            aria-expanded={suggestions.length > 0}
-                            aria-haspopup="listbox"
-                            aria-controls={suggestionsListId}
-                            aria-activedescendant={selectedSuggestion?.id}
-                            spellcheck={false}
-                            autoComplete="off"
-                            autoCorrect="off"
-                            autoCapitalize="off"
-                            onChange={onInputChange}
-                            onKeyDown={onInputKeyDown}
-                            onClick={onInputClick}
-                        />
-                        <div class={styles.inputActions}>
-                            <button type="submit" class={cn(styles.inputAction)} aria-label={t('searchForm_searchButtonLabel')} inert>
-                                <SearchIcon />
-                            </button>
-                            {enableAi && (
-                                <>
-                                    <div class={styles.separator}></div>
-                                    <button
-                                        class={cn(styles.inputAction, styles.squareButton)}
-                                        aria-label={t('searchForm_aiButtonLabel')}
-                                        onClick={(event) => {
-                                            event.preventDefault();
-                                            submitChat({
-                                                chat: term,
-                                                target: eventToTarget(event, platformName),
-                                            });
-                                        }}
-                                    >
-                                        <AiChatIcon className={styles.aiChatIcon} />
-                                    </button>
-                                </>
-                            )}
-                        </div>
-                    </div>
-                </div>
+        <form class={styles.form} onClick={() => inputRef.current?.focus()} onBlur={onFormBlur} onSubmit={onFormSubmit}>
+            <div class={styles.inputContainer}>
+                <SearchIcon inert />
+                <input
+                    ref={inputRef}
+                    type="text"
+                    role="combobox"
+                    class={styles.input}
+                    placeholder={t('searchForm_placeholder')}
+                    aria-label={t('searchForm_placeholder')}
+                    aria-expanded={suggestions.length > 0}
+                    aria-haspopup="listbox"
+                    aria-controls={suggestionsListId}
+                    aria-activedescendant={selectedSuggestion?.id}
+                    spellcheck={false}
+                    autoComplete="off"
+                    autoCorrect="off"
+                    autoCapitalize="off"
+                    onChange={onInputChange}
+                    onKeyDown={onInputKeyDown}
+                    onClick={onInputClick}
+                />
+            </div>
+            {suggestions.length > 0 && (
                 <SuggestionsList
                     id={suggestionsListId}
                     suggestions={suggestions}
@@ -111,7 +82,7 @@ export function SearchForm({ enableAi, term, setTerm }) {
                     setSelectedSuggestion={setSelectedSuggestion}
                     clearSelectedSuggestion={clearSelectedSuggestion}
                 />
-            </form>
-        </div>
+            )}
+        </form>
     );
 }
