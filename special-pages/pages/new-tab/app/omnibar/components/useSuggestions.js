232e1598f3248b8310fe1f764c09513a29fc6e4f
Robert Anderson <randerson@duckduckgo.com>
2025-07-15 20:36:07 +1000

    Display URL if suggestion title is empty

--- special-pages/pages/new-tab/app/omnibar/components/useSuggestions.js
+++ special-pages/pages/new-tab/app/omnibar/components/useSuggestions.js
@@ -271,22 +271,27 @@ export function useSuggestions({ term, onChangeTerm, onOpenSuggestion, onSubmitS
 function getSuggestionTitle(suggestion) {
     switch (suggestion.kind) {
         case 'bookmark':
-            return suggestion.title;
         case 'historyEntry':
-            return suggestion.title;
+        case 'internalPage':
+            return suggestion.title || getDisplayURL(suggestion.url);
         case 'phrase':
             return suggestion.phrase;
         case 'openTab':
             return suggestion.title;
-        case 'website': {
-            const url = new URL(suggestion.url);
-            return url.host + url.pathname + url.search + url.hash;
-        }
-        case 'internalPage':
-            return suggestion.title;
+        case 'website':
+            return getDisplayURL(suggestion.url);
         default:
             throw new Error('Unknown suggestion kind');
     }
+}
+
+/**
+ * @param {string} url
+ * @returns {string}
+ */
+function getDisplayURL(url) {
+    const { host, pathname, search, hash } = new URL(url);
+    return host + pathname + search + hash;
 }
 
 /**
