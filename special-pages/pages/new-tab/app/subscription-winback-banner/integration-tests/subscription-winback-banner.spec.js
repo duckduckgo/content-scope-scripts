adb03e7aaaa8326e096c5a099a23e4bb17d99d20
Jason Ingram <jingram@duckduckgo.com>
2025-10-15 18:13:47 -0400

    Create win-back banner component and supporting files
    Include supporting svg

---
+++ special-pages/pages/new-tab/app/subscription-winback-banner/integration-tests/subscription-winback-banner.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import { NewtabPage } from '../../../integration-tests/new-tab.page.js';
+
+test.describe('newtab remote messaging framework subscriptionWinBackBanner', () => {
+    test('fetches config + data', async ({ page }, workerInfo) => {
+        const ntp = NewtabPage.create(page, workerInfo);
+        await ntp.reducedMotion();
+        await ntp.openPage({ winback: 'true' });
+
+        const calls1 = await ntp.mocks.waitForCallCount({ method: 'initialSetup', count: 1 });
+        const calls2 = await ntp.mocks.waitForCallCount({ method: 'winBackOffer_getData', count: 1 });
+
+        expect(calls1.length).toBe(1);
+        expect(calls2.length).toBe(1);
+    });
+
+    test('renders a title, descriptionText an action button, and dismiss button', async ({ page }, workerInfo) => {
+        const ntp = NewtabPage.create(page, workerInfo);
+        await ntp.reducedMotion();
+        await ntp.openPage({ winback: 'true' });
+
+        await page.getByRole('heading', { name: 'Last day to save 25%!' }).waitFor();
+        await page.getByText('Stay protected with our').waitFor();
+        await page.locator('strong').waitFor();
+
+        await page.getByRole('button', { name: 'See Offer' }).click();
+        await ntp.mocks.waitForCallCount({ method: 'winBackOffer_action', count: 1 });
+        await page.getByTestId('dismissBtn').click();
+        await ntp.mocks.waitForCallCount({ method: 'winBackOffer_dismiss', count: 1 });
+    });
+});
