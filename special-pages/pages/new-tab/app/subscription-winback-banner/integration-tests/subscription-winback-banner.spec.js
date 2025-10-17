ef8193c980dba68dd1d55d0a603cd13d9bd7edb9
Jason Ingram <jingram@duckduckgo.com>
2025-10-17 13:03:25 -0400

    Update test to pass the correct query param value

--- special-pages/pages/new-tab/app/subscription-winback-banner/integration-tests/subscription-winback-banner.spec.js
+++ special-pages/pages/new-tab/app/subscription-winback-banner/integration-tests/subscription-winback-banner.spec.js
@@ -5,7 +5,7 @@ test.describe('newtab remote messaging framework subscriptionWinBackBanner', () 
     test('fetches config + data', async ({ page }, workerInfo) => {
         const ntp = NewtabPage.create(page, workerInfo);
         await ntp.reducedMotion();
-        await ntp.openPage({ winback: 'true' });
+        await ntp.openPage({ winback: 'winback_last_day' });
 
         const calls1 = await ntp.mocks.waitForCallCount({ method: 'initialSetup', count: 1 });
         const calls2 = await ntp.mocks.waitForCallCount({ method: 'winBackOffer_getData', count: 1 });
@@ -17,11 +17,10 @@ test.describe('newtab remote messaging framework subscriptionWinBackBanner', () 
     test('renders a title, descriptionText an action button, and dismiss button', async ({ page }, workerInfo) => {
         const ntp = NewtabPage.create(page, workerInfo);
         await ntp.reducedMotion();
-        await ntp.openPage({ winback: 'true' });
+        await ntp.openPage({ winback: 'winback_last_day' });
 
         await page.getByRole('heading', { name: 'Last day to save 25%!' }).waitFor();
         await page.getByText('Stay protected with our').waitFor();
-        await page.locator('strong').waitFor();
 
         await page.getByRole('button', { name: 'See Offer' }).click();
         await ntp.mocks.waitForCallCount({ method: 'winBackOffer_action', count: 1 });
