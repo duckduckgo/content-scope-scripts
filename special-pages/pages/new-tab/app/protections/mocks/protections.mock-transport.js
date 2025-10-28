8be8fa0a531cb9c376dcbad1d7e970b0497b6d3b
Jason Ingram <jingram@duckduckgo.com>
2025-10-28 12:38:56 -0400

    Add testing states

--- special-pages/pages/new-tab/app/protections/mocks/protections.mock-transport.js
+++ special-pages/pages/new-tab/app/protections/mocks/protections.mock-transport.js
@@ -80,6 +80,8 @@ export function protectionsMockTransport() {
             const msg = /** @type {any} */ (_msg);
             switch (msg.method) {
                 case 'protections_getData':
+                    // No data. Setting `stats=none` (totalCount = 0) also
+                    // hides CPM stats
                     if (url.searchParams.get('stats') === 'none') {
                         dataset.totalCount = 0;
                     }
@@ -88,6 +90,14 @@ export function protectionsMockTransport() {
                     }
                     if (url.searchParams.get('cpm') === 'true') {
                         dataset.totalCookiePopUpsBlocked = 22;
+                    }
+                    // CPM = 0 state
+                    if (url.searchParams.get('cpm') === 'none') {
+                        dataset.totalCookiePopUpsBlocked = 0;
+                    }
+                    // CPM disabled state
+                    if (url.searchParams.get('cpm') === 'null') {
+                        dataset.totalCookiePopUpsBlocked = null;
                     }
                     // Setting cpm=undefined allows us to see the legacy
                     // protections report. Useful until all platforms adopt the
