a077f638a5cddeb98e927af11e2b975f5e1c6185
Jason Ingram <jingram@duckduckgo.com>
2025-10-28 11:56:16 -0400

    Add @todos

--- special-pages/pages/new-tab/app/protections/components/ProtectionsHeading.js
+++ special-pages/pages/new-tab/app/protections/components/ProtectionsHeading.js
@@ -50,6 +50,7 @@ export function ProtectionsHeading({
                 </span>
                 <h2 class={styles.caption}>{t('protections_menuTitle')}</h2>
 
+                {/* @todo accessibility: move focus to content on hover? */}
                 <Tooltip content={t('stats_protectionsReportInfo')}>
                     <InfoIcon class={styles.infoIcon} />
                 </Tooltip>
