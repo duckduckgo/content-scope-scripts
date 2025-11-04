8ac9050cc07adf77f514c4de6a5cbd604db6b55c
David Castro <davidcastromartinez91@gmail.com>
2025-11-04 16:46:11 +0100

    Arbitrary test change to example page.

--- special-pages/pages/example/app/components/App.jsx
+++ special-pages/pages/example/app/components/App.jsx
@@ -4,5 +4,8 @@ import { useTypedTranslation } from '../types.js';
 
 export function App() {
     const { t } = useTypedTranslation();
-    return <main class={styles.main}>{t('helloWorld')}</main>;
+    return <main class={styles.main}>
+        <h1>Hi There! This is a sample change!</h1>
+        <p>{t('helloWorld')}</p>
+        </main>;
 }
