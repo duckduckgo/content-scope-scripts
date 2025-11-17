8f2e728a97bbcf23fc5b006dc37f581f17094754
Jason Ingram <jingram@duckduckgo.com>
2025-11-17 13:38:42 -0500

    Fix for duplicate protections_onDataUpdate

--- special-pages/pages/new-tab/app/protections/components/ProtectionsProvider.js
+++ special-pages/pages/new-tab/app/protections/components/ProtectionsProvider.js
@@ -1,5 +1,5 @@
 import { createContext, h } from 'preact';
-import { useCallback, useEffect, useReducer, useRef } from 'preact/hooks';
+import { useCallback, useContext, useEffect, useReducer, useRef } from 'preact/hooks';
 import { useMessaging } from '../../types.js';
 import { reducer, useConfigSubscription, useInitialDataAndConfig } from '../../service.hooks.js';
 import { ProtectionsService } from '../protections.service.js';
@@ -95,11 +95,11 @@ export function useService() {
  * @return {import("@preact/signals").Signal<number>}
  */
 export function useBlockedCount(initial) {
-    const service = useService();
+    const service = useContext(ProtectionsServiceContext);
     const signal = useSignal(initial);
     // @todo jingram possibly refactor to include full object
     useSignalEffect(() => {
-        return service.current?.onData((evt) => {
+        return service?.onData((evt) => {
             signal.value = evt.data.totalCount;
         });
     });
@@ -111,11 +111,11 @@ export function useBlockedCount(initial) {
  * @return {import("@preact/signals").Signal<undefined | number | null>}
  */
 export function useCookiePopUpsBlockedCount(initial) {
-    const service = useService();
+    const service = useContext(ProtectionsServiceContext);
     const signal = useSignal(initial);
 
     useSignalEffect(() => {
-        return service.current?.onData((evt) => {
+        return service?.onData((evt) => {
             signal.value = evt.data.totalCookiePopUpsBlocked;
         });
     });
