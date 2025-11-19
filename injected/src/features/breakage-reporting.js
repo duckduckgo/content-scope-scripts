f239af5eec7f1ab22e0d1073605fc496a57a3947
jdorweiler <jason@duckduckgo.com>
2025-11-19 13:10:53 -0700

    get settings from webInterference

--- injected/src/features/breakage-reporting.js
+++ injected/src/features/breakage-reporting.js
@@ -2,19 +2,21 @@ import ContentFeature from '../content-feature';
 import { getExpandedPerformanceMetrics, getJsPerformanceMetrics } from './breakage-reporting/utils.js';
 import { runBotDetection } from '../detectors/detections/bot-detection.js';
 import { runFraudDetection } from '../detectors/detections/fraud-detection.js';
-import { DEFAULT_DETECTOR_SETTINGS } from '../detectors/default-config.js';
 
 export default class BreakageReporting extends ContentFeature {
     init() {
         const isExpandedPerformanceMetricsEnabled = this.getFeatureSettingEnabled('expandedPerformanceMetrics', 'enabled');
+
+        const detectorSettings = this.getFeatureSetting('webInterferenceDetection', 'interferenceTypes') || {};
+
         this.messaging.subscribe('getBreakageReportValues', async () => {
             const jsPerformance = getJsPerformanceMetrics();
             const referrer = document.referrer;
 
             // Get detector results (uses cached results from auto-run if available)
             const detectorData = {
-                botDetection: runBotDetection(DEFAULT_DETECTOR_SETTINGS.botDetection),
-                fraudDetection: runFraudDetection(DEFAULT_DETECTOR_SETTINGS.fraudDetection),
+                botDetection: runBotDetection(detectorSettings.botDetection),
+                fraudDetection: runFraudDetection(detectorSettings.fraudDetection),
             };
 
             const result = {
