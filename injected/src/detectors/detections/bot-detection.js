ed5e7e8a7d3a1aea2a90ed0a8446e296f2d659fc
Jason <jason@duckduckgo.com>
2025-11-21 14:26:34 -0700

    detector service for breakage reporting (#2052)
    * feat: web interference detection prototype
    
    * detector prototype v2
    
    * init and service
    
    * update readme
    
    * update readme
    
    * update readme
    
    * add auto run
    
    * drop global domains option
    
    * update readme
    
    * remove old file
    
    * pr review changes, drop yt detector
    
    * fix type erros
    
    * remove unused'
    
    * remove service and init"
    
    * fix test
    
    * pass config
    
    * get settings from webInterference
    
    * comment auto-run
    
    * revert features
    
    * revert features
    
    * remove cache, refresh'
    
    * update readme
    
    * update features.js
    
    * fix getFeature
    
    * detection tests
    
    * add px test
    
    * lint
    
    ---------
    
    Co-authored-by: madblex <alex.buliga.92@gmail.com>

---
+++ injected/src/detectors/detections/bot-detection.js
@@ -0,0 +1,44 @@
+import { checkSelectors, checkWindowProperties, matchesSelectors, matchesTextPatterns } from '../utils/detection-utils.js';
+
+/**
+ * Run bot detection.
+ * @param {Record<string, any>} config
+ */
+export function runBotDetection(config = {}) {
+    const results = Object.entries(config)
+        .filter(([_, challengeConfig]) => challengeConfig?.state === 'enabled')
+        .map(([challengeId, challengeConfig]) => {
+            const detected = checkSelectors(challengeConfig.selectors) || checkWindowProperties(challengeConfig.windowProperties || []);
+            if (!detected) {
+                return null;
+            }
+
+            const challengeStatus = findStatus(challengeConfig.statusSelectors);
+            return {
+                detected: true,
+                vendor: challengeConfig.vendor,
+                challengeType: challengeId,
+                challengeStatus,
+            };
+        })
+        .filter(Boolean);
+
+    return {
+        detected: results.length > 0,
+        type: 'botDetection',
+        results,
+    };
+}
+
+function findStatus(statusSelectors) {
+    if (!Array.isArray(statusSelectors)) {
+        return null;
+    }
+
+    const match = statusSelectors.find((statusConfig) => {
+        const { selectors, textPatterns, textSources } = statusConfig;
+        return matchesSelectors(selectors) || matchesTextPatterns(document.body, textPatterns, textSources);
+    });
+
+    return match?.status ?? null;
+}
