f70c326e6fcf5131465b750eb5448ca757acccb8
Jonathan Kingston <jkingston@duckduckgo.com>
2025-12-05 15:16:21 +0000

    Fix performance navigation check for page reload

--- injected/src/features/breakage-reporting.js
+++ injected/src/features/breakage-reporting.js
@@ -24,8 +24,8 @@ export default class BreakageReporting extends ContentFeature {
             if (getReloaded) {
                 result.pageReloaded =
                 (window.performance.navigation && window.performance.navigation.type === 1) ||
-                window.performance
-                    .getEntriesByType('navigation')
+                /** @type {PerformanceNavigationTiming[]} */
+                (window.performance.getEntriesByType('navigation'))
                     .map((nav) => nav.type)
                     .includes('reload');
             }
