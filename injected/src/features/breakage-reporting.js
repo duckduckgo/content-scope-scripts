afaa5084304617bc1c7e732342f956d089721308
Jonathan Kingston <jkingston@duckduckgo.com>
2025-12-05 15:20:46 +0000

    Lint fix

--- injected/src/features/breakage-reporting.js
+++ injected/src/features/breakage-reporting.js
@@ -23,11 +23,9 @@ export default class BreakageReporting extends ContentFeature {
             const getReloaded = this.getFeatureSettingEnabled('getReloaded', 'enabled');
             if (getReloaded) {
                 result.pageReloaded =
-                (window.performance.navigation && window.performance.navigation.type === 1) ||
-                /** @type {PerformanceNavigationTiming[]} */
-                (window.performance.getEntriesByType('navigation'))
-                    .map((nav) => nav.type)
-                    .includes('reload');
+                    (window.performance.navigation && window.performance.navigation.type === 1) ||
+                    /** @type {PerformanceNavigationTiming[]} */
+                    (window.performance.getEntriesByType('navigation')).map((nav) => nav.type).includes('reload');
             }
 
             // Only run detectors if explicitly configured
