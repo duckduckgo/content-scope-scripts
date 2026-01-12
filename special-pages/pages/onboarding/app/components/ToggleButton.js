1255467c51374e520933aa8afacd7e0ca8afb960
Moon Kyong <kmk1986@gmail.com>
2026-01-12 14:59:55 -0800

    Reduce svg path size

--- special-pages/pages/onboarding/app/components/ToggleButton.js
+++ special-pages/pages/onboarding/app/components/ToggleButton.js
@@ -13,12 +13,7 @@ const RadioButton = {
     ),
     Unselected: () => (
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM12 3.5C7.30558 3.5 3.5 7.30558 3.5 12C3.5 16.6944 7.30558 20.5 12 20.5C16.6944 20.5 20.5 16.6944 20.5 12C20.5 7.30558 16.6944 3.5 12 3.5Z"
-                className={styles.radioCircleUnselected}
-            />
+            <circle cx="12" cy="12" r="9.25" strokeWidth="1.5" className={styles.radioCircleUnselected} />
         </svg>
     ),
 };
