ee6d951cd71c78e46a923081985786450a86f9ed
Moon Kyong <kmk1986@gmail.com>
2026-01-13 11:18:24 -0800

    Fix the circle size

--- special-pages/pages/onboarding/app/components/ToggleButton.js
+++ special-pages/pages/onboarding/app/components/ToggleButton.js
@@ -13,7 +13,7 @@ const RadioButton = {
     ),
     Unselected: () => (
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
-            <circle cx="12" cy="12" r="9.25" strokeWidth="1.5" className={styles.radioCircleUnselected} />
+            <circle cx="12" cy="11" r="9.25" strokeWidth="1.5" className={styles.radioCircleUnselected} />
         </svg>
     ),
 };
