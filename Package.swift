b88986dfbe956ce22ba8e998106f06ce74a1f039
Federico Cappelli <kap.posta@gmail.com>
2026-01-13 17:57:42 +0000

    no message

--- Package.swift
+++ Package.swift
@@ -1,37 +1,29 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "content-scope-scripts",
-    defaultLocalization: "en",
-    platforms: [
-        .iOS(.v15),
-        .macOS("11.4")
-    ],
+    name: "ContentScopeScripts",
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ContentScopeScriptsResources",
-            targets: ["ContentScopeScriptsResources"]
-        ),
+            name: "ContentScopeScripts",
+            targets: ["ContentScopeScripts"]),
+    ],
+    dependencies: [
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ContentScopeScriptsResources",
-            path: ".",
-            sources: [
-                "Sources/ContentScopeScriptsResources",
-            ],
+            name: "ContentScopeScripts",
+            dependencies: [],
             resources: [
-                // Apple content scope scripts
-                .copy("build/apple/contentScope.js"),
-                .copy("build/apple/contentScopeIsolated.js"),
-                .copy("build/apple/duckAiDataClearing.js"),
-                // Duck:// pages used by Apple clients
-                .copy("build/apple/pages"),
+                .process("dist/contentScope.js"),
+                .process("dist/contentScopeIsolated.js"),
+                .copy("dist/pages"),
             ]
         ),
     ]
 )
-
