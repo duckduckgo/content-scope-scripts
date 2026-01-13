52d921c195f39c8249979a5f66ed437dcd4aa9fb
Federico Cappelli <kap.posta@gmail.com>
2026-01-13 14:53:20 +0000

    no message

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,7 +8,7 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [
         .iOS(.v15),
-        .macOS(.v11),
+        .macOS("11.4")
     ],
     products: [
         .library(
