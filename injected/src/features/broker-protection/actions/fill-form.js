b798229b52756b21ca7a6b882a0d2821c55929af
Brian Hall <brian@hack.design>
2026-01-28 18:20:02 +0100

    Remove unnecessary type casting

--- injected/src/features/broker-protection/actions/fill-form.js
+++ injected/src/features/broker-protection/actions/fill-form.js
@@ -200,7 +200,7 @@ function setValueForInput(el, val) {
             // Attempt to find the matching value in the select options, ignoring case
             const selectValues = [...selectElement.options].map((o) => o.value);
             const valStr = String(val);
-            const matchingValue = selectValues.find((option) => String(option).toLowerCase() === valStr.toLowerCase());
+            const matchingValue = selectValues.find((option) => option.toLowerCase() === valStr.toLowerCase());
 
             if (matchingValue === undefined) {
                 return { result: false, error: `could not find matching value for select element: ${val}` };
