ecbc2145c275088c3e7b8c7396ea584c1bba173f
jdorweiler <1882892+jdorweiler@users.noreply.github.com>
2026-02-06 18:02:54 +0000

    Release build 13.1.0 [ci release]

--- Sources/ContentScopeScripts/dist/duckAiChatHistory.js
+++ Sources/ContentScopeScripts/dist/duckAiChatHistory.js
@@ -484,7 +484,14 @@
   function isGloballyDisabled(args) {
     return args.site.allowlisted || args.site.isBroken;
   }
-  var platformSpecificFeatures = ["navigatorInterface", "windowsPermissionUsage", "messageBridge", "favicon", "breakageReporting"];
+  var platformSpecificFeatures = [
+    "navigatorInterface",
+    "windowsPermissionUsage",
+    "messageBridge",
+    "favicon",
+    "breakageReporting",
+    "webInterferenceDetection"
+  ];
   function isPlatformSpecificFeature(featureName) {
     return platformSpecificFeatures.includes(featureName);
   }
