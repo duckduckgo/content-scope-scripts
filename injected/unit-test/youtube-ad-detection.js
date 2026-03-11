e5551b53b6384b4db3fdbbeb5068a166503a99ca
jdorweiler <jason@duckduckgo.com>
2026-03-11 15:23:36 -0600

    update test

--- injected/unit-test/youtube-ad-detection.js
+++ injected/unit-test/youtube-ad-detection.js
@@ -11,7 +11,7 @@ const minimalConfig = {
     playabilityErrorPatterns: [],
     adBlockerDetectionSelectors: [],
     adBlockerDetectionPatterns: [],
-    loginStateSelectors: null,
+    loginStateSelectors: { signInButton: '', avatarButton: '', premiumLogo: '' },
 };
 
 const configWithAllEvents = {
