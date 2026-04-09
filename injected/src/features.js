c89834e26fb2247a712e088ef47a3042a02d0428
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-04-09 10:38:27 -0300

    Revert "Moves webTelemetry to non isolated"
    This reverts commit 76a952df4351b6d16ba0a65da8d588068dbe708a.

--- injected/src/features.js
+++ injected/src/features.js
@@ -57,7 +57,7 @@ const otherFeatures = /** @type {FeatureName[]} */ ([
 
 /** @type {Record<string, FeatureName[]>} */
 export const platformSupport = {
-    apple: ['webCompat', 'duckPlayerNative', ...baseFeatures, 'webTelemetry', 'pageContext', 'print', 'trackerProtection'],
+    apple: ['webCompat', 'duckPlayerNative', ...baseFeatures, 'pageContext', 'print', 'trackerProtection'],
     'apple-isolated': [
         'contextMenu',
         'duckPlayer',
@@ -71,6 +71,7 @@ export const platformSupport = {
         'webDetection',
         'webEvents',
         'webInterferenceDetection',
+        'webTelemetry',
         'pageObserver',
         'hover',
         'tabSuspension',
