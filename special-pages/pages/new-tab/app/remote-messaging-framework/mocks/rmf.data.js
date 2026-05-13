8fc8b217ca2d3d20847e20e1c7d8ad630670230f
Jonathan-L <jlockhart@duckduckgo.com>
2026-05-13 21:20:53 +0100

    added preview image mock

--- special-pages/pages/new-tab/app/remote-messaging-framework/mocks/rmf.data.js
+++ special-pages/pages/new-tab/app/remote-messaging-framework/mocks/rmf.data.js
@@ -133,4 +133,14 @@ export const rmfDataExamples = {
             primaryActionText: 'Update macOS',
         },
     },
+    big_single_action_preview: {
+        content: {
+            messageType: 'big_single_action',
+            id: 'id-big-single-alt',
+            titleText: 'Try out our Preview!',
+            descriptionText: 'Check out our preview release to try features before everyone else.',
+            icon: 'Preview',
+            primaryActionText: 'Try preview',
+        },
+    },
 };
