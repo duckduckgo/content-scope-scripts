198daa8b800f6e61de57fcfc91bbf855422c1e17
Brian Hall <brian@hack.design>
2026-06-27 07:08:25 +0200

    Add aliases for captcha providers (#2803)
    * Add aliases for captcha providers
    
    * Ensure an aliased captcha provider returns the correct type
    
    * Dedupe captcha types when getting all providers
    
    * Change local-llm to basic-math

--- injected/src/features/broker-protection/captcha-services/factory.js
+++ injected/src/features/broker-protection/captcha-services/factory.js
@@ -9,9 +9,19 @@ export class CaptchaFactory {
     /**
      * Register a captcha provider
      * @param {import('./providers/provider.interface').CaptchaProvider} provider - The provider to register
+     * @param {Object} [options] - The optional provider options
+     * @param {Array<string>} [options.aliases] - An optional list of aliases for that provider
      */
-    registerProvider(provider) {
+    registerProvider(provider, options) {
         this.providers.set(provider.getType(), provider);
+
+        // Some captchas use the same DOM mechanisms to get the relevant captcha information, but
+        // need to call dbp-api with a distinct captcha type.
+        if (options?.aliases) {
+            options?.aliases.forEach((alias) => {
+                this.providers.set(alias, provider);
+            });
+        }
     }
 
     /**
@@ -48,6 +58,8 @@ export class CaptchaFactory {
      * @returns {Array<import('./providers/provider.interface').CaptchaProvider>}
      */
     _getAllProviders() {
-        return Array.from(this.providers.values());
+        // Aliases register the same provider instance under multiple keys, so deduplicate to
+        // avoid running a provider's detection methods once per alias on the detection miss path.
+        return Array.from(new Set(this.providers.values()));
     }
 }
