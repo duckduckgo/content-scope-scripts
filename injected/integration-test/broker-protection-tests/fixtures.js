22bfc22f43a4e13cad055bcad96d6f024092db07
madblex <149243574+madblex@users.noreply.github.com>
2026-08-21 09:04:14 +0000

    Release build 16.13.0 [ci release]

--- injected/integration-test/broker-protection-tests/fixtures.js
+++ injected/integration-test/broker-protection-tests/fixtures.js
@@ -1,56 +1,19 @@
 import { BrokerProtectionPage } from '../page-objects/broker-protection.js';
+import { BROKER_PROTECTION_CONFIGS } from './tests-config.js';
 /**
- * @import {PlaywrightTestArgs, PlaywrightTestOptions, PlaywrightWorkerArgs, PlaywrightWorkerOptions, TestType} from "@playwright/test"
+ * @import {PlaywrightTestArgs, PlaywrightTestOptions, PlaywrightWorkerArgs, PlaywrightWorkerOptions, TestType, test as playwrightTest} from "@playwright/test"
  */
 
 /**
- * @param {typeof import("@playwright/test").test} test
- * @return {TestType<PlaywrightTestArgs & PlaywrightTestOptions & PlaywrightWorkerArgs & PlaywrightWorkerOptions & { createConfiguredDbp: (config: Record<string, any>) => Promise<BrokerProtectionPage> }, {}>}
+ * @param {typeof playwrightTest} test
+ * @return {TestType<PlaywrightTestArgs & PlaywrightTestOptions & PlaywrightWorkerArgs & PlaywrightWorkerOptions & { dbp: BrokerProtectionPage }, {}>}
  */
 export function createConfiguredDbpTest(test) {
     return test.extend({
-        createConfiguredDbp: async ({ page }, use, workerInfo) => {
-            /**
-             * @param {Record<string, any>} config
-             */
-            const createWithConfig = async (config) => {
-                const dbp = BrokerProtectionPage.create(page, workerInfo.project.use);
-                await dbp.withFeatureConfig(config);
-                return dbp;
-            };
-
-            await use(createWithConfig);
-        },
-    });
-}
-
-/**
- * @param {typeof import("@playwright/test").test} test
- * @return {TestType<PlaywrightTestArgs & PlaywrightTestOptions & PlaywrightWorkerArgs & PlaywrightWorkerOptions & { createConfiguredDbp: (config: Record<string, any>) => Promise<BrokerProtectionPage> }, {}>}
- */
-export function createConfiguredDbpTestWithNavigation(test) {
-    return test.extend({
-        createConfiguredDbp: async ({ page }, use, workerInfo) => {
-            /**
-             * @param {object} params
-             * @param {string} params.targetPage
-             * @param {string|object} params.action
-             * @param {Record<string, any>} params.config
-             */
-            const createWithConfig = async (params) => {
-                const { config, targetPage, action } = params;
-                const dbp = BrokerProtectionPage.create(page, workerInfo.project.use);
-                await dbp.withFeatureConfig(config);
-                await dbp.navigatesTo(targetPage);
-                if (typeof action === 'string') {
-                    dbp.receivesAction(action);
-                } else {
-                    await dbp.receivesInlineAction(action);
-                }
-                return dbp;
-            };
-
-            await use(createWithConfig);
+        dbp: async ({ page }, use, workerInfo) => {
+            const dbp = BrokerProtectionPage.create(page, workerInfo.project.use);
+            await dbp.withFeatureConfig(BROKER_PROTECTION_CONFIGS.default);
+            await use(dbp);
         },
     });
 }
